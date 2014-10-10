3695cac0bb9ee4da2854a6f37c3daf9fc53f6ce2
Eric Favre <ericfavreddd@gmail.com>
2014-10-10 16:06:09 +0200

    add admin for message

---
+++ app/admin/message.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Message do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+  permit_params :date, :content, :user_id, :user, :group_id, :group, :is_private
+
+end
