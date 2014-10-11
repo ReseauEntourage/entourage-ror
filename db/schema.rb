6640ac8e83c89fc48d323726babb9bdbcd5016ef
Louis Davin <ldavin@octo.com>
2014-10-11 13:21:21 +0200

    Add tokens on users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011095442) do
+ActiveRecord::Schema.define(version: 20141011105539) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20141011095442) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
+    t.string   "token"
   end
 
 end
