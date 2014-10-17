fb4fbee3eed1f6b913c60961cf795c623c772d49
Eric Favre <ericfavreddd@gmail.com>
2014-10-17 17:32:48 +0200

    limit POIs returned by map controller to 10 items

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
 
   def index
     @categories = Category.all
-    @pois = Poi.all
+    @pois = Poi.all.limit(10)
     @encounters = Encounter.all.includes(:user)
   end
 
