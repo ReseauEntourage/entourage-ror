a7c29bea002abda614949725efa3813831b67476
Eric Favre <efavre@octo.com>
2015-07-27 13:44:56 +0200

    home map : order tour points by passing time

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,13 +4,12 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 13,
     center: new google.maps.LatLng(48.858859, 2.3470599),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
   });
 
   map.data.setStyle(function(feature) {
     var tourType = feature.getProperty('tour_type');
     var color = colors[tourType];
-    return /** @type {google.maps.Data.StyleOptions} */({
+    return ({
       strokeColor: color,
       strokeWeight: 2
     });
