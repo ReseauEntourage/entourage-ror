10af30ac047363c4a04df5c49e1363f2626d416f
Eric Favre <efavre@octo.com>
2015-07-29 10:35:32 +0200

    deactivate tour report email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
