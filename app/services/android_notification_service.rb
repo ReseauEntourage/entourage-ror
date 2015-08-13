5980012aef806388df739ecfe25b40b5bd2c58e2
Jean-Yves Rivallan <jyrivallan@octo.com>
2015-08-13 18:11:33 +0200

    added a safeguard on the android notification service

--- app/services/android_notification_service.rb
+++ app/services/android_notification_service.rb
@@ -4,13 +4,19 @@ class AndroidNotificationService
   end
   
   def send_notification(sender, object, content, device_ids)
-    notification = Rpush::Gcm::Notification.new
-    notification.app = Rpush::Gcm::App.find_by_name('entourage')
-    notification.registration_ids = device_ids
-    notification.data = { sender: sender, object: object, content: content }
-    notification.save!
+    entourage = Rpush::Gcm::App.find_by_name('entourage')
     
-    @notification_pusher.push
+    if entourage.nil?
+      Rails.logger.warn 'No android notification has been sent. Please save a Rpush::Gcm::App in database'.red
+    else
+      notification = Rpush::Gcm::Notification.new
+      notification.app = entourage
+      notification.registration_ids = device_ids
+      notification.data = { sender: sender, object: object, content: content }
+      notification.save!
+      
+      @notification_pusher.push
+    end
   end
   
   private
--- spec/services/android_notification_service_spec.rb
+++ spec/services/android_notification_service_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 describe AndroidNotificationService do
   describe '#send_notification' do
-    let!(:android_app) { FactoryGirl.create :android_app }
     let!(:sender) { 'sender' }
     let!(:object) { 'object' }
     let!(:content) { 'content' }
     let!(:device_ids) { ['id1', 'id2', 'id3'] }
     let!(:notification_pusher) { spy('notification_pusher') }
-    subject! { AndroidNotificationService.new(notification_pusher).send_notification(sender, object, content, device_ids) }
-    it { expect(Rpush::Gcm::Notification.last.app).to eq(android_app) }
-    it { expect(Rpush::Gcm::Notification.last.registration_ids).to eq(device_ids) }
-    it { expect(Rpush::Gcm::Notification.last.data).to eq({ "sender" => sender, "object" => object, "content" => content }) }
-    it { expect(notification_pusher).to have_received(:push).with(no_args) }
+    context 'android app is present' do
+      let!(:android_app) { FactoryGirl.create :android_app }
+      subject! { AndroidNotificationService.new(notification_pusher).send_notification(sender, object, content, device_ids) }
+      it { expect(Rpush::Gcm::Notification.last.app).to eq(android_app) }
+      it { expect(Rpush::Gcm::Notification.last.registration_ids).to eq(device_ids) }
+      it { expect(Rpush::Gcm::Notification.last.data).to eq({ "sender" => sender, "object" => object, "content" => content }) }
+      it { expect(notification_pusher).to have_received(:push).with(no_args) }
+    end
+    context 'android app is absent' do
+      before { Rails.logger.stub(:warn) }
+      subject! { AndroidNotificationService.new(notification_pusher).send_notification(sender, object, content, device_ids) }
+      it { expect(Rails.logger).to have_received(:warn).with('No android notification has been sent. Please save a Rpush::Gcm::App in database'.red) }
+    end
   end
 end
