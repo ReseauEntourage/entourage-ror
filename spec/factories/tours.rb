2d6dcc5fbaac16346f0522fae6f7e67479602628
Jean-Yves Rivallan <jyrivallan@octo.com>
2015-08-25 13:55:32 +0200

    enhanced the tour factory

--- spec/factories/tours.rb
+++ spec/factories/tours.rb
@@ -8,6 +8,9 @@ FactoryGirl.define do
     user
     
     trait :filled do
+      status 'closed'
+      created_at { Time.now - 3 * 60 * 60 }
+      closed_at { Time.now - 2 * 60 * 60 }
       after(:create) do |tour, evaluator|
         create_list(:tour_point, 10, :in_paris, :now, tour: tour)
         create_list(:encounter, 2, :in_paris, :now, tour: tour)
