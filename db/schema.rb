a1bca8499f434c6a2dcb034e32dcdaf2a38b6848
François Pellissier <francois.pellissier@gmail.com>
2015-09-10 11:10:19 +0200

    Add logo_url field to Oganizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902083151) do
+ActiveRecord::Schema.define(version: 20150910083952) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150902083151) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "logo_url"
   end
 
   create_table "pois", force: :cascade do |t|
