0b60bbd0250670b05fb2b81c404280678d8ab652
Jean-Yves Rivallan <jyrivallan@octo.com>
2015-09-17 15:12:15 +0200

    fixed a unit test

--- spec/controllers/organization/users_controller_spec.rb
+++ spec/controllers/organization/users_controller_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe Organization::UsersController, :type => :controller do
           post 'send_sms', id: sms_user.id, format: :json
         end
         it { expect(response.status).to eq(200) }
-        it { expect(sms_notification_service).to have_received(:send_notification).with(sms_user.phone, "Bienvenue sur Entourage. Votre code est #{sms_user.sms_code}. Retrouvez l'application ici : #{link}.") }
+        it { expect(sms_notification_service).to have_received(:send_notification).with(sms_user.phone, "Bienvenue sur Entourage. Votre code est #{sms_user.sms_code}. Retrouvez l'application ici : #{link} .") }
       end
       context 'the user exists but different organizations' do
         let(:sms_user) { FactoryGirl.create :user }
