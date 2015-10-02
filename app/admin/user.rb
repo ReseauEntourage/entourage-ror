eb7c1e81e1131af7bf5a796ab06ee17f79f8aad9
Jean-Yves Rivallan <jyrivallan@octo.com>
2015-10-02 15:27:53 +0200

    added coordinated organizations in activeadmin user edit page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,14 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :first_name, :last_name, :phone, :sms_code, :manager, :organization_id
+  permit_params :email, :first_name, :last_name, :phone, :sms_code, :manager, :organization_id, :token, :device_type, :default_latitude, :default_longitude, coordinated_organization_ids: []
+
   active_admin_import
 
+  form do |f|
+    f.inputs
+    f.inputs "Coordination" do
+      f.input :coordinated_organizations, as: :check_boxes, collection: Organization.all
+    end
+    f.actions
+  end
 end
