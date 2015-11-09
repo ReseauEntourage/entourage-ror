e760dc884472a4651fcb54d8026a1541dd0f5926
FrPellissier <francois.pellissier@gmail.com>
2015-11-09 15:32:36 +0100

    Adding task: force_resend_tour_reports

--- app/services/cleanup_service.rb
+++ app/services/cleanup_service.rb
@@ -2,10 +2,20 @@ class CleanupService
   def self.force_close_tours
     old_tours = Tour.where(status: Tour.statuses[:ongoing])
       .where('created_at <= ?', Time.now - 4.hours)
-      
+
     old_tours.each do |t|
       t.force_close
-      Rails.logger.warn "Force closed tour #{t}"
+      Rails.logger.warn "Force closing tour #{t}"
+    end
+  end
+
+  def self.force_resend_tour_reports
+    old_tours = Tour.where(status: Tour.statuses[:closed], email_sent: true)
+      .where('closed_at <= ?', Time.now - 4.hours)
+
+    old_tours.each do |t|
+      t.send_tour_report
+      Rails.logger.warn "Force sending tour report #{t}"
     end
   end
 end
