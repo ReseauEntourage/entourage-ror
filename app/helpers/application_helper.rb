5acd039764cfbe2cfe506dcb66c9845c59d65d87
Vincent Daubry <vdaubry@gmail.com>
2015-12-07 15:30:29 +0100

    fix admin error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     { 'success' => "alert-success", 'error' => "alert-danger", 'alert' => "alert-warning", 'notice' => "alert-info" }[flash_type] || flash_type.to_s
   end
 
-  def flash_messages(opts = {})
+  def display_flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:p, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
