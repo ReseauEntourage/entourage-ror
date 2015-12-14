2fe5f2f73de4893654f5635e6cb9ea1e332cab79
Vincent Daubry <vdaubry@gmail.com>
2015-12-14 16:12:15 +0100

    fix font loading + datepicker lib

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -54,6 +54,54 @@ var ready = function() {
   if ($(".organizations.dashboard")[0]) {
     displayDashboardMapData();
   }
+
+  $('[data-toggle="tooltip"]').tooltip()
+  
+  $('input[name="daterange"]').daterangepicker({
+    opens:'left',
+    linkedCalendars: false,
+    ranges: {
+      "Aujourd'hui": [moment(), moment()],
+      "Hier": [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      "Les 7 derniers jours": [moment().subtract(6, 'days'), moment()],
+      "Les 30 derniers jours": [moment().subtract(29, 'days'), moment()],
+      "Le mois en cours": [moment().startOf('month'), moment().endOf('month')],
+      "Le mois dernier": [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    locale: {
+      "format": "DD/MM/YYYY",
+      "separator": "-",
+      "applyLabel": "OK",
+      "cancelLabel": "Annuler",
+      "fromLabel": "De",
+      "toLabel": "à",
+      "customRangeLabel": "Autre",
+      "daysOfWeek": [
+        "Dim",
+        "Lun",
+        "Mar",
+        "Mer",
+        "Jeu",
+        "Ven",
+        "Sam"
+      ],
+      "monthNames": [
+        "Janvier",
+        "Février",
+        "Mars",
+        "Avril",
+        "Mai",
+        "Juin",
+        "Juillet",
+        "Aout",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Decembre"
+      ],
+      "firstDay": 1
+    }
+  })
 };
 
 $(document).ready(ready);
