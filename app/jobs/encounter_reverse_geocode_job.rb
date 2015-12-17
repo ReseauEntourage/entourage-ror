7ad2f2713784b228e695ce184b43a307b3251aee
Vincent Daubry <vdaubry@gmail.com>
2015-12-17 13:05:05 +0100

    save encounter address

--- app/jobs/encounter_reverse_geocode_job.rb
+++ app/jobs/encounter_reverse_geocode_job.rb
@@ -1,6 +1,10 @@
 class EncounterReverseGeocodeJob < ActiveJob::Base
 
   def perform(encounter_id)
-    Encounter.find(encounter_id).reverse_geocode unless Rails.env.test?
+    return if Rails.env.test?
+
+    encounter = Encounter.find(encounter_id)
+    encounter.reverse_geocode
+    encounter.save
   end
 end
