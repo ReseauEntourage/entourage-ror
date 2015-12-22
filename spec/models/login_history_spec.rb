884adf46306e8db8e96944b29b549064bc634ac3
Vincent Daubry <vdaubry@gmail.com>
2015-12-22 15:28:19 +0100

    do not auto load test db from schema

--- spec/models/login_history_spec.rb
+++ spec/models/login_history_spec.rb
@@ -10,11 +10,7 @@ RSpec.describe LoginHistory, :type => :model do
     it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be false }
     it { expect(LoginHistory.new(user_id: 2, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be true }
     it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 21:01:00").utc).save).to be false }
-
-    context "fail on sempahore ? (db index problem, currently investigating)", :skip_sempahore => true do
-      it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 20:44:00")).save).to be true }
-      it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("09/10/2010 21:44:00")).save).to be true }
-    end
-
+    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 20:44:00")).save).to be true }
+    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("09/10/2010 21:44:00")).save).to be true }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,6 @@ require 'rspec/rails'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
