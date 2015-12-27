428c69b10b9eb4d63ab2688365993f1a5df7b364
Vincent Daubry <vdaubry@gmail.com>
2015-12-27 10:53:40 +0100

    add list and destroy scheduled pushes

--- app/services/tour_services/schedule_push_service.rb
+++ app/services/tour_services/schedule_push_service.rb
@@ -3,7 +3,9 @@ module TourServices
     def initialize(organization:, date:)
       @organization = organization
       @date = date
-      raise TourServices::InvalidScheduledPushDateError.new("Cannot schedule a push with past date : #{date}") if date < Date.today
+      if date < Date.today
+        raise TourServices::InvalidScheduledPushDateError.new("Cannot schedule a push with past date : #{date}")
+      end
     end
 
     def schedule(object:, message:, sender:)
@@ -17,7 +19,22 @@ module TourServices
 
     def send_to(user)
       return if scheduled_message.blank?
-      push_service.send_notification(scheduled_message[:sender], scheduled_message[:object], scheduled_message[:message], User.where(id: user.id))
+      push_service.send_notification(scheduled_message[:sender],
+                                     scheduled_message[:object],
+                                     scheduled_message[:message],
+                                     User.where(id: user.id))
+    end
+
+    def destroy
+      $redis.del(key)
+    end
+
+    def self.all_scheduled_pushes(organization:)
+      $redis.keys("scheduled_message:organization:#{organization.id}*").map  do |key|
+        date = key.split(":").last
+        message = $redis.hgetall(key)
+        message.merge({date: date})
+      end.sort_by { |msg| Date.parse(msg[:date]) }
     end
 
     private
---
+++ spec/controllers/scheduled_pushes_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+include AuthHelper
+
+describe ScheduledPushesController do
+  let(:service) { TourServices::SchedulePushService }
+
+  describe 'GET index' do
+    context "user not logged in" do
+      before { get :index }
+      it { should redirect_to new_session_path  }
+    end
+
+    context "user logged in" do
+      let!(:user) { user_basic_login }
+
+      context "has push" do
+        before { service.new(organization: user.organization, date: Date.parse("10/10/2016")).schedule(object: "foo1", message: "bar", sender: "me") }
+        before { service.new(organization: user.organization, date: Date.parse("11/10/2016")).schedule(object: "foo2", message: "bar", sender: "me") }
+        before { service.new(organization: user.organization, date: Date.parse("09/10/2016")).schedule(object: "foo3", message: "bar", sender: "me") }
+        before { service.new(organization: FactoryGirl.create(:organization), date: Date.parse("09/10/2016")).schedule(object: "foo3", message: "bar", sender: "me") }
+        before { get :index }
+        it { expect(assigns(:scheduled_pushes)).to eq([{"object"=>"foo3", "message"=>"bar", "sender"=>"me", :date=>"2016-10-09"},
+                                                       {"object"=>"foo1", "message"=>"bar", "sender"=>"me", :date=>"2016-10-10"},
+                                                       {"object"=>"foo2", "message"=>"bar", "sender"=>"me", :date=>"2016-10-11"}]) }
+      end
+
+      context "no push" do
+        before { get :index }
+        it { expect(assigns(:scheduled_pushes)).to eq([]) }
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    context "user not logged in" do
+      before { delete :destroy, date: "10/10/2016" }
+      it { should redirect_to new_session_path  }
+    end
+
+    context "user logged in" do
+      let!(:user) { user_basic_login }
+      before { service.new(organization: user.organization, date: Date.parse("10/10/2016")).schedule(object: "foo1", message: "bar", sender: "me") }
+      before { service.new(organization: user.organization, date: Date.parse("11/10/2016")).schedule(object: "foo2", message: "bar", sender: "me") }
+      before { delete :destroy, date: "10/10/2016" }
+      it { expect(TourServices::SchedulePushService.all_scheduled_pushes(organization: user.organization)).to eq([{"object"=>"foo2", "message"=>"bar", "sender"=>"me", :date=>"2016-10-11"}]) }
+    end
+  end
+end
