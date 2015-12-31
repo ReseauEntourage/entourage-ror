69d20a5b5c700394a8e04f118083cda96736ad47
Vincent Daubry <vdaubry@gmail.com>
2015-12-31 10:50:01 +0100

    addpoi serializer

--- app/controllers/api/v0/pois_controller.rb
+++ app/controllers/api/v0/pois_controller.rb
@@ -7,15 +7,20 @@ module Api
         @categories = Category.all
         @pois = Poi.all
         @pois = @pois.around params[:latitude], params[:longitude], params[:distance] if params[:latitude].present? and params[:longitude].present?
+
+        #TODO : refactor API to refactor 1 top level POI ressources and associated categories ressources
+        poi_json = JSON.parse(ActiveModel::ArraySerializer.new(@pois, each_serializer: PoiSerializer).to_json)
+        categorie_json = JSON.parse(ActiveModel::ArraySerializer.new(@categories, each_serializer: CategorySerializer).to_json)
+        render json: {pois: poi_json, categories: categorie_json }, status: 200
       end
 
       def create
         @poi = Poi.new(poi_params)
         @poi.validated = false
         if @poi.save
-          render "show", status: 201
+          render json: @poi, status: 201
         else
-          render '400', status: 400
+          render json: {message: "Could not create POI", reasons: @poi.errors.full_message }, status: 400
         end
       end
 
--- spec/controllers/api/v0/pois_controller_spec.rb
+++ spec/controllers/api/v0/pois_controller_spec.rb
@@ -5,7 +5,8 @@ describe Api::V0::PoisController, :type => :controller do
   
   context 'authorized' do
     let!(:user) { create :user }
-    describe '#index' do
+
+    describe 'index' do
       context 'without parameters' do
         let!(:category1) { create :category }
         let!(:category2) { create :category }
@@ -15,18 +16,34 @@ describe Api::V0::PoisController, :type => :controller do
         before { get 'index', token: user.token, :format => :json }
         it { expect(assigns(:categories)).to eq([category1, category2]) }
         it { expect(assigns(:pois)).to eq([poi1, poi3]) }
+
+        it "renders POI" do
+          res = JSON.parse(response.body)
+          expect(res).to eq({"categories"=>[
+              {"id"=>category1.id, "name"=>category1.name},
+              {"id"=>category2.id, "name"=>category2.name}],
+                             "pois"=>[
+                                 {"id"=>poi1.id,
+                                  "name"=>"Dede",
+                                  "description"=>nil,
+                                  "longitude"=>2.30681949999996,
+                                  "latitude"=>48.870424, "adress"=>"Au 50 75008 Paris", "phone"=>"0000000000", "website"=>"entourage.com", "email"=>"entourage@entourage.com", "audience"=>"Mon audience", validated: true, "category_id"=>poi1.category_id, "category"=>{"id"=>poi1.category.id, "name"=>poi1.category.name}}, {"id"=>poi3.id, "name"=>"Dede", "description"=>nil, "longitude"=>2.30681949999996, "latitude"=>48.870424, "adress"=>"Au 50 75008 Paris", "phone"=>"0000000000", "website"=>"entourage.com", "email"=>"entourage@entourage.com", "audience"=>"Mon audience", validated: true, "category_id"=>poi3.category_id, "category"=>{"id"=>poi3.category.id, "name"=>poi3.category.name}}]})
+        end
       end
+
       context 'with location parameters' do
         let!(:poi1) { create :poi, latitude: 10, longitude: 12 }
         let!(:poi2) { create :poi, latitude: 9.9, longitude: 10.1 }
         let!(:poi3) { create :poi, latitude: 10, longitude: 10 }
         let!(:poi4) { create :poi, latitude: 10.05, longitude: 9.95 }
         let!(:poi5) { create :poi, latitude: 12, longitude: 10 }
+
         context 'without distance' do
           before { get :index, token: user.token, latitude: 10.0, longitude: 10.0, format: :json }
           it { expect(response.status).to eq(200) }
           it { expect(assigns[:pois]).to eq [poi3, poi4] }
         end
+
         context 'with distance' do
           before { get :index, token: user.token, latitude: 10.0, longitude: 10.0, distance: 20.0, format: :json }
           it { expect(response.status).to eq(200) }
@@ -35,7 +52,7 @@ describe Api::V0::PoisController, :type => :controller do
       end
     end
     
-    describe '#create' do
+    describe 'create' do
       let!(:poi) { build :poi }
       before { post :create, token: user.token, poi: { name: poi.name, latitude: poi.latitude, longitude: poi.longitude, adress: poi.adress, phone: poi.phone, website: poi.website, email: poi.email, audience: poi.audience, category_id: poi.category_id }, format: :json}
       it { expect(response.status).to eq(201) }
@@ -49,13 +66,20 @@ describe Api::V0::PoisController, :type => :controller do
       it { expect(Poi.unscoped.last.audience).to eq poi.audience }
       it { expect(Poi.unscoped.last.category).to eq poi.category }
       it { expect(Poi.unscoped.last.validated).to be false }
+
+      it "renders POI" do
+        poi = Poi.unscoped.last
+        res = JSON.parse(response.body)
+        expect(res).to eq({"poi"=>{"id"=>poi.id, "name"=>"Dede", "description"=>nil, "longitude"=>2.30681949999996, "latitude"=>48.870424, "adress"=>"Au 50 75008 Paris", "phone"=>"0000000000", "website"=>"entourage.com", "email"=>"entourage@entourage.com", "audience"=>"Mon audience", "validated"=>false, "category_id"=>poi.category_id, "category"=>{"id"=>poi.category.id, "name"=>poi.category.name}}})
+      end
     end
     
-    describe '#report' do
+    describe 'report' do
       let!(:poi) { create :poi }
       let!(:mail) { spy('mail') }
       let!(:member_mailer) { spy('member_mailer', poi_report: mail) }
       let!(:message) { 'message' }
+
       describe 'correct request' do
         before do
           controller.member_mailer = member_mailer
@@ -65,10 +89,12 @@ describe Api::V0::PoisController, :type => :controller do
         it { expect(member_mailer).to have_received(:poi_report).with poi, user, message }
         it { expect(mail).to have_received(:deliver_now).with no_args }
       end
+
       describe 'wrong poi id' do
         before { post :report, id: -1, token: user.token, message: message, format: :json }
         it { expect(response.status).to eq(404) }
       end
+
       describe 'no message' do
         before { post :report, id: poi.id, token: user.token, format: :json }
         it { expect(response.status).to eq(400) }
