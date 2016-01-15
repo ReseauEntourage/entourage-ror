8ef6fc5080379a2334a255635eb09a1403881dc9
Vincent Daubry <vdaubry@gmail.com>
2016-01-15 06:00:28 +0100

    force authent for feeds

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class FeedsController < Api::V1::BaseController
-      skip_before_filter :authenticate_user!
       def index
         render file: 'mocks/feeds.json'
       end
