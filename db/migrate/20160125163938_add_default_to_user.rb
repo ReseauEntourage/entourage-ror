bbe9c314e1ab67307bea2b82474554daafbfc7a3
Vincent Daubry <vdaubry@gmail.com>
2016-01-25 18:11:11 +0100

    reversible migration

--- db/migrate/20160125163938_add_default_to_user.rb
+++ db/migrate/20160125163938_add_default_to_user.rb
@@ -1,9 +1,15 @@
 class AddDefaultToUser < ActiveRecord::Migration
-  def change
+  def up
     User.where("phone IS NULL OR organization_id IS NULL").destroy_all
 
     change_column :users, :manager,         :boolean, null: false, default: false
     change_column :users, :phone,           :string,  null: false
     change_column :users, :organization_id, :integer, null: false
+  end
+
+  def down
+    change_column :users, :manager,         :boolean
+    change_column :users, :phone,           :string
+    change_column :users, :organization_id, :integer
   end
 end
