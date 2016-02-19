9d1ad115af8c7727751660e4de5cfb1f45f26158
Vincent Daubry <vdaubry@gmail.com>
2016-02-19 19:35:31 +0100

    add constrains on questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
+  belongs_to :organization
+
   validates :title, :answer_type, :answer_value, :organization_id, presence: true
+  validate :max_question_per_organization
+
+  def max_question_per_organization
+    if organization && organization.questions.count >=5
+      self.errors.add(:base, "Vous ne pouvez pas ajouter plus de 5 questions pour une association")
+    end
+  end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :question do
     title "MyString"
-answer_type "MyString"
-answer_value "MyString"
-organization_id 1
+    answer_type "MyString"
+    answer_value "MyString"
+    organization
   end
 
 end
