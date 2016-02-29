7a0b4c79c32129cb18c2f8c5d5f83ed03526c22d
Vincent Daubry <vdaubry@gmail.com>
2016-02-29 18:09:49 +0100

    add api keys for versions 1.1 and 2.0

--- app/services/api/application_key.rb
+++ app/services/api/application_key.rb
@@ -11,12 +11,16 @@ module Api
     private
     attr_reader :api_key
 
-    #Generate keys with
+    #Generate keys with SecureRandom.hex(12)
     def api_keys
       {
         "api_debug" => {version: "1.0", device: "foo"},
         "b05e6d0d2be8" => {version: "1.0.3", device: "iOS"},
-        "32e2ced9df89" => {version: "1.0.24", device: "Android"}
+        "32e2ced9df89" => {version: "1.0.24", device: "Android"},
+        "d05394bcf705bbd4d6923bd9" => {version: "1.1.0", device: "iOS"},
+        "7a8c1f9f4b973384aaff3ed3" => {version: "1.1.0", device: "Android"},
+        "aa8b4eaa3701cf943fde3624" => {version: "2.0.0", device: "iOS"},
+        "8a3ef2f6b712b96ac4d0d654" => {version: "2.0.0", device: "Android"}
       }
     end
   end
