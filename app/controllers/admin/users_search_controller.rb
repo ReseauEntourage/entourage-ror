cbd539de84dc416d5eb705f18916873895e2d7da
Vincent Daubry <vdaubry@gmail.com>
2016-04-01 13:50:18 +0200

    don't filter relations on public users

--- app/controllers/admin/users_search_controller.rb
+++ app/controllers/admin/users_search_controller.rb
@@ -10,8 +10,7 @@ module Admin
     end
 
     def public_user_autocomplete
-      users = User.type_public
-                  .search_by(search_param, search_param, search_param, params[:search])
+      users = User.search_by(search_param, search_param, search_param, params[:search])
                   .order("last_name ASC")
                   .limit(25)
       results = users.map {|u| {label: u.full_name, value: u.full_name, id: u.id} }
