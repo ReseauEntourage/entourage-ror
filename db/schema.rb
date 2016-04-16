afe2996f85807ad7c1bfad762987f12dcb11be7a
Vincent Daubry <vdaubry@gmail.com>
2016-04-16 09:33:40 +0200

    Update Rails + Rpush

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410143008) do
+ActiveRecord::Schema.define(version: 20160416072946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace",     limit: 255
+    t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   limit: 255, null: false
-    t.string   "resource_type", limit: 255, null: false
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
-    t.string   "author_type",   limit: 255
+    t.string   "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20160410143008) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",       limit: 255
+    t.string   "name"
   end
 
   create_table "chat_messages", force: :cascade do |t|
@@ -68,10 +68,10 @@ ActiveRecord::Schema.define(version: 20160410143008) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "street_person_name", limit: 255
+    t.string   "street_person_name"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "voice_message_url",  limit: 255
+    t.string   "voice_message_url"
     t.integer  "tour_id"
     t.string   "encrypted_message"
     t.string   "address"
@@ -134,7 +134,7 @@ ActiveRecord::Schema.define(version: 20160410143008) do
   end
 
   create_table "newsletter_subscriptions", force: :cascade do |t|
-    t.string   "email",      limit: 255
+    t.string   "email"
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -157,19 +157,19 @@ ActiveRecord::Schema.define(version: 20160410143008) do
   add_index "organizations", ["name"], name: "index_organizations_on_name", unique: true, using: :btree
 
   create_table "pois", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.text     "description"
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "adress",      limit: 255
-    t.string   "phone",       limit: 255
-    t.string   "website",     limit: 255
-    t.string   "email",       limit: 255
-    t.string   "audience",    limit: 255
+    t.string   "adress"
+    t.string   "phone"
+    t.string   "website"
+    t.string   "email"
+    t.string   "audience"
     t.integer  "category_id"
-    t.boolean  "validated",               default: false, null: false
+    t.boolean  "validated",   default: false, null: false
   end
 
   add_index "pois", ["latitude", "longitude"], name: "index_pois_on_latitude_and_longitude", using: :btree
@@ -221,7 +221,7 @@ ActiveRecord::Schema.define(version: 20160410143008) do
     t.integer  "badge"
     t.string   "device_token",      limit: 64
     t.string   "sound",                        default: "default"
-    t.string   "alert"
+    t.text     "alert"
     t.text     "data"
     t.integer  "expiry",                       default: 86400
     t.boolean  "delivered",                    default: false,     null: false
@@ -246,6 +246,8 @@ ActiveRecord::Schema.define(version: 20160410143008) do
     t.integer  "priority"
     t.text     "url_args"
     t.string   "category"
+    t.boolean  "content_available",            default: false
+    t.text     "notification"
   end
 
   add_index "rpush_notifications", ["delivered", "failed"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))", using: :btree
@@ -327,23 +329,23 @@ ActiveRecord::Schema.define(version: 20160410143008) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",             limit: 255
-    t.string   "first_name",        limit: 255
-    t.string   "last_name",         limit: 255
-    t.string   "phone",                                               null: false
-    t.string   "token",             limit: 255
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone",                                   null: false
+    t.string   "token"
     t.string   "device_id"
     t.integer  "device_type"
     t.string   "sms_code"
     t.integer  "organization_id"
-    t.boolean  "manager",                       default: false,       null: false
+    t.boolean  "manager",           default: false,       null: false
     t.float    "default_latitude"
     t.float    "default_longitude"
-    t.boolean  "admin",                         default: false,       null: false
-    t.string   "user_type",                     default: "pro",       null: false
+    t.boolean  "admin",             default: false,       null: false
+    t.string   "user_type",         default: "pro",       null: false
     t.string   "avatar_key"
-    t.string   "validation_status",             default: "validated", null: false
-    t.boolean  "deleted",                       default: false,       null: false
+    t.string   "validation_status", default: "validated", null: false
+    t.boolean  "deleted",           default: false,       null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
