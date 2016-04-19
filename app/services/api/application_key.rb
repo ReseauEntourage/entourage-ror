07d74cb9a34fcf2885e5c31b3db197b866619ed9
Vincent Daubry <vdaubry@gmail.com>
2016-04-19 14:50:09 +0200

    V1.2 api keys

--- app/services/api/application_key.rb
+++ app/services/api/application_key.rb
@@ -21,6 +21,8 @@ module Api
         "32e2ced9df89" => {version: "1.0.24", device: "Android", device_family: UserApplication::ANDROID},
         "d05394bcf705bbd4d6923bd9" => {version: "1.1.0", device: "iOS", device_family: UserApplication::IOS},
         "7a8c1f9f4b973384aaff3ed3" => {version: "1.1.0", device: "Android", device_family: UserApplication::ANDROID},
+        "fbe5b5e0bd4ec94146b3dc9b" => {version: "1.2.0", device: "iOS", device_family: UserApplication::IOS},
+        "2b8259ac4aad2cfd0b46be77" => {version: "1.2.0", device: "Android", device_family: UserApplication::ANDROID},
         "aa8b4eaa3701cf943fde3624" => {version: "2.0.0", device: "iOS", device_family: UserApplication::IOS},
         "8a3ef2f6b712b96ac4d0d654" => {version: "2.0.0", device: "Android", device_family: UserApplication::ANDROID}
       }
