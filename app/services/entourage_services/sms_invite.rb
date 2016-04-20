86f1910408bd80145d8ecd3bc13596cceda7c9ef
Vincent Daubry <vdaubry@gmail.com>
2016-04-20 07:32:04 +0200

    send sms

--- app/services/entourage_services/sms_invite.rb
+++ app/services/entourage_services/sms_invite.rb
@@ -14,11 +14,16 @@ module EntourageServices
         return callback.on_not_part_of_entourage.try(:call)
       end
 
+      Faire une transaction :
+      créer le user puis l'invitation
+
       invite = EntourageInvitation.new(invitable: entourage,
                                        inviter: inviter,
                                        phone_number: phone_number,
                                        invitation_mode: EntourageInvitation::MODE_SMS)
+
       if invite.save
+        SmsSenderJob.perform_later(phone_number, message)
         callback.on_success.try(:call, invite)
       else
         callback.on_failure.try(:call, invite)
@@ -27,6 +32,14 @@ module EntourageServices
 
     private
     attr_reader :phone_number, :entourage, :callback, :inviter
+
+    def message
+      "Bonjour, vous êtes invité à rejoindre un Entourage. Votre code est #{sms_code}. Retrouvez l'application ici : #{link} ."
+    end
+
+    def link
+      link = Rails.env.test? ? "http://foo.bar" : url_shortener.shorten(Rails.application.routes.url_helpers.store_redirection_url)
+    end
   end
 
   class SmsInviteCallback < Callback
