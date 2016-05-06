0d1da750f9d5ed2152153e7841bc4826b99f4c85
Vincent Daubry <vdaubry@gmail.com>
2016-05-06 09:34:20 +0200

    ruby 2.3.1

--- scripts/restore_local_db.sh
+++ scripts/restore_local_db.sh
@@ -4,11 +4,11 @@ set -e
 echo "CLOSE ALL PROGRAMS USING THE DATABASE : Ruby web server, SQL client, etc"
 
 echo "snapshot production DB"
-heroku pg:backups capture -a entourage-back-preprod
+heroku pg:backups capture -a entourage-back
 echo "Reset DB"
 rake db:drop db:create
 echo "Download DB from production"
-curl -o tmp/db.dump `heroku pg:backups public-url -a entourage-back-preprod`
+curl -o tmp/db.dump `heroku pg:backups public-url -a entourage-back
 echo "Restore DB"
 pg_restore -h localhost -d entourage-dev tmp/db.dump
 echo "Restore test db"
