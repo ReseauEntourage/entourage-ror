9d697bd167a670d97fe69ef499ddc738d1c8ebeb
Vincent Daubry <vdaubry@gmail.com>
2016-06-14 14:56:34 +0200

    edit / create marketing referers

--- app/controllers/admin/marketing_referers_controller.rb
+++ app/controllers/admin/marketing_referers_controller.rb
@@ -1,7 +1,41 @@
 module Admin
   class MarketingReferersController < Admin::BaseController
+    before_action :set_marketing_referer, only: [:edit, :update]
+
     def index
       @marketing_referers = MarketingReferer.page(params[:page]).per(25)
+    end
+
+    def edit
+    end
+
+    def new
+      @marketing_referer = MarketingReferer.new
+    end
+
+    def create
+      @marketing_referer = MarketingReferer.new(marketing_referer_params)
+      if @marketing_referer.save
+        redirect_to admin_marketing_referers_path, notice: "marketing_referer créé"
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @marketing_referer.update(marketing_referer_params)
+        flash[:notice] = "marketing_referer mis à jour"
+      end
+      render :edit
+    end
+
+    private
+    def marketing_referer_params
+      params.require(:marketing_referer).permit(:name)
+    end
+
+    def set_marketing_referer
+      @marketing_referer = MarketingReferer.find(params[:id])
     end
   end
 end
