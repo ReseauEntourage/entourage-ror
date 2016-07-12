1a1e68198878c8cd6dbccb7be096ecc3210123f4
John Meiss <jonathan@meiss.fr>
2016-07-12 20:01:40 +0200

    fix(tours): users/tours.json charge overload

--- app/controllers/api/v0/users/tours_controller.rb
+++ app/controllers/api/v0/users/tours_controller.rb
@@ -7,7 +7,9 @@ module Api
         def index
           page = params[:page] || 1
           per = [(params[:per].try(:to_i) || 25), 25].min
-          tours = @user.tours.order(updated_at: :desc).page(page).per(per)
+          # tours = @user.tours.order(updated_at: :desc).page(page).per(per)
+          # @todo: John hot fix
+          tours = @user.tours.order(updated_at: :desc).page(page).per(25)
           render json: tours, status: 200, each_serializer: ::V0::TourSerializer
         end
 
@@ -20,4 +22,3 @@ module Api
     end
   end
 end
-
