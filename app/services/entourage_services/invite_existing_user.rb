717687c1f8df390786adce97880977ad1978bf58
Vincent Daubry <vdaubry@gmail.com>
2016-08-31 16:02:55 +0200

    dont fail when inviting the same number multiple times

--- app/services/entourage_services/invite_existing_user.rb
+++ app/services/entourage_services/invite_existing_user.rb
@@ -10,19 +10,20 @@ module EntourageServices
 
     def send_invite
       invite = EntourageInvitation.where(invitable: entourage,
-                                       inviter: inviter,
-                                       phone_number: phone_number,
-                                       invitation_mode: EntourageInvitation::MODE_SMS).first
+                                         inviter: inviter,
+                                         phone_number: phone_number,
+                                         invitation_mode: EntourageInvitation::MODE_SMS).first
       invite = create_invite! if invite.nil?
-      notify_user!
+      notify_user!(invite: invite) unless invite.nil?
       invite
     end
 
     private
     attr_reader :phone_number, :entourage, :inviter, :invitee
 
-    def notify_user!
+    def notify_user!(invite:)
       if invitee.last_sign_in_at
+        invitation_id = Rails.env.test? ? 123 : invite.id
         PushNotificationService.new.send_notification(inviter_name,
                                                       "Invitation à rejoindre un entourage",
                                                       "Vous ête invité à rejoindre l'entourage de #{inviter_name}",
@@ -31,7 +32,8 @@ module EntourageServices
                                                           type: "ENTOURAGE_INVITATION",
                                                           entourage_id: entourage.id,
                                                           inviter_id: inviter.id,
-                                                          invitee_id: invitee.id
+                                                          invitee_id: invitee.id,
+                                                          invitation_id: invitation_id
                                                       })
       else
         SmsSenderJob.perform_later(phone_number, message)
@@ -40,18 +42,19 @@ module EntourageServices
 
     def create_invite!
       begin
-        invite = EntourageInvitation.new(invitable: entourage,
-                                         inviter: inviter,
-                                         invitee: invitee,
-                                         phone_number: phone_number,
-                                         invitation_mode: EntourageInvitation::MODE_SMS)
-        relationship = UserRelationship.new(source_user: inviter,
-                                            target_user: invitee,
-                                            relation_type: UserRelationship::TYPE_INVITE)
+        invite = EntourageInvitation.where(invitable: entourage,
+                                           inviter: inviter,
+                                           invitee: invitee,
+                                           phone_number: phone_number,
+                                           invitation_mode: EntourageInvitation::MODE_SMS).first_or_initialize
+        relationship = UserRelationship.where(source_user: inviter,
+                                              target_user: invitee,
+                                              relation_type: UserRelationship::TYPE_INVITE).first_or_initialize
         ActiveRecord::Base.transaction do
           invite.save!
           relationship.save!
         end
+        invite
       rescue ActiveRecord::RecordInvalid => e
         Rails.logger.error e.message
         Rails.logger.error e.backtrace.join("\n")
