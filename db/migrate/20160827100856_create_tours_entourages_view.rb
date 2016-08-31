f60c55781f4d0e01e71cac98a63a2b3e131d4580
Vincent Daubry <vdaubry@gmail.com>
2016-08-31 17:37:54 +0200

    consolidate all column for feeds view

--- db/migrate/20160827100856_create_tours_entourages_view.rb
+++ db/migrate/20160827100856_create_tours_entourages_view.rb
@@ -3,30 +3,37 @@ class CreateToursEntouragesView < ActiveRecord::Migration
     sql = <<-SQL
       CREATE VIEW feeds AS
         SELECT id,
-          status,
-          title,
-          entourage_type,
-          user_id,
-          latitude,
-          longitude,
-          number_of_people,
-          created_at,
-          updated_at
-        FROM entourages
+        status,
+        title,
+        entourage_type,
+        user_id,
+        latitude,
+        longitude,
+        number_of_people,
+        created_at,
+        updated_at
+      FROM entourages
 
-        UNION ALL
+      UNION ALL
 
-        SELECT id,
-          '0' AS "status",
-          '' AS "TITLE",
-          tour_type,
-          user_id,
-          0 AS "latitude",
-          0 AS "longitude",
-          number_of_people,
-          created_at,
-          updated_at
-        FROM tours
+      SELECT
+        DISTINCT ON (id)
+        id,
+        CASE WHEN status=0 THEN 'ongoing' WHEN status=1 THEN 'closed' WHEN status=2 THEN 'freezed' END AS "status",
+        '' AS "TITLE",
+        tour_type,
+        user_id,
+        tp.longitude,
+        tp.latitude,
+        number_of_people,
+        created_at,
+        updated_at
+      FROM tours
+      LEFT OUTER JOIN (
+         SELECT latitude, longitude, tour_id
+         FROM tour_points
+      ) tp ON tp.tour_id = tours.id
+      ORDER BY id ASC
     SQL
 
     execute(sql)
