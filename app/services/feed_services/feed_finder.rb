901847e5c8a36bfe0ede7ddc8d92c6954fe89fa0
Vincent Daubry <vdaubry@gmail.com>
2016-09-11 17:43:46 +0200

    add timerange and before filter to feeds

--- app/services/feed_services/feed_finder.rb
+++ app/services/feed_services/feed_finder.rb
@@ -8,7 +8,8 @@ module FeedServices
                    entourage_types:,
                    tour_types:,
                    show_my_entourages_only: "false",
-                   show_my_tours_only: "false")
+                   show_my_tours_only: "false",
+                   time_range: 24)
       @user = user
       @page = page
       @per = per
@@ -17,13 +18,15 @@ module FeedServices
       @feed_type = join_types(entourage_types: entourage_types, tour_types: tour_types)
       @show_my_entourages_only = show_my_entourages_only=="true"
       @show_my_tours_only = show_my_tours_only=="true"
+      @time_range = (time_range || 24).to_i
     end
 
     def feeds
       feeds = Feed
 
-      feeds = feeds.where(feedable_type: "Entourage") unless show_tours=="true"
+      feeds = feeds.where(feedable_type: "Entourage") unless (show_tours=="true" && user.pro?)
       feeds = feeds.where(feed_type: feed_type) if feed_type
+      feeds = feeds.where("feeds.created_at > ?", time_range.hours.ago)
 
       if show_my_entourages_only && show_my_tours_only
         feeds = feeds.joins("INNER JOIN join_requests ON ((join_requests.joinable_type='Entourage' AND feeds.feedable_type='Entourage' AND join_requests.joinable_id=feeds.feedable_id) OR (join_requests.joinable_type='Tour' AND feeds.feedable_type='Tour' AND join_requests.joinable_id=feeds.feedable_id) AND join_requests.status='accepted')")
@@ -33,18 +36,19 @@ module FeedServices
         feeds = feeds.joins("INNER JOIN join_requests ON ((join_requests.joinable_type='Entourage' AND feeds.feedable_type='Entourage' AND join_requests.joinable_id=feeds.feedable_id) OR (join_requests.joinable_type='Tour' AND feeds.feedable_type='Tour' AND join_requests.joinable_id=feeds.feedable_id AND join_requests.status='accepted'))")
       end
 
-      if page || per
+      feeds = if page || per
         feeds.page(page).per(per)
       elsif before
         feeds.before(DateTime.parse(before)).limit(25)
       else
         feeds.limit(25)
       end
+
       feeds.order("updated_at DESC")
     end
 
     private
-    attr_reader :user, :page, :per, :before, :show_tours, :feed_type, :show_my_entourages_only, :show_my_tours_only
+    attr_reader :user, :page, :per, :before, :show_tours, :feed_type, :show_my_entourages_only, :show_my_tours_only, :time_range
 
     def join_types(entourage_types:, tour_types:)
       entourage_types = formated_type(entourage_types) || Entourage::ENTOURAGE_TYPES
