ddb38cc7477d93e0a261cb691b10637b61e5b9e5
Vincent Daubry <vdaubry@gmail.com>
2016-11-09 15:01:30 +0100

    disable sms sender retry

--- app/jobs/sms_sender_job.rb
+++ app/jobs/sms_sender_job.rb
@@ -1,5 +1,14 @@
-class SmsSenderJob < ActiveJob::Base
+class SmsSenderJob
+  #TODO: remove later. We use Sidekiq API to disable retries to debug user signup
+  include Sidekiq::Worker
+  sidekiq_options :retry => false
+
   def perform(phone, message)
     SmsNotificationService.new.send_notification(phone, message)
+  end
+
+  #Activejob adapter
+  def self.perform_later(phone, message)
+    SmsSenderJob.perform_async(phone, message)
   end
 end
