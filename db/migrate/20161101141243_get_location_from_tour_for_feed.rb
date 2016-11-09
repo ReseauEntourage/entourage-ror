3c1109e449b0bfcca19be83c77c6dba9309b8390
Vincent Daubry <vdaubry@gmail.com>
2016-11-09 10:53:49 +0100

    update feeds view

---
+++ db/migrate/20161101141243_get_location_from_tour_for_feed.rb
@@ -0,0 +1,80 @@
+class GetLocationFromTourForFeed < ActiveRecord::Migration
+  def up
+    sql = <<-SQL
+      CREATE OR REPLACE VIEW feeds AS
+        SELECT id AS "feedable_id",
+          'Entourage' AS "feedable_type",
+          status,
+          title,
+          entourage_type as "feed_type",
+          user_id,
+          latitude,
+          longitude,
+          number_of_people,
+          created_at,
+          updated_at
+        FROM entourages
+
+        UNION ALL
+
+        SELECT
+          distinct ON (id)
+          id AS "feedable_id",
+          'Tour' AS "feedable_type",
+          CASE WHEN status=0 THEN 'ongoing' WHEN status=1 THEN 'closed' WHEN status=2 THEN 'freezed' END AS "status",
+          '' AS "TITLE",
+          tour_type as "feed_type",
+          user_id,
+          latitude,
+          longitude,
+          number_of_people,
+          created_at,
+          updated_at
+        FROM tours
+    SQL
+
+    execute(sql)
+  end
+
+  def down
+    sql = <<-SQL
+      CREATE OR REPLACE VIEW feeds AS
+        SELECT id AS "feedable_id",
+          'Entourage' AS "feedable_type",
+          status,
+          title,
+          entourage_type as "feed_type",
+          user_id,
+          latitude,
+          longitude,
+          number_of_people,
+          created_at,
+          updated_at
+        FROM entourages
+
+        UNION ALL
+
+        SELECT
+          distinct ON (id)
+          id AS "feedable_id",
+          'Tour' AS "feedable_type",
+          CASE WHEN status=0 THEN 'ongoing' WHEN status=1 THEN 'closed' WHEN status=2 THEN 'freezed' END AS "status",
+          '' AS "TITLE",
+          tour_type as "feed_type",
+          user_id,
+          tp.latitude,
+          tp.longitude,
+          number_of_people,
+          created_at,
+          updated_at
+        FROM tours
+        LEFT OUTER JOIN (
+           SELECT latitude, longitude, tour_id
+           from tour_points
+        ) tp ON tp.tour_id = tours.id
+        ORDER by feedable_id ASC
+    SQL
+
+    execute(sql)
+  end
+end
--- lib/tasks/data_migration.rake
+++ lib/tasks/data_migration.rake
@@ -5,7 +5,7 @@ namespace :data_migration do
       UPDATE tours
       SET longitude = subquery.longitude,
           latitude = subquery.latitude
-      FROm (SELECT distinct ON (tour_id) tour_id, longitude, latitude FROM tour_points ORDER BY tour_id, passing_time ASC) AS subquery
+      FROM (SELECT distinct ON (tour_id) tour_id, longitude, latitude FROM tour_points ORDER BY tour_id, passing_time ASC) AS subquery
       WHERE tours.id = subquery.tour_id
     SQL
     ActiveRecord::Base.connection.execute(sql)
