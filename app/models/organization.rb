ffa2eb5c53d805971bd38a07dc5b239c87ffa06f
Chhim Stéphanie <stephaniechhim@gmail.com>
2016-12-09 12:51:04 +0100

    [Admin] Organization#index > displayed more data.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,4 +6,31 @@ class Organization < ActiveRecord::Base
 
   scope :not_test, -> { where test_organization: false}
   scope :ordered, -> { order("name ASC")}
+
+  def active_members_last_month
+    users.where('last_sign_in_at > ?', Time.current - 30.days).count
+  end
+
+  def last_tour_date
+    begin
+      Tour.joins(user: :organization).where(users: {organization_id: id}).order('created_at DESC')
+      .first
+      .created_at
+      .strftime('%d/%m/%Y')
+    rescue
+      "Pas encore de mauraude."
+    end
+  end
+
+  def tours_count
+    Tour.joins(user: :organization)
+      .where(users: {organization_id: id})
+      .count
+  end
+
+  def meetings_count
+    Encounter.joins(tour: :user)
+      .where(users: {organization_id: id})
+      .count
+  end
 end
