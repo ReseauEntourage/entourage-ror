35e46891635172d16d4e6e86b643bc4133a8c7c3
Vincent Daubry <vdaubry@gmail.com>
2017-01-04 06:11:36 +0100

    remove sentry

--- config/initializers/sentry.rb
+++
@@ -1,7 +0,0 @@
-require 'raven'
-
-Raven.configure do |config|
-  config.dsn = ENV["SENTRY_DNS"] if ENV["SENTRY_DNS"]
-  config.environments = %w[ production ]
-  config.excluded_exceptions = ['ActionController::ParameterMissing']
-end
