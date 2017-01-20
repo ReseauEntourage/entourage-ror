c3636043846cc9d1c6ffafd0a745dbc8490ab323
Chhim Stéphanie <stephaniechhim@gmail.com>
2017-01-20 17:33:21 +0100

    MemberMailer > added specific "from" per email.

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -2,9 +2,12 @@ class MemberMailer < ActionMailer::Base
   default from: "contact@entourage.social"
   add_template_helper(OrganizationHelper)
 
+  COMMUNITY_EMAIL   = ENV["COMMUNITY_EMAIL"]   || "communaute@entourage.social"
+  TOUR_REPORT_EMAIL = ENV["TOUR_REPORT_EMAIL"] || "maraudes@entourage.social"
+
   def welcome(user)
     @user = user
-    mail(to: user.email, subject: 'Bienvenue sur Entourage !')
+    mail(from: COMMUNITY_EMAIL, to: user.email, subject: 'Bienvenue sur Entourage !')
   end
 
   def tour_report(tour)
@@ -16,15 +19,15 @@ class MemberMailer < ActionMailer::Base
     attachments['tour_points.csv'] = File.read(exporter.export_tour_points)
     attachments['encounters.csv'] = File.read(exporter.export_encounters)
 
-    mail(to: @user.email, subject: 'Résumé de la maraude')
+    mail(from: TOUR_REPORT_EMAIL, to: @user.email, subject: 'Résumé de la maraude')
   end
-  
+
   def poi_report(poi, user, message)
     if ENV.key? "POI_REPORT_EMAIL"
       @poi = poi
       @user = user
       @message = message
-    
+
       mail(to: ENV["POI_REPORT_EMAIL"], subject: 'Correction de POI')
     else
       logger.warn "Could not deliver POI report. Please provide POI_REPORT_EMAIL as an environment variable".red
