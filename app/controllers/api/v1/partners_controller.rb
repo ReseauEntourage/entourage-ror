e571ac9979ae9e3976ddd3b6737cf351570bd81b
Vincent Daubry <vdaubry@gmail.com>
2017-01-24 23:31:57 +0100

    implement api endpoints

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class PartnersController < Api::V1::BaseController
-
       #curl -H "Content-Type: application/json" "http://localhost:3000/api/v1/partners?token=153ad0b7ef67e5c44b8ef5afc12709e4"
       def index
         @partners = Partner.page(params[:page]).per(50)
--- app/controllers/api/v1/users/partners_controller.rb
+++ app/controllers/api/v1/users/partners_controller.rb
@@ -6,52 +6,28 @@ module Api
 
         #curl -H "Content-Type: application/json" "http://localhost:3000/api/v1/users/93/partners?token=153ad0b7ef67e5c44b8ef5afc12709e4"
         def index
-          render json: {
-              "partners": [{
-                               "id": 1,
-                               "name": "ATD Quart Monde",
-                               "large_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/ATDQM-coul-V-fr.png",
-                               "small_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/Badge+image.png",
-                               "default": true
-                           },
-                           {
-                               "id": 2,
-                               "name": "Autre partner",
-                               "large_logo_url":"http://foo.com/bar.png",
-                               "small_logo_url":"http://foo.com/bar.png",
-                               "default": false
-                           }]
-          }, status: 200
+          @partners = current_user.partners.page(params[:page]).per(50)
+          render json: @partners, status: 200, each_serializer: ::V1::PartnerSerializer, scope: {user: current_user}
         end
 
         #curl -H "Content-Type: application/json" -X POST -d '{"partner" : { "id": 3 }}' "http://localhost:3000/api/v1/users/93/partners?token=153ad0b7ef67e5c44b8ef5afc12709e4"
         def create
-          render json: {
-              "partner": {
-                  "id": 1,
-                  "name": "ATD Quart Monde",
-                  "large_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/ATDQM-coul-V-fr.png",
-                  "small_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/Badge+image.png",
-                  "default": true
-              }
-          }, status: 201
+          partner = Partner.find(params[:partner][:id])
+          current_user.partners << partner
+          render json: partner, status: 201, serializer: ::V1::PartnerSerializer, scope: {user: current_user}
         end
 
         #curl -H "Content-Type: application/json" -X PUT -d '{"partner" : { "default": true }}' "http://localhost:3000/api/v1/users/93/partners/3?token=153ad0b7ef67e5c44b8ef5afc12709e4"
         def update
-          render json: {
-              "partner": {
-                  "id": 1,
-                  "name": "ATD Quart Monde",
-                  "large_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/ATDQM-coul-V-fr.png",
-                  "small_logo_url":"https://s3-eu-west-1.amazonaws.com/entourage-ressources/Badge+image.png",
-                  "default": true
-              }
-          }, status: 200
+          default = (params[:partner][:default].to_s == "true")
+          partner = current_user.partners.where(partners: {id: params[:id]}).first
+          current_user.user_partners.where(partner: partner).first.update(default: default)
+          render json: partner, status: 201, serializer: ::V1::PartnerSerializer, scope: {user: current_user}
         end
 
         #curl -H "Content-Type: application/json" -X DELETE "http://localhost:3000/api/v1/users/93/partners/3?token=153ad0b7ef67e5c44b8ef5afc12709e4"
         def destroy
+          current_user.user_partners.where(partner_id: params[:id]).destroy_all
           head status: 204
         end
 
@@ -60,7 +36,6 @@ module Api
         def set_user
           @user = User.find(params[:user_id])
         end
-
       end
     end
   end
