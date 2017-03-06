9309ffe5017be23eb238ffd2d6d242311066fd0b
François Pellissier <francois.pellissier@gmail.com>
2017-03-06 19:45:45 +0100

    feat(join request) exclude cancelled join requests for uniqueness

--- app/models/join_request.rb
+++ app/models/join_request.rb
@@ -10,7 +10,7 @@ class JoinRequest < ActiveRecord::Base
   belongs_to :joinable, polymorphic: true
 
   validates :user_id, :joinable_id, :joinable_type, :status, presence: true
-  validates_uniqueness_of :joinable_id, {scope: [:user_id], message: "a déjà été ajouté"}
+  validates_uniqueness_of :joinable_id, {scope: [:user_id], conditions: -> { where.not(status: CANCELLED_STATUS) }, message: "a déjà été ajouté"}
   validates_inclusion_of :status, in: ["pending", "accepted", "rejected", "cancelled"]
 
   scope :accepted, -> {where(status: ACCEPTED_STATUS)}
