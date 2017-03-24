508fcf8830d58850e0228278ea3c4fe9a916dfc9
François Pellissier <francois.pellissier@gmail.com>
2017-03-24 11:47:19 +0100

    fix(notif): fixed bugs sending ios jobs from ios service

--- app/services/ios_notification_service.rb
+++ app/services/ios_notification_service.rb
@@ -1,8 +1,6 @@
 class IosNotificationService
   def send_notification(sender, object, content, device_ids, extra={}, badge=nil)
     return if device_ids.blank?
-    device_ids.each do |device_token|
-      IosNotificationJob.perform_later(sender, object, content, device_token, extra, badge)
-    end
+    IosNotificationJob.perform_later(sender, object, content, device_ids, extra, badge)
   end
 end
