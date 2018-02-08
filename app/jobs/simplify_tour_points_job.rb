bc40d944c1ea35ea15a72e76b0ddd6e75c1283be
Grégoire Clermont <gregoire@entourage.social>
2018-02-08 17:18:56 +0100

    fix(simplified tour points): clear STP cache at the end of the tour

--- app/jobs/simplify_tour_points_job.rb
+++ app/jobs/simplify_tour_points_job.rb
@@ -2,7 +2,7 @@ class SimplifyTourPointsJob < ActiveJob::Base
 
   def perform(tour_id, should_send_mail=false)
     tour = Tour.find(tour_id)
-    tour_points = TourPointsServices::TourPointsSimplifier.new(tour_id: tour_id).simplified_tour_points
+    tour_points = TourPointsServices::TourPointsSimplifier.new(tour_id: tour_id).simplified_tour_points(clear_cache: true)
     tour.simplified_tour_points.destroy_all
     tour_points.each_with_index do |point, i|
       tour.simplified_tour_points.create(longitude: point["longitude"], latitude: point["latitude"], created_at: tour.created_at+i.seconds)
