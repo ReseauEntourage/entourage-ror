cd2d73d8e034016c1b75fbd4360be103318828dc
Robin Moisson <robin.moisson@gmail.com>
2018-03-18 18:49:46 +0200

    vagrant setup: put versions in variables

--- scripts/vagrant_setup.sh
+++ scripts/vagrant_setup.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+RUBY_VERSION="2.3.1"
+RUBY_INSTALL_VERSION="0.6.1"
+CHRUBY_VERSION="0.3.9"
+
+
 # install dependencies
 cat <<EOF | sudo tee /etc/default/locale
 LC_ALL=en_US.UTF-8
@@ -28,22 +33,22 @@ wget https://raw.github.com/postmodern/postmodern.github.io/master/postmodern.as
 gpg --import postmodern.asc
 
 # install ruby-install
-wget -q https://raw.github.com/postmodern/ruby-install/master/pkg/ruby-install-0.6.1.tar.gz.asc
-wget -qO ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
-gpg --verify ruby-install-0.6.1.tar.gz.asc ruby-install-0.6.1.tar.gz
-tar -xzvf ruby-install-0.6.1.tar.gz
-cd ruby-install-0.6.1/
+wget -q "https://raw.github.com/postmodern/ruby-install/master/pkg/ruby-install-$RUBY_INSTALL_VERSION.tar.gz.asc"
+wget -qO "ruby-install-$RUBY_INSTALL_VERSION.tar.gz" "https://github.com/postmodern/ruby-install/archive/v$RUBY_INSTALL_VERSION.tar.gz"
+gpg --verify "ruby-install-$RUBY_INSTALL_VERSION.tar.gz.asc" "ruby-install-$RUBY_INSTALL_VERSION.tar.gz"
+tar -xzvf "ruby-install-$RUBY_INSTALL_VERSION.tar.gz"
+cd "ruby-install-$RUBY_INSTALL_VERSION/"
 sudo make install
 
 # install pinned ruby version
-ruby-install ruby 2.3.1
+ruby-install ruby $RUBY_VERSION
 
 # install chruby
-wget -q https://raw.github.com/postmodern/chruby/master/pkg/chruby-0.3.9.tar.gz.asc
-wget -qO chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-gpg --verify chruby-0.3.9.tar.gz.asc chruby-0.3.9.tar.gz
-tar -xzvf chruby-0.3.9.tar.gz
-cd chruby-0.3.9/
+wget -q "https://raw.github.com/postmodern/chruby/master/pkg/chruby-$CHRUBY_VERSION.tar.gz.asc"
+wget -qO "chruby-$CHRUBY_VERSION.tar.gz" "https://github.com/postmodern/chruby/archive/v$CHRUBY_VERSION.tar.gz"
+gpg --verify "chruby-$CHRUBY_VERSION.tar.gz.asc" "chruby-$CHRUBY_VERSION.tar.gz"
+tar -xzvf "chruby-$CHRUBY_VERSION.tar.gz"
+cd "chruby-$CHRUBY_VERSION/"
 sudo make install
 echo 'source /usr/local/share/chruby/chruby.sh' >> ~/.bashrc
 echo 'chruby ruby' >> ~/.bashrc
