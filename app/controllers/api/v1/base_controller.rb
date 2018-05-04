7f4d19febb9ba4366fe20124936840c060eacc6d
Grégoire Clermont <gregoire@entourage.social>
2018-05-04 12:00:32 +0200

    tentative fix of cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -17,7 +17,7 @@ module Api
       def allow_cors
         headers["Access-Control-Allow-Origin"] = "*"
         headers["Access-Control-Allow-Methods"] = %w{GET POST PUT PATCH DELETE}.join(",")
-        headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token X-API-Auth-Token}.join(",")
+        headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token X-API-KEY}.join(",")
       end
 
       def options
