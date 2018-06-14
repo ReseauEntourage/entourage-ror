f1a4997b51bd2890a1443eda1fc5ce9f7d60e592
Grégoire Clermont <gregoire@entourage.social>
2018-06-14 11:44:51 +0200

    reduce log verbosity in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.active_record.logger.level = Logger::INFO
+
   # DEV / TEST CREDENTIALS
   ENV["BASIC_ADMIN_USER"] = "admin"
   ENV["BASIC_ADMIN_PASSWORD"] = "3nt0ur4g3"
