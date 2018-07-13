afe75d83e7c078d41a7b46ab8d90893fa68c113f
Grégoire Clermont <gregoire@entourage.social>
2018-07-13 12:27:20 +0200

    add sentry logging of Geocoder queries

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -12,15 +12,22 @@ Geocoder.configure(
 class Geocoder::Lookup::Base
   alias_method :_fetch_data, :fetch_data
 
-  def fetch_data(*args)
-    _fetch_data(*args).tap do |response|
-      begin
-        Raven.breadcrumbs.record do |crumb|
-          crumb.data = { response: response }
-          crumb.category = 'geocoder.response'
-        end
-      rescue
+  def fetch_data(query)
+    begin
+      Raven.breadcrumbs.record do |crumb|
+        crumb.data = { text: query.text, options: query.options }
+        crumb.category = 'geocoder.query'
       end
+    rescue
     end
+    response = _fetch_data(query)
+    begin
+      Raven.breadcrumbs.record do |crumb|
+        crumb.data = { response: response }
+        crumb.category = 'geocoder.response'
+      end
+    rescue
+    end
+    response
   end
 end
