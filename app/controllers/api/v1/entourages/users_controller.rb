fa013eef75fe34f0fea784a2b7832ffe68111882
Grégoire Clermont <gregoire@entourage.social>
2018-07-16 15:41:31 +0200

    enable join requests for outings

--- app/controllers/api/v1/entourages/users_controller.rb
+++ app/controllers/api/v1/entourages/users_controller.rb
@@ -159,7 +159,7 @@ module Api
         end
 
         def restrict_group_types!
-          unless ['action'].include?(@entourage.group_type)
+          unless ['action', 'outing'].include?(@entourage.group_type)
             render json: {message: "This operation is not available for groups of type '#{@entourage.group_type}'"}, status: :bad_request
           end
         end
