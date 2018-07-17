24f6542600f11836bbd8f3d03e8ef09e2820cd1c
Grégoire Clermont <gregoire@entourage.social>
2018-07-17 10:45:30 +0200

    fix newrelic app name in staging

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,11 @@ $server_community = Community.new ENV['COMMUNITY']
 # Can't be done in an initializer because New Relic starts before app initialization.
 # examples: "PFP API", "Entourage API (Development)"
 new_relic_app_name = "#{$server_community.dev_name} API"
-new_relic_app_name += " (#{Rails.env.capitalize})" unless Rails.env.production?
+if ENV['STAGING']
+  new_relic_app_name += " (Staging)"
+elsif !Rails.env.production?
+  new_relic_app_name += " (#{Rails.env.capitalize})"
+end
 ENV['NEW_RELIC_APP_NAME'] = new_relic_app_name
 
 # Require the gems listed in Gemfile, including any gems
