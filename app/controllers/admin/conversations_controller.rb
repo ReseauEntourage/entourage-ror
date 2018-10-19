06060df8e763422182c9351e0196c9a7672ce845
Grégoire Clermont <gregoire@entourage.social>
2018-10-19 17:10:46 +0200

    feat: bouton Contacter dans le profil utilisateur du back-office

--- app/controllers/admin/conversations_controller.rb
+++ app/controllers/admin/conversations_controller.rb
@@ -28,32 +28,46 @@ module Admin
 
     def show
       user = moderator
-      @conversation = user.entourage_participations.where(group_type: :conversation).find(params[:id])
-      @recipients = @conversation.members.where.not(id: moderator.id).merge(JoinRequest.accepted).select(:first_name, :last_name).to_a
+      @conversation = find_conversation params[:id], user: user
+
+      @recipients =
+        if @conversation.new_record?
+          User.where(id: @conversation.join_requests.map(&:user_id) - [moderator.id])
+        else
+          @conversation.members.where.not(id: moderator.id).merge(JoinRequest.accepted)
+        end
+
+      @recipients = @recipients.select(:id, :first_name, :last_name).to_a
       @chat_messages = @conversation.chat_messages.order(:created_at).includes(:user)
       @messages_author = moderator
     end
 
     def message
       user = moderator
-      @conversation = Entourage.find(params[:id])
-      join_request = user.join_requests.accepted.find_by!(joinable: @conversation)
+      conversation = find_conversation params[:id], user: user
+
+      join_request =
+        if conversation.new_record?
+          conversation.join_requests.to_a.find { |r| r.user_id == user.id }
+        else
+          user.join_requests.accepted.find_by!(joinable: conversation)
+        end
 
       chat_builder = ChatServices::ChatMessageBuilder.new(
         params: chat_messages_params,
         user: user,
-        joinable: @conversation,
+        joinable: conversation,
         join_request: join_request
       )
 
       chat_builder.create do |on|
         on.success do |message|
           join_request.touch(:last_message_read)
-          redirect_to admin_conversation_path(@conversation)
+          redirect_to admin_conversation_path(conversation)
         end
 
         on.failure do |message|
-          redirect_to admin_conversation_path(@conversation), alert: "Erreur lors de l'envoi du message : #{message.errors.full_messages.to_sentence}"
+          redirect_to admin_conversation_path(conversation), alert: "Erreur lors de l'envoi du message : #{message.errors.full_messages.to_sentence}"
         end
       end
     end
@@ -66,6 +80,18 @@ module Admin
 
     def chat_messages_params
       params.require(:chat_message).permit(:content)
+    end
+
+    def find_conversation id, user:
+      if ConversationService.list_uuid?(id)
+        participant_ids = ConversationService.participant_ids_from_list_uuid(params[:id])
+        raise ActiveRecord::RecordNotFound unless participant_ids.include?(user.id.to_s)
+        hash_uuid = ConversationService.hash_for_participants(participant_ids)
+        Entourage.find_by(uuid_v2: hash_uuid) ||
+          ConversationService.build_conversation(participant_ids: participant_ids)
+      else
+        user.entourage_participations.where(group_type: :conversation).find_by_id_or_uuid(params[:id])
+      end
     end
   end
 end
