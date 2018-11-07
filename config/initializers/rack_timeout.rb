b741569a9e051d1fe149c2c1a9f21b187c862541
Grégoire Clermont <gregoire@entourage.social>
2018-11-07 18:01:02 +0100

    reduce the log level of the rack-timeout gem

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,4 +1,5 @@
 if Rails.env.production?
   #Abort request that takes too long to execute, see https://devcenter.heroku.com/articles/request-timeout#timeout-behavior
   Rack::Timeout.service_timeout = ENV["RACK_TIMEOUT"].try(:to_i) || 5
+  Rack::Timeout::Logger.level  = Logger::WARN
 end
