a4e1a3a7745b5323e767abc5a6900fd4ac4feb68
Grégoire Clermont <gregoire@entourage.social>
2018-11-07 19:05:58 +0100

    disable CSRF protection for /api/v1/*

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      protect_from_forgery with: :null_session
+      skip_before_action :verify_authenticity_token
       before_filter :allow_cors
       before_filter :community_warning
       before_filter :validate_request!, only: [:check]
