e58af4b5768019975d6814f44e9cc7458451daac
Grégoire Clermont <gregoire@entourage.social>
2018-11-15 19:52:13 +0100

    check if tour_report has been delivered before deleting tour data

--- app/services/cleanup_service.rb
+++ app/services/cleanup_service.rb
@@ -10,14 +10,62 @@ class CleanupService
   end
 
   def self.remove_old_encounter_message
-    Encounter.where('created_at <= ?', 48.hours.ago)
+    encounters =
+      Encounter
+        .where('created_at <= ?', 48.hours.ago)
         .where('encrypted_message IS NOT NULL OR '\
                'street_person_name IS NOT NULL OR '\
                'latitude IS NOT NULL')
-        .update_all(encrypted_message: nil,
-                    street_person_name: nil,
-                    latitude: nil,
-                    longitude:nil,
-                    address: nil)
+
+    tour_ids = encounters.uniq.pluck(:tour_id)
+
+    bins = {
+      delete: [],
+      keep:   []
+    }
+
+    tour_ids.each do |tour_id|
+      report_emails = Mailjet::Message.all(custom_id: "tour_report-#{tour_id}")
+      email_status = Hash[report_emails.map { |m| [m.id, m.status] }]
+
+      decision =
+        if (email_status.values & ['sent', 'opened', 'clicked']).any?
+          :delete
+        else
+          :keep
+        end
+
+      Rails.logger.info JSON.fast_generate(
+        type: 'cleanup_service.remove_old_encounter_message',
+        tour_id: tour_id,
+        decision: decision,
+        email_status: email_status
+      )
+
+      bins[decision].push tour_id
+    end
+
+    encounters
+      .where(tour_id: bins[:delete])
+      .update_all(encrypted_message: nil,
+                  street_person_name: nil,
+                  latitude: nil,
+                  longitude:nil,
+                  address: nil)
+
+    send_slack_alert(bins[:keep]) if bins[:keep].any?
+  end
+
+  def self.send_slack_alert tour_ids
+    return if ENV['SLACK_WEBHOOK_URL'].blank?
+
+    Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping(
+      text: "*Le compte-rendu de maraude n'a pas été délivré* après 48 h " \
+            "pour les maraudes suivantes : _#{tour_ids.to_sentence}_.\n" \
+            "Les données n'ont donc pas été effacées.",
+      channel: '#mailjet-errors',
+      username: 'Maraudes',
+      icon_emoji: ':memo:'
+    )
   end
 end
