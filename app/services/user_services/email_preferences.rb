4163bcbd52c44343cd0adfe8e5c630322e79b63a
Grégoire Clermont <gregoire@entourage.social>
2018-11-29 16:34:12 +0100

    synchronize MailChimp status when a user updates their email preferences

--- app/services/user_services/email_preferences.rb
+++ app/services/user_services/email_preferences.rb
@@ -8,5 +8,44 @@ module UserServices
         protocol: :https
       )
     end
+
+    def self.enable_callback?
+      !Rails.env.test?
+    end
+
+    def self.sync_mailchimp_subscription_status user
+      if user.accepts_emails
+        MailchimpService.update(
+          :newsletter, user.email,
+          status: :subscribed
+        ) rescue MailchimpService::ResourceNotFound
+      else
+        MailchimpService.add_or_update(
+          :newsletter, user.email,
+          status: :unsubscribed,
+          status_if_new: :unsubscribed,
+          unsubscribe_reason: "via le site"
+        ) rescue MailchimpService::ForgottenEmailNotSubscribed
+      end
+    rescue MailchimpService::ConfigError => e
+      Raven.capture_exception(e)
+    end
+
+    module Callback
+      extend ActiveSupport::Concern
+
+      included do
+        after_commit :sync_mailchimp_subscription_status_async
+      end
+
+      private
+
+      def sync_mailchimp_subscription_status_async
+        return unless UserServices::EmailPreferences.enable_callback?
+        return unless previous_changes.keys.include?('accepts_emails')
+        AsyncService.new(UserServices::EmailPreferences)
+          .sync_mailchimp_subscription_status(self)
+      end
+    end
   end
 end
---
+++ spec/services/user_services/email_preferences_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe UserServices::EmailPreferences do
+  let(:service) { UserServices::EmailPreferences }
+  before do
+    service.stub(:enable_callback?) { true }
+    allow(MailchimpService).to receive(:update)
+    allow(MailchimpService).to receive(:add_or_update)
+  end
+
+  context "when a user unsubscribes" do
+    let(:user) { create :public_user, accepts_emails: true }
+    it do
+      user.update(accepts_emails: false)
+
+      expect(MailchimpService)
+        .to have_received(:add_or_update)
+        .with(
+          :newsletter, user.email,
+          status: :unsubscribed,
+          status_if_new: :unsubscribed,
+          unsubscribe_reason: "via le site"
+        )
+    end
+  end
+
+  context "when a user subscribes" do
+    let(:user) { create :public_user, accepts_emails: false }
+    it do
+      user.update(accepts_emails: true)
+
+      expect(MailchimpService)
+        .to have_received(:update)
+        .with(
+          :newsletter, user.email,
+          status: :subscribed
+        )
+    end
+  end
+end
