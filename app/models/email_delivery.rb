e55c3ff8290ac26f3d221021bb7f6a22588dd56e
Grégoire Clermont <gregoire@entourage.social>
2019-01-08 18:10:25 +0100

    finish migration

--- app/models/email_delivery.rb
+++ app/models/email_delivery.rb
@@ -1,14 +1,8 @@
 class EmailDelivery < ActiveRecord::Base
   belongs_to :user
-
-  if EmailDelivery.column_names.include?('campaign')
-    validates_presence_of :user_id, :campaign, :sent_at
-    validates_numericality_of :user_id
-  else
-    belongs_to :campaign, class_name: :EmailCampaign, foreign_key: :email_campaign_id
-    validates_presence_of :user_id, :email_campaign_id, :sent_at
-    validates_numericality_of :user_id, :email_campaign_id
-  end
+  belongs_to :campaign, class_name: :EmailCampaign, foreign_key: :email_campaign_id
+  validates_presence_of :user_id, :email_campaign_id, :sent_at
+  validates_numericality_of :user_id, :email_campaign_id
 
   scope :for_campaign, ->(name) { joins(:campaign).where(email_campaigns: {name: name}) }
 
