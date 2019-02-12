cff3e8fc4eb79b1ed54a43c937f3a69488dcfd8c
François Pellissier <francois.pellissier@gmail.com>
2019-02-12 12:36:41 +0100

    chore(sms): EN-765 remove sinch as SMS provider

--- app/services/sms_notification_service.rb
+++ app/services/sms_notification_service.rb
@@ -1,16 +1,11 @@
 class SmsNotificationService
-  def initialize(notification_pusher = nil)
-    @notification_pusher = notification_pusher
-  end
-
   def send_notification(phone_number, message, sms_type)
-    if(ENV['SMS_PROVIDER']=='SINCH')
-      send_sinch_sms(phone_number, message)
-    else
+    if(ENV['SMS_PROVIDER']=='AWS')
       send_aws_sms(phone_number, message, sms_type)
     end
   end
 
+  private
   def send_aws_sms(phone_number, message, sms_type)
     unless ENV.key?('SMS_SENDER_NAME')
       Rails.logger.warn 'No SMS has been sent. Please provide SMS_SENDER_NAME environment variables'
@@ -42,26 +37,5 @@ class SmsNotificationService
       deliveryState='Sending Error'
     end
     SmsDelivery.create(phone_number: phone_number, status: deliveryState, sms_type: sms_type)
-  end
-
-  def send_sinch_sms(phone_number, message)
-    unless ENV.key?('SINCH_API_KEY') && ENV.key?('SINCH_API_SECRET')
-      Rails.logger.warn 'No SMS has been sent. Please provide SINCH_API_KEY and SINCH_API_SECRET environment variables'
-      return
-    end
-
-    response = notification_pusher.send(ENV['SINCH_API_KEY'], ENV['SINCH_API_SECRET'], message, phone_number)
-
-    if !response.is_a?(Hash) || response.key?('errorCode')
-      Rails.logger.info "Error trying to send SMS to #{phone_number} response=#{response.inspect}"
-    else
-      Rails.logger.info "Sent SMS to #{phone_number} response=#{response.inspect}"
-    end
-  end
-
-  private
-
-  def notification_pusher
-    @notification_pusher ||= SinchSms
   end
 end
--- spec/services/sms_notification_service_spec.rb
+++ spec/services/sms_notification_service_spec.rb
@@ -5,24 +5,11 @@ describe SmsNotificationService do
     let!(:message) { 'message' }
     let!(:phone_number) { 'phone number' }
     let!(:sms_type) { 'welcome' }
-    let!(:sinch_mock) { double('sinch') }
-    let!(:sms_notification_service) { SmsNotificationService.new sinch_mock }
+    let!(:sms_notification_service) { SmsNotificationService.new}
     # create a stubbed client instance (mock the one in our code), and stub data
     let(:stub_client) { Aws::SNS::Client.new(stub_responses: true) }
 
-    context "sinch as sms provider and api key and secret are provided as env variables" do
-      before do
-        ENV["SMS_PROVIDER"] = "SINCH"
-        ENV["SINCH_API_KEY"] = "key"
-        ENV["SINCH_API_SECRET"] = "secret"
-        # needed since "send" is an object method
-        sinch_mock.stub(:send)
-        sms_notification_service.send_notification(phone_number, message, sms_type)
-      end
-      it { expect(sinch_mock).to have_received(:send).with('key', 'secret', message, phone_number) }
-    end
-
-    context "aws as sms provider and api key and secret are provided as env variables" do
+    context "api key and secret are provided as env variables" do
       before do
         ENV["SMS_PROVIDER"] = "AWS"
         ENV["SMS_SENDER_NAME"] = "Entourage"
@@ -32,17 +19,6 @@ describe SmsNotificationService do
         sms_notification_service.send_notification(phone_number, message, sms_type)
       end
       it { expect(stub_client).to have_received(:publish).with({phone_number: phone_number, message: message}) }
-    end
-
-    context "sinch as sms provider and api key and secret are not provided as env variables" do
-      before do
-        ENV["SMS_PROVIDER"] = "SINCH"
-        ENV.delete("SINCH_API_KEY")
-        ENV.delete("SINCH_API_SECRET")
-        Rails.logger.stub(:warn)
-        sms_notification_service.send_notification(phone_number, message, sms_type)
-      end
-      it { expect(Rails.logger).to have_received(:warn).with('No SMS has been sent. Please provide SINCH_API_KEY and SINCH_API_SECRET environment variables') }
     end
 
     context "aws as sms provider and api key and secret are not provided as env variables" do
