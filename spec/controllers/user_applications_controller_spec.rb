766f154f1ce08149666c0018845aad2b84bb701c
François Pellissier <francois.pellissier@gmail.com>
2019-02-25 10:39:38 +0100

    feature(logout): add an API endpoint for deleting push_token when logging out

--- spec/controllers/user_applications_controller_spec.rb
+++ spec/controllers/user_applications_controller_spec.rb
@@ -46,23 +46,7 @@ RSpec.describe Api::V1::UserApplicationsController, type: :controller do
         before { put :update, application: {push_token: "foobar", device_os: "ios", version: "1.1"}, token: user.token }
         it { expect(UserApplication.count).to eq(1) }
       end
-
-      context "remove ios application token (old way)" do
-        let!(:ios_user_application_old) { FactoryGirl.create(:user_application, user: user, push_token: "old_token2", device_os: "ios", version: "1.0") }
-        let!(:ios_user_application) { FactoryGirl.create(:user_application, user: user, push_token: "old_token", device_os: "ios", version: "1.1") }
-        before { put :update, application: {push_token: "0", device_os: "ios", version: "1.1"}, token: user.token }
-        it { expect(user.user_applications.count).to eq(1) }
-        it { expect(user.user_applications.last.push_token).to eq("old_token2") }
-      end
-
-      context "remove android application token (old way)" do
-        let!(:ios_user_application_old) { FactoryGirl.create(:user_application, user: user, push_token: "old_token2", device_os: "android", version: "1.0") }
-        let!(:ios_user_application) { FactoryGirl.create(:user_application, user: user, push_token: "old_token", device_os: "android", version: "1.1") }
-        before { put :update, application: {push_token: "", device_os: "android", version: "1.1"}, token: user.token }
-        it { expect(user.user_applications.count).to eq(1) }
-        it { expect(user.user_applications.last.push_token).to eq("old_token2") }
-      end
-
+      
       context "remove ios application token" do
         let!(:ios_user_application_old) { FactoryGirl.create(:user_application, user: user, push_token: "old_token2", device_os: "ios", version: "1.0") }
         let!(:ios_user_application) { FactoryGirl.create(:user_application, user: user, push_token: "old_token", device_os: "ios", version: "1.1") }
