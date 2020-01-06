04794c86817f06c981cd6d125af956ddbf1fdc3e
gregclermont <580609+gregclermont@users.noreply.github.com>
2020-01-06 10:40:01 +0100

    disable iraiser slack webhook

--- app/controllers/iraiser_controller.rb
+++ app/controllers/iraiser_controller.rb
@@ -1,5 +1,7 @@
 class IraiserController < ActionController::Base
   def notification
+    render nothing: true
+
     raw_headers = Hash[env.select { |k, _| k.starts_with?('HTTP_') }.map { |k, v| [k[5..-1].tr('_', '-').capitalize, v] }]
     AsyncService.new(IraiserWebhookService).handle_notification(
       raw_headers,
