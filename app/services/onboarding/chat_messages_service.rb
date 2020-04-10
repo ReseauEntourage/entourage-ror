c7ad0e5ba916f6a991a78021047492d87a40e39d
gregclermont <580609+gregclermont@users.noreply.github.com>
2020-04-10 12:12:21 +0200

    numéro du standard dans le DM auto de Guillaume

--- app/services/onboarding/chat_messages_service.rb
+++ app/services/onboarding/chat_messages_service.rb
@@ -16,7 +16,7 @@ module Onboarding
         Bonjour #{first_name},
         Bienvenue sur le réseau Entourage !
         Je m’appelle Guillaume, je m’occupe de l’accompagnement : je réponds à vos questions, je vous oriente, et veille au respect de la charte éthique de l’association.
-        De belles actions n’attendent que vous sur le réseau... qui promettent de belles rencontres ! Et si vous avez la moindre question, contactez moi directement ici par message.
+        De belles actions n’attendent que vous sur le réseau... qui promettent de belles rencontres ! Et si vous avez la moindre question, contactez moi directement ici par message, ou par téléphone au 01 88 24 70 70.
         À bientôt !
       MESSAGE
 
