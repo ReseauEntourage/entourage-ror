f1206ac6ef420c0a96e86068ba26876f3741a969
gregclermont <580609+gregclermont@users.noreply.github.com>
2020-05-05 16:39:49 +0200

    admin zones de modé

---
+++ app/controllers/admin/moderation_areas_controller.rb
@@ -0,0 +1,33 @@
+module Admin
+  class ModerationAreasController < Admin::BaseController
+    layout 'admin_large'
+
+    def index
+      @areas = ModerationArea.order(:id).all
+    end
+
+    def edit
+      @area = ModerationArea.find(params[:id])
+    end
+
+    def update
+      @area = ModerationArea.find(params[:id])
+
+      @area.assign_attributes(area_params)
+
+      if @area.save
+        redirect_to edit_admin_moderation_area_path(@area), notice: "Zone mise à jour"
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def area_params
+      params.require(:moderation_area).permit(
+        :moderator_id, :welcome_message_1, :welcome_message_2
+      )
+    end
+  end
+end
