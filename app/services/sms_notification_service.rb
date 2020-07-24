415b9c623f56b8079b13b1f4a37c6492c288c94c
gregclermont <580609+gregclermont@users.noreply.github.com>
2020-07-24 13:04:49 +0200

    add Nexmo as a secondary SMS provider

--- app/services/sms_notification_service.rb
+++ app/services/sms_notification_service.rb
@@ -6,14 +6,28 @@ class SmsNotificationService
     end
 
     provider = ENV['SMS_PROVIDER']
+    secondary_provider = ENV['SMS_PROVIDER_SECONDARY'].presence
 
     if audit_phone_number?(phone_number)
       provider = 'Slack'
     end
 
+    if secondary_provider != nil
+      number_of_deliveries_in_the_last_hour = SmsDelivery.where(
+        phone_number: phone_number,
+        created_at: 1.hour.ago..Time.now
+      ).count
+
+      if number_of_deliveries_in_the_last_hour.odd?
+        provider = secondary_provider
+      end
+    end
+
     case provider
     when 'AWS'
       deliveryState = send_aws_sms(phone_number, message, sms_type)
+    when 'Nexmo'
+      deliveryState = send_nexmo_sms(phone_number, message, sms_type)
     when 'Slack'
       deliveryState = send_slack_message(phone_number, message, sms_type)
     when 'logs'
@@ -45,13 +59,38 @@ class SmsNotificationService
       })
 
       if response.message_id
-        Rails.logger.info "Sent SMS to #{phone_number} response=#{response.inspect}"
+        Rails.logger.info "Sent SMS to #{phone_number} provider=AWS response=#{response.inspect}"
       else
-        Rails.logger.info "Error trying to send SMS to #{phone_number} response=#{response.inspect}"
+        Rails.logger.info "Error trying to send SMS to #{phone_number} provider=AWS response=#{response.inspect}"
         deliveryState='Provider Error'
       end
     rescue  => e
-      Rails.logger.error "Error trying to send SMS to #{phone_number} error=#{e.message}"
+      Rails.logger.error "Error trying to send SMS to #{phone_number} provider=AWS error=#{e.message}"
+      deliveryState='Sending Error'
+    end
+    return deliveryState
+  end
+
+  def send_nexmo_sms(phone_number, message, sms_type)
+    deliveryState = 'Ok'
+    begin
+      nexmo = Nexmo::Client.new(
+        api_key: ENV['NEXMO_API_KEY'],
+        api_secret: ENV['NEXMO_API_SECRET']
+      )
+
+      response = nexmo.sms.send(
+        to: phone_number,
+        text: message,
+        from: ENV['SMS_SENDER_NAME'],
+      )
+
+      Rails.logger.info "Sent SMS to #{phone_number} provider=Nexmo response=#{response.messages.first.to_h.to_json}"
+    rescue Nexmo::Error => e
+      Rails.logger.info "Error trying to send SMS to #{phone_number} provider=Nexmo response=#{e.message.inspect}"
+      deliveryState='Provider Error'
+    rescue  => e
+      Rails.logger.error "Error trying to send SMS to #{phone_number} provider=Nexmo error=#{e.message.inspect}"
       deliveryState='Sending Error'
     end
     return deliveryState
