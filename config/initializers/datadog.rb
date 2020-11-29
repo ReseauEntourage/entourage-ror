f8b6094f5668ce4ce083ea45b706b43cba2fcd32
Paul Vidal <paul.vidal@yahoo.fr>
2020-11-29 14:01:43 +0100

    only run in staging or production

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,8 @@
 # https://docs.datadoghq.com/tracing/setup_overview/setup/ruby/#quickstart-for-rails-applications
 
-Datadog.configure do |c|
-  # This will activate auto-instrumentation for Rails
-  c.use :rails, { "analytics_enabled" => true, "service_name" => "entourage-backend", "log_injection" => true }
+if EnvironmentHelper.staging? || EnvironmentHelper.production?
+  Datadog.configure do |c|
+    # This will activate auto-instrumentation for Rails
+    c.use :rails, { "analytics_enabled" => true, "service_name" => "entourage-backend", "log_injection" => true }
+  end
 end
