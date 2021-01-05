f38d073ea2cbe115745dcdbb8372c5ae01b6f187
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-01-05 17:26:10 +0100

    EN-1295 Handling admin redirect when domain is admin

--- app/services/environment_helper.rb
+++ app/services/environment_helper.rb
@@ -11,4 +11,16 @@ module EnvironmentHelper
   def self.test?;        env == :test;        end
   def self.staging?;     env == :staging;     end
   def self.production?;  env == :production;  end
+
+  def self.is_host? host, request
+    request.host_with_port == {
+      :default => ENV['HOST'],
+      :api => ENV['API_HOST'],
+      :admin => ENV['ADMIN_HOST']
+    }[host.to_sym]
+  end
+
+  def self.default_host? request; is_host? :default, request; end
+  def self.api_host?     request; is_host? :api, request;     end
+  def self.admin_host?   request; is_host? :admin, request;   end
 end
