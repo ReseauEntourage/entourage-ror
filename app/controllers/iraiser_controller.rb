85f8d2022551ecf5314324dcb6f4a8e63fd39558
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-01-07 15:01:37 +0100

    Comments

--- app/controllers/iraiser_controller.rb
+++ app/controllers/iraiser_controller.rb
@@ -1,4 +1,6 @@
 class IraiserController < ActionController::Base
+  # deprecated on a iraiser hook
+  # @see https://entourage.iraiser.eu/manager.php/manager/settings_push/edit on webhook
   def notification
     return render nothing: true
 
--- app/controllers/mailjet_controller.rb
+++ app/controllers/mailjet_controller.rb
@@ -1,4 +1,6 @@
 class MailjetController < ActionController::Base
+  # @see https://app.mailjet.com/account/triggers
+  # a hook on mailjet sends notification using this controller to notify that a user has unsuscribe
   def event
     events = params[:_json] || []
     events.each do |event|
