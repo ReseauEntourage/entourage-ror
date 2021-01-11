9f8284a45eb251d816afd59152826b7e06bb22e3
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-01-11 17:01:39 +0100

    EN-3328 dpt becomes dpts to allow multiple departement filter

--- app/services/airtable/bonnes_ondes_restart_abstract_table.rb
+++ app/services/airtable/bonnes_ondes_restart_abstract_table.rb
@@ -7,36 +7,39 @@ module Airtable
 
     self.base_key = ENV['AIRTABLE_BONNES_ONDES_RESTART']
 
-    def self.by_dpt_and_stade dpt, stade
-      all(filter: "AND({Stade ?} = '#{stade}', {Dépt} = '#{dpt}')")
+    def self.by_dpt_and_stade dpts, stade
+      dpts = dpts.map do |dpt|
+        "{Dépt} = '#{dpt}'"
+      end.join(', ')
+
+      all(filter: "AND({Stade ?} = '#{stade}', OR(#{dpts}))")
     end
 
-    def self.from_airtable dpt, stade
+    def self.from_airtable dpts, stade
       # map = self.map
       # by_dpt_and_stade(dpt, stade).map(&:fields).map do |entoure|
       #   entoure.slice(map[:mobile], map[:email], map[:lastname], map[:firstname])
       # end.map(&:values)
-      by_dpt_and_stade(dpt, stade).map(&:fields).map(&:values)
+      by_dpt_and_stade(dpts, stade).map(&:fields).map(&:values)
     end
 
-    def self.to_slack channel, url, dpt, stade
+    def self.to_slack channel, url, dpts, stade
       Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping(
         channel: channel,
         username: USERNAME,
-        text: "Export du #{Date.today} pour le scope #{dpt}, #{stade}",
+        text: "Export du #{Date.today} pour le scope #{dpts}, #{stade}",
         attachments: [{
           text: url
         }]
       )
     end
 
-    def self.upload channel, dpt, stade
-      # Dir.mkdir(DEFAULT_PATH) unless Dir.exist?(DEFAULT_PATH)
+    def self.upload channel, dpts, stade
+      ensure_directory_exist
+      file = path dpts
 
-      file = "#{DEFAULT_PATH}/#{Date.today}-#{dpt}-#{Time.now.to_i}.csv"
-
-      CSV.open(file, 'w+', write_headers: true, headers: self.headers) do |writer|
-        from_airtable(dpt, stade).each do |csv|
+      CSV.open(file, 'w+', write_headers: true, headers: headers) do |writer|
+        from_airtable(dpts, stade).each do |csv|
           writer << csv
         end
       end
@@ -44,7 +47,17 @@ module Airtable
       Storage::Client.csv.upload(file: file, key: file)
       url = Storage::Client.csv.url_for(key: file)
 
-      to_slack channel, url, dpt, stade
+      to_slack channel, url, dpts, stade
+    end
+
+    private
+
+    def self.ensure_directory_exist
+      Dir.mkdir(DEFAULT_PATH) unless Dir.exist?(DEFAULT_PATH)
+    end
+
+    def self.path dpts
+      "#{DEFAULT_PATH}/#{Date.today}-#{dpts.join('-')}-#{Time.now.to_i}.csv"
     end
   end
 end
