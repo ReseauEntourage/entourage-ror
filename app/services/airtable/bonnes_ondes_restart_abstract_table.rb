8c559f252cd9892505bb1751089ee6e04acac3a1
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-01-12 17:00:56 +0100

    EN-3328 Create a task to export airtable data
    Task should be run like: bin/d exec bin/rake airtable:export\['#test-nicolas','75 78 92','1.a A matcher'\]

--- app/services/airtable/bonnes_ondes_restart_abstract_table.rb
+++ app/services/airtable/bonnes_ondes_restart_abstract_table.rb
@@ -2,9 +2,6 @@ Airrecord.api_key = ENV['AIRTABLE_API_KEY']
 
 module Airtable
   class BonnesOndesRestartAbstractTable < Airrecord::Table
-    DEFAULT_PATH = "#{Rails.root}/tmp"
-    USERNAME = 'Airtable-backend'
-
     self.base_key = ENV['AIRTABLE_BONNES_ONDES_RESTART']
 
     def self.by_dpt_and_stade dpts, stade
@@ -21,43 +18,6 @@ module Airtable
       #   entoure.slice(map[:mobile], map[:email], map[:lastname], map[:firstname])
       # end.map(&:values)
       by_dpt_and_stade(dpts, stade).map(&:fields).map(&:values)
-    end
-
-    def self.to_slack channel, url, dpts, stade
-      Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping(
-        channel: channel,
-        username: USERNAME,
-        text: "Export du #{Date.today} pour le scope #{dpts}, #{stade}",
-        attachments: [{
-          text: url
-        }]
-      )
-    end
-
-    def self.upload channel, dpts, stade
-      ensure_directory_exist
-      file = path dpts
-
-      CSV.open(file, 'w+', write_headers: true, headers: headers) do |writer|
-        from_airtable(dpts, stade).each do |csv|
-          writer << csv
-        end
-      end
-
-      Storage::Client.csv.upload(file: file, key: file)
-      url = Storage::Client.csv.url_for(key: file)
-
-      to_slack channel, url, dpts, stade
-    end
-
-    private
-
-    def self.ensure_directory_exist
-      Dir.mkdir(DEFAULT_PATH) unless Dir.exist?(DEFAULT_PATH)
-    end
-
-    def self.path dpts
-      "#{DEFAULT_PATH}/#{Date.today}-#{dpts.join('-')}-#{Time.now.to_i}.csv"
     end
   end
 end
