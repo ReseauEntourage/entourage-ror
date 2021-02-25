778d9c1bb61291d5c6e93fe240879dcb75fa5628
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-02-25 16:18:15 +0100

    Refactor

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -4,29 +4,62 @@ module Api
       skip_before_filter :community_warning
 
       def index
-        # per is defined in BaseController
-        page = params[:page] || 1
+        render json: {
+          metadata: {
+            order: [:announcements, :outings, :entourages, :tours],
+          },
 
-        announcements = FeedServices::AnnouncementsService.announcements_for_user(current_user)
+          announcements: ::ActiveModel::ArraySerializer.new(
+            get_announcements,
+            each_serializer: ::V1::AnnouncementSerializer,
+            scope: { user: current_user, base_url: request.base_url }
+          ),
 
-        if params[:latitude] && params[:longitude]
-          outings = FeedServices::OutingsFinder.new(
-            user: current_user,
-            latitude: params[:latitude],
-            longitude: params[:longitude],
-            starting_after: params[:starting_after]
-          ).feeds
-        else
-          outings = []
-        end
+          outings: ::ActiveModel::ArraySerializer.new(
+            get_outings,
+            each_serializer: ::V1::EntourageSerializer,
+            scope: { user: current_user }
+          ),
 
-        entourages = EntourageServices::EntourageFinder.new(
+          entourages: ::ActiveModel::ArraySerializer.new(
+            get_entourages,
+            each_serializer: ::V1::EntourageSerializer,
+            scope: { user: current_user }
+          ),
+
+          tours: ::ActiveModel::ArraySerializer.new(
+            get_tours,
+            each_serializer: ::V1::TourSerializer,
+            scope: { user: current_user }
+          )
+        }.to_json, status: 200
+      end
+
+      private
+
+      def get_announcements
+        FeedServices::AnnouncementsService.announcements_for_user(current_user)
+      end
+
+      def get_outings
+        return [] unless params[:latitude] && params[:longitude]
+
+        FeedServices::OutingsFinder.new(
+          user: current_user,
+          latitude: params[:latitude],
+          longitude: params[:longitude],
+          starting_after: params[:starting_after]
+        ).feeds.map(&:feedable)
+      end
+
+      def get_entourages
+        EntourageServices::EntourageFinder.new(
           user: current_user,
           types: params[:types] || params[:entourage_types],
           latitude: params[:latitude],
           longitude: params[:longitude],
           distance: params[:distance],
-          page: page,
+          page: params[:page],
           per: per,
           show_past_events: params[:show_past_events],
           time_range: params[:time_range],
@@ -34,52 +67,22 @@ module Api
           partners_only: params[:partners_only],
           no_outings: true
         ).entourages
+      end
 
-        if current_user.public?
-          tours = []
-        else
-          tours = TourServices::TourFilterApi.new(
-            user: current_user,
-            status: params[:status],
-            type: params[:type] || params[:tour_type],
-            vehicle_type: params[:vehicle_type],
-            latitude: params[:latitude],
-            longitude: params[:longitude],
-            distance: params[:distance],
-            page: page,
-            per: per
-          ).tours
-        end
+      def get_tours
+        return [] if current_user.public?
 
-        render json: {
-          metadata: {
-            order: [:announcements, :outings, :entourages, :tours],
-          },
-
-          announcements: ::ActiveModel::ArraySerializer.new(
-            announcements,
-            each_serializer: ::V1::AnnouncementSerializer,
-            scope: { user: current_user, base_url: request.base_url }
-          ),
-
-          outings: ::ActiveModel::ArraySerializer.new(
-            outings.map(&:feedable), # requires OutingsFinder refactor on Object mapping
-            each_serializer: ::V1::EntourageSerializer,
-            scope: { user: current_user }
-          ),
-
-          entourages: ::ActiveModel::ArraySerializer.new(
-            entourages,
-            each_serializer: ::V1::EntourageSerializer,
-            scope: { user: current_user }
-          ),
-
-          tours: ::ActiveModel::ArraySerializer.new(
-            tours,
-            each_serializer: ::V1::TourSerializer,
-            scope: { user: current_user }
-          )
-        }.to_json, status: 200
+        TourServices::TourFilterApi.new(
+          user: current_user,
+          status: params[:status],
+          type: params[:type] || params[:tour_type],
+          vehicle_type: params[:vehicle_type],
+          latitude: params[:latitude],
+          longitude: params[:longitude],
+          distance: params[:distance],
+          page: params[:page],
+          per: per
+        ).tours
       end
     end
   end
