a13842514202fc59c72385a0177a0bed7c25d17e
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-03-16 16:07:32 +0100

    Display git sha version

--- app/services/environment_helper.rb
+++ app/services/environment_helper.rb
@@ -11,4 +11,6 @@ module EnvironmentHelper
   def self.test?;        env == :test;        end
   def self.staging?;     env == :staging;     end
   def self.production?;  env == :production;  end
+
+  def self.git_sha; ENV['HEROKU_SLUG_DESCRIPTION'] end
 end
