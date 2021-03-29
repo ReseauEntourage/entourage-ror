0427705308a83c817d50e7bdc90540fc0d44f17d
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-03-29 09:17:16 +0200

    EN-3573 Display pinned actions in home

--- app/services/entourage_services/pins.rb
+++ app/services/entourage_services/pins.rb
@@ -7,13 +7,14 @@ module EntourageServices
 
       if (types.nil? || types.any? { |t| t.starts_with?('ask_for_help_') || t.starts_with?('contribution_') })
         pinned << self.neighborhood_group_for(user)
+        pinned << self.pinned_for(user)
       end
 
       if (types.nil? || types.include?('outing'))
         pinned << 121064 # Atelier solidaire : 1h pour comprendre comment aider les personnes SDF
       end
 
-      pinned
+      pinned.uniq.compact
     end
 
     NEIGHBORHOOD_GROUPS = Hash[{
@@ -44,6 +45,22 @@ module EntourageServices
       entourage_id = NEIGHBORHOOD_GROUPS[postal_code] || NEIGHBORHOOD_GROUPS[departement]
       return if entourage_id.nil?
       entourage_id
+    end
+
+    def self.pinned_for user
+      return if user.address.nil?
+      return if user.address.country != 'FR'
+
+      postal_code = user.address.postal_code.to_s
+      departement = postal_code.first(2)
+
+      Entourage.select(:id)
+        .where(pin: true)
+        .where('pins ? :postal_code OR pins ? :departement',
+          postal_code: postal_code,
+          departement: departement
+        ).map(&:id)
+        .first
     end
   end
 end
