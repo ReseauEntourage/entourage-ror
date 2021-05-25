01a740dbbfed87b4e537151e0031e7e223168714
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-05-25 14:15:00 +0200

    Revert "Merge branch 'EN-3703-sms-codes-on-slack' into staging"
    This reverts commit dcadb642e5f2c7435e61a9b58e7bc42dc0e09dc0, reversing
    changes made to 55a4a44a20220af8a8fa6f7808fc1134f3a462b4.

--- app/services/sms_notification_service.rb
+++ app/services/sms_notification_service.rb
@@ -34,11 +34,6 @@ class SmsNotificationService
       return
     end
 
-    # we want a copy of sms_codes on slack
-    if ENV['COPY_SMS_CODE_TO_SLACK'] == 'true' && ['regenerate', 'welcome'].include?(sms_type)
-      deliveryState = send_slack_message(phone_number, message, sms_type, "sms-codes-#{EnvironmentHelper.env}")
-    end
-
     SmsDelivery.create(phone_number: phone_number, status: deliveryState, sms_type: sms_type, provider: provider)
   end
 
@@ -98,8 +93,10 @@ class SmsNotificationService
     return deliveryState
   end
 
-  def send_slack_message(phone_number, message, sms_type, channel = '#test-env-sms')
+  def send_slack_message(phone_number, message, sms_type)
     return if ENV['SLACK_WEBHOOK_URL'].blank?
+
+    channel = '#test-env-sms'
 
     if audit_phone_number?(phone_number)
       channel = '#audit-linkedout-entourage'
@@ -109,7 +106,7 @@ class SmsNotificationService
       channel: channel,
       username: ENV['SMS_SENDER_NAME'],
       icon_emoji: ':speech_balloon:',
-      text: "À #{obfuscate phone_number} (#{EnvironmentHelper.env})\n"\
+      text: "À #{phone_number} (#{EnvironmentHelper.env})\n"\
             "```\n"\
             "#{message}"\
             "```"
@@ -128,9 +125,5 @@ class SmsNotificationService
   def audit_phone_number? phone_number
     ENV['AUDIT_PHONE_PREFIXES'].present? &&
     ENV['AUDIT_PHONE_PREFIXES'].split(',').any? { |prefix| phone_number.start_with?(prefix) }
-  end
-
-  def obfuscate phone_number
-    phone_number.dup.tap { |p| p[4...8] = "****" }
   end
 end
