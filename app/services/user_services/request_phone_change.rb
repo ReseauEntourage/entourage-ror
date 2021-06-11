f39dc08dde3665602acd9b9e646d551930e5e2d8
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-11 16:17:20 +0200

    EN-3805 Record phone request when the user requests for it

--- app/services/user_services/request_phone_change.rb
+++ app/services/user_services/request_phone_change.rb
@@ -7,6 +7,8 @@ module UserServices
     end
 
     def request(requested_phone:, email:)
+      record_phone_request! requested_phone, email
+
       Slack::Notifier.new(webhook_url).ping(
         channel: channel,
         username: USERNAME,
@@ -36,6 +38,16 @@ module UserServices
 
     def user_url
       Rails.application.routes.url_helpers.admin_user_url(user.id, host: host)
+    end
+
+    def record_phone_request! requested_phone, email
+      UserPhoneChange.create(
+        user_id: @user.id,
+        kind: :request,
+        phone_was: @user.phone,
+        phone: requested_phone,
+        email: email
+      )
     end
   end
 end
