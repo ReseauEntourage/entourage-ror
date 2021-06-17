11d1d68d0b4797cf6a144ea60c3f5a16c475a158
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-17 13:40:32 +0200

    EN-3740 Block, unblock a user with a specific route

---
+++ app/models/user_history.rb
@@ -0,0 +1,28 @@
+class UserHistory < ApplicationRecord
+  belongs_to :user
+  belongs_to :user, foreign_key: :updater_id
+
+  attribute :metadata, :jsonb_with_schema
+
+  def metadata= value
+    value = add_metadata_schema_urn(value)
+    super(value)
+  end
+
+  def add_metadata_schema_urn value
+    value = {} if value.nil?
+    value['$id'] = "urn:user_history:#{kind}:metadata" if kind
+    value
+  end
+
+  def self.json_schema urn
+    JsonSchemaService.base do
+      case urn
+      when 'block:metadata', 'unblock:metadata'
+        {
+          cnil_explanation: { type: :string }
+        }
+      end
+    end
+  end
+end
