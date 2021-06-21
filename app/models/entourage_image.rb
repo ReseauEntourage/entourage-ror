7cd82a559a356e44cf904fcc93babe205eb57238
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-21 15:19:34 +0200

    EN-3718 display small images in admin.entourage_images#index

--- app/models/entourage_image.rb
+++ app/models/entourage_image.rb
@@ -1,3 +1,11 @@
 class EntourageImage < ApplicationRecord
   validates_presence_of :title
+
+  def landscape_small_url
+    landscape_thumbnail_url || landscape_url
+  end
+
+  def portrait_small_url
+    portrait_thumbnail_url || portrait_url
+  end
 end
