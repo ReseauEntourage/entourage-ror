66fbd4c0a3febcb7b90ffdb40aacd99078555579
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-23 17:12:36 +0200

    EN-3824 Fix migrations when field is empty

--- db/migrate/20210623112000_update_entourage_images_paths.rb
+++ db/migrate/20210623112000_update_entourage_images_paths.rb
@@ -2,7 +2,7 @@ class UpdateEntourageImagesPaths < ActiveRecord::Migration[5.1]
   def up
     EntourageImage.all.each do |entourage_image|
       ['landscape_thumbnail_url', 'landscape_url', 'portrait_thumbnail_url', 'portrait_url'].each do |field|
-        if entourage_image[field].include? ENV['ENTOURAGE_AVATARS_BUCKET']
+        if entourage_image[field].present? && entourage_image[field].include?(ENV['ENTOURAGE_AVATARS_BUCKET'])
           entourage_image.update_attribute(field,
             "entourage_images/images/#{entourage_image[field].gsub /(.)*entourage_images\/images\//, ''}"
           )
--- db/migrate/20210623112001_update_entourages_paths.rb
+++ db/migrate/20210623112001_update_entourages_paths.rb
@@ -2,7 +2,7 @@ class UpdateEntouragesPaths < ActiveRecord::Migration[5.1]
   def up
     Entourage.where(group_type: :outing).where("metadata->>'landscape_url' like '%#{ENV['ENTOURAGE_AVATARS_BUCKET']}%'").each do |entourage|
       [:landscape_thumbnail_url, :landscape_url, :portrait_thumbnail_url, :portrait_url].each do |field|
-        if entourage.metadata[field].include? ENV['ENTOURAGE_AVATARS_BUCKET']
+        if entourage.metadata[field] && entourage.metadata[field].include?(ENV['ENTOURAGE_AVATARS_BUCKET'])
           entourage.metadata[field] = "entourage_images/images/#{entourage.metadata[field].gsub /(.)*entourage_images\/images\//, ''}"
           entourage.save
         end
