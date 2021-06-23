95e1cabd68d6f145b824be57990d09ad3e88b1ab
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-23 11:23:39 +0200

    EN-3824 Store entourage_images relative paths rather than absolute ones

--- app/models/entourage_image.rb
+++ app/models/entourage_image.rb
@@ -8,4 +8,24 @@ class EntourageImage < ApplicationRecord
   def portrait_small_url
     portrait_thumbnail_url || portrait_url
   end
+
+  def landscape_thumbnail_url
+    storage.url_for key: self['landscape_thumbnail_url']
+  end
+
+  def landscape_url
+    storage.url_for key: self['landscape_url']
+  end
+
+  def portrait_thumbnail_url
+    storage.url_for key: self['portrait_thumbnail_url']
+  end
+
+  def portrait_url
+    storage.url_for key: self['portrait_url']
+  end
+
+  def storage
+    Storage::Client.images
+  end
 end
