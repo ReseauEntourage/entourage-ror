0ea0a3e03038a69015c9425e6d90af46ce3548a9
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-06-23 11:23:39 +0200

    EN-3824 Store entourage_images relative paths rather than absolute ones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210621111001) do
+ActiveRecord::Schema.define(version: 20210623112000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
