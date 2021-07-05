67261b391aba9ca68ff28c2067f4e0822e30394c
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-07-05 13:54:35 +0200

    EN-3671 disable use_authenticated_cookie_encryption

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.action_dispatch.cookies_serializer = :json
+# @temporary remove as soon as Entourage version using Rails 5.2 is stable
+# @see https://guides.rubyonrails.org/upgrading_ruby_on_rails.html#expiry-in-signed-or-encrypted-cookie-is-now-embedded-in-the-cookies-values
+Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = false
