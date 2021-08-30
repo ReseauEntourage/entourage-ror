a5fa14fce1d76dd5ef7ab67f8f6308296f187584
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-08-30 15:05:13 +0200

    EN-4056 Ouch: fix supernumerary star

--- app/services/user_services/unblock.rb
+++ app/services/user_services/unblock.rb
@@ -4,6 +4,6 @@ module UserServices
       User.blocked.where('unblock_at is not null and unblock_at < ?', Time.now).pluck(:id).each do |user_id|
         SlackServices::UnblockUser.new(user_id: user_id).notify
       end
-    end*
+    end
   end
 end
