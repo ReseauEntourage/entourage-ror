d788314922382d650d072dfd0cc2b88568173480
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-10-06 15:16:02 +0200

    EN-4159 Fix: we should refer to followers, not partner users

--- spec/services/v1/entourage_serializer_spec.rb
+++ spec/services/v1/entourage_serializer_spec.rb
@@ -5,39 +5,29 @@ describe V1::EntourageSerializer do
   include ActiveModel::Serializers::JSON
 
   describe 'author' do
+    let(:user) { FactoryBot.create(:public_user) }
     let(:partner) { FactoryBot.create(:partner) }
     let(:creator) { FactoryBot.create(:public_user, partner: partner) }
     let(:entourage) { FactoryBot.create(:entourage, user: creator) }
+    let(:serialization) { V1::EntourageSerializer.new(entourage, scope: {
+      user: user
+    })}
 
-    it 'has default keys' do
-      serialization = V1::EntourageSerializer.new(entourage, scope: {
-        user: FactoryBot.create(:public_user)
-      })
+    it { expect(serialization.author).to have_key(:id) }
+    it { expect(serialization.author).to have_key(:display_name) }
+    it { expect(serialization.author).to have_key(:avatar_url) }
+    it { expect(serialization.author).to have_key(:partner) }
+    it { expect(serialization.author).to have_key(:partner_role_title) }
+    it { expect(serialization.author).to have_key(:partner_with_current_user) }
 
-      expect(serialization.author).to have_key(:id)
-      expect(serialization.author).to have_key(:display_name)
-      expect(serialization.author).to have_key(:avatar_url)
-      expect(serialization.author).to have_key(:partner)
-      expect(serialization.author).to have_key(:partner_role_title)
-      expect(serialization.author).to have_key(:partner_with_current_user)
+    context 'user with no partner' do
+      it { expect(serialization.author[:partner_with_current_user]).to be(false) }
     end
 
-    it 'user with no partner' do
-      serialization = V1::EntourageSerializer.new(entourage, scope: {
-        user: FactoryBot.create(:public_user)
-      })
+    context 'user with partner' do
+      let!(:following) { FactoryBot.create(:following, partner: partner, user: user )}
 
-      expect(serialization.author).to have_key(:partner_with_current_user)
-      expect(serialization.author[:partner_with_current_user]).to be(false)
-    end
-
-    it 'user with partner' do
-      serialization = V1::EntourageSerializer.new(entourage, scope: {
-        user: FactoryBot.create(:public_user, partner: partner)
-      })
-
-      expect(serialization.author).to have_key(:partner_with_current_user)
-      expect(serialization.author[:partner_with_current_user]).to be(true)
+      it { expect(serialization.author[:partner_with_current_user]).to be(true) }
     end
   end
 end
