ed10d7e6e4b9e8e94027bab57d6ca0c10f090fe6
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-10-19 16:54:29 +0200

    EN-3985 Fixes and move user search from user_search controller to user#index

--- app/controllers/admin/users_search_controller.rb
+++ app/controllers/admin/users_search_controller.rb
@@ -8,16 +8,5 @@ module Admin
       results = users.map {|u| {label: u.full_name, value: u.full_name, id: u.id} }
       render json: results
     end
-
-    def user_search
-      @params = params.permit([:status]).to_h
-      @users = User.includes(:organization)
-                   .where(community: current_user.community.slug)
-                   .search_by(params[:search])
-                   .order("last_sign_in_at desc nulls last")
-                   .page(params[:page])
-                   .per(25)
-      render "admin/users/index"
-    end
   end
 end
