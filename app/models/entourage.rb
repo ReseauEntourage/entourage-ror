063225dfcca722eda6cdf2005704d076efd16a9a
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-12-13 10:52:57 +0100

    EN-4471 add column outing date

--- app/models/entourage.rb
+++ app/models/entourage.rb
@@ -292,6 +292,18 @@ class Entourage < ApplicationRecord
     end.to_h
   end
 
+  def starts_at
+    return unless outing?
+    return unless metadata
+    metadata[:starts_at]
+  end
+
+  def ends_at
+    return unless outing?
+    return unless metadata
+    metadata[:ends_at]
+  end
+
   def outing_image_url?
     return unless outing?
 
