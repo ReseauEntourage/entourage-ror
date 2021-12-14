cf529372f969a50c2b2f25f79470d76331d6f6bd
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-12-14 15:36:52 +0100

    EN-4502 slack alert performed only for action, outing; not performed for conversation or tour

--- app/observers/join_request_observer.rb
+++ app/observers/join_request_observer.rb
@@ -2,13 +2,10 @@ class JoinRequestObserver < ActiveRecord::Observer
   observe :join_request
 
   def after_create(record)
-    return unless record.pending?
-
     action(:create, record)
   end
 
   def after_update(record)
-    return unless record.pending?
     return unless record.saved_change_to_status?
 
     action(:update, record)
@@ -20,6 +17,13 @@ class JoinRequestObserver < ActiveRecord::Observer
   # @param record JoinRequest
   # sends a log to Slack
   def action(verb, record)
+    return unless record.joinable
+    return unless record.entourage?
+    return unless record.pending?
+    return if record.joinable.conversation?
+
     SlackServices::StackTrace.new(title: "Pending join_request detected on #{verb}, id: #{record.id}", stack_trace: "> #{caller.inspect}").notify
+  rescue
+    # we want this hook to never fail the main process
   end
 end
