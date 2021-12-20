164676c06380e9d646bc3a3da7f91456c8281fca
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2021-12-20 19:26:25 +0100

    EN-4493 Use ams_lazy_relationships to avoid n+1 serializer queries
    Also avoid some useless instanciations

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module EntourageBack
   class Application < Rails::Application
     config.time_zone = 'Paris'
 
+    # Batch loading
+    config.middleware.use BatchLoader::Middleware
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
