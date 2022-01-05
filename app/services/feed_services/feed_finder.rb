d7b240ff0d47011ed3e34c90972e6d2cca83e20d
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-01-05 16:01:59 +0100

    EN-4487 factorize methods
    Some similar methods exist in FeedServices::OutingsFinder and EntourageServices::EntourageFinder but they are not exactly the same

--- app/services/feed_services/feed_finder.rb
+++ app/services/feed_services/feed_finder.rb
@@ -273,28 +273,6 @@ module FeedServices
       end
     end
 
-    def preload_entourage_moderations(feeds)
-      entourage_ids = feeds.find_all { |feed| feed.feedable.is_a?(Entourage) && feed.feedable.has_outcome? }.map(&:feedable_id)
-      return if entourage_ids.empty?
-      entourage_moderations = EntourageModeration.where(entourage_id: entourage_ids)
-      entourage_moderations = Hash[entourage_moderations.map { |m| [m.entourage_id, m] }]
-      feeds.each do |feed|
-        next unless feed.feedable.is_a?(Entourage) && feed.feedable.has_outcome?
-        feed.feedable.association(:moderation).target = entourage_moderations[feed.feedable_id]
-      end
-    end
-
-    def preload_tour_user_organizations(feeds)
-      organization_ids = feeds.find_all { |feed| feed.feedable.is_a?(Tour) }.map { |feed| feed.feedable.user&.organization_id }.compact.uniq
-      return if organization_ids.empty?
-      organizations = Organization.where(id: organization_ids)
-      organizations = Hash[organizations.map { |o| [o.id, o] }]
-      feeds.each do |feed|
-        next unless feed.feedable.is_a?(Tour)
-        next if feed.feedable.user.nil?
-        feed.feedable.user.organization = organizations[feed.feedable.user.organization_id]
-      end
-    end
     # This is just for the sake of making the cursor seemingly random and hard
     # to decode, to discourage clients of caching tokens or forging them.
     def self.encode_cursor cursor
--- app/services/feed_services/preloader.rb
+++ app/services/feed_services/preloader.rb
@@ -15,5 +15,28 @@ module FeedServices
         feed.number_of_unread_messages = counts[join_request_id]
       end
     end
+
+    def preload_tour_user_organizations(feeds)
+      organization_ids = feeds.find_all { |feed| feed.feedable.is_a?(Tour) }.map { |feed| feed.feedable.user&.organization_id }.compact.uniq
+      return if organization_ids.empty?
+      organizations = Organization.where(id: organization_ids)
+      organizations = Hash[organizations.map { |o| [o.id, o] }]
+      feeds.each do |feed|
+        next unless feed.feedable.is_a?(Tour)
+        next if feed.feedable.user.nil?
+        feed.feedable.user.organization = organizations[feed.feedable.user.organization_id]
+      end
+    end
+
+    def preload_entourage_moderations(feeds)
+      entourage_ids = feeds.find_all { |feed| feed.feedable.is_a?(Entourage) && feed.feedable.has_outcome? }.map(&:feedable_id)
+      return if entourage_ids.empty?
+      entourage_moderations = EntourageModeration.where(entourage_id: entourage_ids)
+      entourage_moderations = Hash[entourage_moderations.map { |m| [m.entourage_id, m] }]
+      feeds.each do |feed|
+        next unless feed.feedable.is_a?(Entourage) && feed.feedable.has_outcome?
+        feed.feedable.association(:moderation).target = entourage_moderations[feed.feedable_id]
+      end
+    end
   end
 end
