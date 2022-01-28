300ffb896635f0ea3a3658efd08e8073e6d9c5f1
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-01-28 15:44:02 +0100

    EN-4567 refactor error management

--- app/controllers/api/v1/uptimes_controller.rb
+++ app/controllers/api/v1/uptimes_controller.rb
@@ -1,20 +1,18 @@
 module Api
   module V1
     class UptimesController < Api::V1::BaseController
-      skip_before_action :authenticate_user!
-      before_action :authenticate_super_admin!
+      skip_before_action :authenticate_user!, only: [:soliguides, :soliguide]
+      before_action :authenticate_super_admin!, only: [:soliguides, :soliguide]
+
+      rescue_from JSON::ParserError, with: :rescue_parse_error
+      rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_unauthorized
+      rescue_from ActionController::BadRequest, with: :rescue_bad_request
 
       # POST https://api.soliguide.fr/new-search
       def soliguides
         response = PoiServices::SoliguideIndex.uptime
 
-        return render json: {
-          message: :bad_token
-        }, status: :unauthorized if ["401", "403"].include?(response.code.to_s)
-
-        return render json: {
-          message: :unexcepted_status
-        }, status: :bad_request unless ["200", "201"].include?(response.code.to_s)
+        validate_response! response
 
         body = JSON.parse(response.read_body)
 
@@ -23,21 +21,13 @@ module Api
         }, status: :bad_request unless body.has_key?('places')
 
         render json: { message: :ok, count: body['places'].count }, status: :ok
-      rescue JSON::ParserError => e
-        render json: { message: :not_parsable }, status: :bad_request
       end
 
       # GET https://api.soliguide.fr/place/:lieu_id
       def soliguide
         response = PoiServices::SoliguideShow.uptime
 
-        return render json: {
-          message: :bad_token
-        }, status: :unauthorized if ["401", "403"].include?(response.code.to_s)
-
-        return render json: {
-          message: :unexcepted_status
-        }, status: :bad_request unless ["200", "201"].include?(response.code.to_s)
+        validate_response! response
 
         body = JSON.parse(response.body)
 
@@ -46,8 +36,6 @@ module Api
         }, status: :bad_request unless body.has_key?('lieu_id')
 
         render json: { message: :ok, lieu_id: body['lieu_id'] }, status: :ok
-      rescue JSON::ParserError => e
-        render json: { message: :not_parsable }, status: :bad_request
       end
 
       private
@@ -56,6 +44,23 @@ module Api
         render json: {
           message: :unauthorized
         }, status: :unauthorized unless current_user && current_user.super_admin?
+      end
+
+      def validate_response! response
+        raise ActionController::InvalidAuthenticityToken, :bad_token if ["401", "403"].include?(response.code.to_s)
+        raise ActionController::BadRequest, :unexcepted_status unless ["200", "201"].include?(response.code.to_s)
+      end
+
+      def rescue_parse_error
+        render json: { message: :not_parsable }, status: :bad_request
+      end
+
+      def rescue_unauthorized error
+        render json: { message: error.message }, status: :unauthorized
+      end
+
+      def rescue_bad_request error
+        render json: { message: error.message }, status: :bad_request
       end
     end
   end
