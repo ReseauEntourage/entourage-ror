284c24c46576d2e96cc4a463f18f7b7852097f1e
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-01-28 14:45:22 +0100

    N-4567 route to uptime on Soliguide GET place/:id

--- app/controllers/api/v1/uptimes_controller.rb
+++ app/controllers/api/v1/uptimes_controller.rb
@@ -29,6 +29,25 @@ module Api
 
       # GET https://api.soliguide.fr/place/:lieu_id
       def soliguide
+        response = PoiServices::SoliguideShow.uptime
+
+        return render json: {
+          message: :bad_token
+        }, status: :unauthorized if ["401", "403"].include?(response.code.to_s)
+
+        return render json: {
+          message: :unexcepted_status
+        }, status: :bad_request unless ["200", "201"].include?(response.code.to_s)
+
+        body = JSON.parse(response.body)
+
+        return render json: {
+          message: :no_place
+        }, status: :bad_request unless body.has_key?('lieu_id')
+
+        render json: { message: :ok, lieu_id: body['lieu_id'] }, status: :ok
+      rescue JSON::ParserError => e
+        render json: { message: :not_parsable }, status: :bad_request
       end
 
       private
