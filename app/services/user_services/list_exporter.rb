0ecf9189784b7bbb4596760e599bc1875753da90
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-02-03 12:13:26 +0100

    users export may be sendable by email

--- app/services/user_services/list_exporter.rb
+++ app/services/user_services/list_exporter.rb
@@ -17,14 +17,20 @@ module UserServices
       deleted
     }
 
+    FULL_FIELDS = FIELDS + %w{
+      action_participations_count
+      outing_participations_count
+      conversation_participations_count
+    }
+
+    SLACK_CHANNEL = '#test-nicolas'
+
     def initialize(users:)
       @users = users
     end
 
     def csv
-      ensure_directory_exist
-
-      file = path
+      file = self.get_file
 
       CSV.open(file, 'w+') do |writer|
         writer << FIELDS.map { |field| I18n.t("activerecord.attributes.user.#{field}") }
@@ -37,15 +43,50 @@ module UserServices
       file
     end
 
+    class << self
+      def export user_ids
+        file = path
+
+        CSV.open(file, 'w+') do |writer|
+          writer << FULL_FIELDS.map { |field| I18n.t("activerecord.attributes.user.#{field}") }
+
+          user_ids.each do |user_id|
+            user = User.find(user_id)
+            writer << FULL_FIELDS.map { |field| user.send(field) }
+          end
+        end
+
+        file
+      end
+
+      def get_file
+        ensure_directory_exist
+        path
+      end
+
+      def ensure_directory_exist
+        Dir.mkdir(DEFAULT_PATH) unless Dir.exist?(DEFAULT_PATH)
+      end
+
+      def path
+        "#{DEFAULT_PATH}/users-#{Time.now.to_i}.csv"
+      end
+
+      def to_slack csv
+        return if ENV['SLACK_WEBHOOK_URL'].blank?
+
+        Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping(
+          channel: SLACK_CHANNEL,
+          username: ENV['SMS_SENDER_NAME'],
+          text: CSV.read(csv).to_s
+        )
+
+        return 'Ok'
+      end
+    end
+
     private
+
     attr_reader :users
-
-    def ensure_directory_exist
-      Dir.mkdir(DEFAULT_PATH) unless Dir.exist?(DEFAULT_PATH)
-    end
-
-    def path
-      "#{DEFAULT_PATH}/users-#{Time.now.to_i}.csv"
-    end
   end
 end
