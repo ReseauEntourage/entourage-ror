facdfd72529f40ab2bf0c30b9870039c7929f34c
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-02-24 16:35:13 +0100

    EN-4707 remove deprecated users interests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  acts_as_taggable_on :tags
+  acts_as_taggable_on :interests
 
   include Onboarding::UserEventsTracking::UserConcern
   include UserServices::Engagement
@@ -93,7 +93,6 @@ class User < ApplicationRecord
 
   enum device_type: [ :android, :ios ]
   attribute :roles, :jsonb_set
-  attribute :interests, :jsonb_set
 
   scope :type_pro, -> { where(user_type: "pro") }
   scope :validated, -> { where(validation_status: "validated") }
@@ -250,7 +249,7 @@ class User < ApplicationRecord
     return if community.nil?
     attribute = attribute.to_sym
 
-    invalid = self[attribute] - community.send(attribute)
+    invalid = send(attribute) - community.send(attribute)
     invalid = yield invalid if block_given?
 
     errors.add(
@@ -271,7 +270,7 @@ class User < ApplicationRecord
   end
 
   def validate_interests!
-    validate_set_attr :interests
+    validate_set_attr :interest_list
   end
 
   def validate_partner!
