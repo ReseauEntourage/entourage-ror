6a82b2825ac98f118c149e4c5aebbe2e5df2b572
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-02-25 14:20:50 +0100

    EN-4707 allow admins to edit user interests

--- db/migrate/20220224143000_remove_interests_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveInterestsFromUsers < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :users, :interests
-  end
-
-  def down
-    add_column :users, :interests, :string
-  end
-end
