17c2efb9b22022d8005b9b01ac98a751435c7204
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-02-28 10:37:22 +0100

    EN-4707 refactor: UserSerializer 'phone_only' in another class to lighten main one

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,41 +1,37 @@
 module V1
   class UserSerializer < ActiveModel::Serializer
-    attribute :id,           unless: :phone_only?
-    attribute :display_name, unless: :phone_only?
-    attribute :first_name,   unless: :phone_only?
-    attribute :last_name,    unless: :phone_only?
-    attribute :roles,        unless: :phone_only?
-    attribute :about,        unless: :phone_only?
-    attribute :avatar_url,   unless: :phone_only?
-    attribute :user_type,    unless: :phone_only?
-    attribute :partner,      unless: :phone_only?
-    attribute :engaged,      unless: :phone_only?
-    attribute :unread_count, unless: :phone_only?
-    attribute :permissions,  unless: :phone_only?
-    attribute :phone,        if: :phone_only?
+    attribute :id
+    attribute :display_name
+    attribute :first_name
+    attribute :last_name
+    attribute :roles
+    attribute :about
+    attribute :avatar_url
+    attribute :user_type
+    attribute :partner
+    attribute :engaged
+    attribute :unread_count
+    attribute :permissions
     attribute :placeholders, if: :placeholders?
     attribute :memberships,  if: :memberships?
     attribute :conversation, if: :conversation?
     # uuid and anonymous are not confidential but right now we only need them for current_user in the clients so we don't return it in other contexts
-    attribute :anonymous,           if: :default?
-    attribute :uuid,                if: :default?
-    attribute :feature_flags,       if: :default?
-    attribute :token,               if: :default?
-    attribute :email,               if: :default?
-    attribute :has_password,        if: :default?
-    attribute :firebase_properties, if: :default?
-    attribute :goal,                if: :default?
-    attribute :interests,           if: :default?
-    attribute :travel_distance,     if: :default?
+    attribute :anonymous,           if: :me?
+    attribute :uuid,                if: :me?
+    attribute :feature_flags,       if: :me?
+    attribute :token,               if: :me?
+    attribute :email,               if: :me?
+    attribute :has_password,        if: :me?
+    attribute :firebase_properties, if: :me?
+    attribute :goal,                if: :me?
+    # attribute :phone,               if: :me?
+    attribute :interests,           if: :me?
+    attribute :travel_distance,     if: :me?
 
-    has_one :stats,        unless: :phone_only?
-    has_one :organization, serializer: ::V1::OrganizationSerializer, unless: :phone_only?
-    has_one :address,      serializer: AddressSerializer, if: :default?
-    has_one :address_2,    serializer: AddressSerializer, if: :default?
-
-    def phone_only?
-      scope[:phone_only] == true
-    end
+    has_one :stats
+    has_one :organization, serializer: ::V1::OrganizationSerializer
+    has_one :address,      serializer: AddressSerializer, if: :me?
+    has_one :address_2,    serializer: AddressSerializer, if: :me?
 
     def placeholders?
       me? && scope[:user].anonymous?
@@ -47,11 +43,6 @@ module V1
 
     def conversation?
       scope[:conversation] && scope[:user]
-    end
-
-    def default?
-      return false if phone_only?
-      me?
     end
 
     def stats
