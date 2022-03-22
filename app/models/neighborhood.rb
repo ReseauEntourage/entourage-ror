bf66a716bd5b98c3395fbd69fca9c73a46ddccd4
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-03-22 15:40:01 +0100

    EN-4711 add neighborhoods relationships to outings

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -5,44 +5,40 @@ class Neighborhood < ApplicationRecord
 
   has_many :join_requests, as: :joinable, dependent: :destroy
   has_many :members, through: :join_requests, source: :user
+  has_many :neighborhoods_entourages
+
+  has_many :outings, -> { where(group_type: :outing) }, through: :neighborhoods_entourages, source: :entourage
 
   reverse_geocoded_by :latitude, :longitude
   has_many :chat_messages, as: :messageable, dependent: :destroy
 
-  # # Fields
-  # title
-  # ethics
-  # members
-  # members_count
-  # photo_url
-  # interests
-  # events
-  # events_count
-  # past_events_count
-  # future_events_count
-  # ongoing_events
+  validates_presence_of [:name, :latitude, :longitude]
 
   def members_count
     members.count
   end
 
-  def past_events
-    nil
+  def past_outings
+    outings.where("metadata->>'ends_at' < ?", Time.zone.now)
   end
 
-  def past_events_count
-    nil
+  def past_outings_count
+    past_outings.count
   end
 
-  def future_events
-    nil
+  def future_outings
+    outings.where("metadata->>'starts_at' > ?", Time.zone.now)
   end
 
-  def future_events_count
-    nil
+  def future_outings_count
+    future_outings.count
   end
 
-  def has_ongoing_event
-    nil
+  def ongoing_outings
+    outings.where("metadata->>'starts_at' >= ?", Time.zone.now).where("metadata->>'ends_at' <= ?", Time.zone.now)
+  end
+
+  def has_ongoing_outing?
+    ongoing_outings.any?
   end
 end
--- db/migrate/20220321141000_create_neighborhoods.rb
+++ db/migrate/20220321141000_create_neighborhoods.rb
@@ -6,17 +6,21 @@ class CreateNeighborhoods < ActiveRecord::Migration[5.2]
       t.string :description
       t.string :ethics
       t.string :photo_url
+      t.float :latitude, null: false
+      t.float :longitude, null: false
 
       t.timestamps null: false
 
       t.index :user_id
       t.index :name
+      t.index [:latitude, :longitude], name: :neighborhoods_coordinates
     end
   end
 
   def down
     remove_index :neighborhoods, :user_id
     remove_index :neighborhoods, :name
+    remove_index :neighborhoods, :neighborhoods_coordinates
 
     drop_table :neighborhoods
   end
