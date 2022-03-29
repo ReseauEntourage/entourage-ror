a4c6e05b998d63e8d04aa3edb8b45b4ac7de4fea
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-03-29 12:48:37 +0200

    EN-4702 interests should be part of every user displayed

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -934,6 +934,7 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
             "permissions" => {
               "outing" => { "creation" => false }
             },
+            "interests" => [],
             "memberships" => [],
             "conversation" => {
               "uuid" => "1_list_#{user.id}-#{other_user.id}"
