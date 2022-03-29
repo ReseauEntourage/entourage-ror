282c4bb8c47c757908179136a6937a48f8f85f8b
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-03-29 10:39:20 +0200

    EN-4711 post message in a neighborhood

---
+++ spec/controllers/api/v1/neighborhoods/chat_messages_controller_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+describe Api::V1::Neighborhoods::ChatMessagesController do
+  let(:neighborhood) { create :neighborhood }
+
+  describe 'POST create' do
+    context "not signed in" do
+      before { post :create, params: { neighborhood_id: neighborhood.to_param, chat_message: { content: "foobar"} } }
+      it { expect(response.status).to eq(401) }
+      it { expect(ChatMessage.count).to eq(0) }
+    end
+
+    context "signed in" do
+      let(:user) { FactoryBot.create(:pro_user) }
+
+      let!(:ios_app) { FactoryBot.create(:ios_app, name: 'neighborhood') }
+      let!(:android_app) { FactoryBot.create(:android_app, name: 'neighborhood') }
+
+      context "valid params" do
+        let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: "accepted") }
+
+        before { post :create, params: {
+          neighborhood_id: neighborhood.to_param, chat_message: { content: "foobar", message_type: :text }, token: user.token
+        } }
+
+        let(:result) { JSON.parse(response.body) }
+
+        it { expect(response.status).to eq(201) }
+        it { expect(ChatMessage.count).to eq(1) }
+        it { expect(JSON.parse(response.body)).to eq({
+          "chat_message" => {
+            "id" => ChatMessage.first.id,
+            "message_type" => "text",
+            "content" => "foobar",
+            "user" => {
+              "id" => user.id,
+              "avatar_url" => nil,
+              "display_name" => "John D.",
+              "partner" => nil
+            },
+            "created_at" => ChatMessage.first.created_at.iso8601(3)
+          }
+        }) }
+      end
+
+      describe "send push notif" do
+        it "sends notif to everyone accaepted except message sender" do
+          join_request = FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: "accepted")
+          join_request2 = FactoryBot.create(:join_request, joinable: neighborhood, status: "accepted")
+
+          FactoryBot.create(:join_request, joinable: neighborhood, status: "pending")
+
+          expect_any_instance_of(PushNotificationService).to receive(:send_notification).with(
+            "John D.",
+            'Foot Paris 17è',
+            'foobaz',
+            [ join_request2.user ],
+            {
+              joinable_id: neighborhood.id,
+              joinable_type: "Neighborhood",
+              group_type: 'neighborhood',
+              type: "NEW_CHAT_MESSAGE"
+            }
+          )
+
+          post :create, params: { neighborhood_id: neighborhood.to_param, chat_message: { content: "foobaz" }, token: user.token }
+        end
+      end
+    end
+  end
+end
