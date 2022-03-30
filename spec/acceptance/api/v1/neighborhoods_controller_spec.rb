be7f8c6b64c214aa20c39043d449362f1b0490a0
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-03-30 09:46:31 +0200

    EN-4711 add API doc to neighborhood#create

---
+++ spec/acceptance/api/v1/neighborhoods_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource Api::V1::NeighborhoodsController do
+  explanation "Neighborhoods"
+  header "Content-Type", "application/json"
+
+  post 'api/v1/neighborhoods' do
+    route_summary "Creates a neighborhood"
+
+    parameter :token, type: :string, required: true
+
+    with_options :scope => :neighborhood, :required => true do
+      parameter :name, "Name"
+      parameter :ethics, "Ethics", required: false
+      parameter :latitude, "Latitude"
+      parameter :longitude, "Longitude"
+      parameter :interests, "Interests", required: false
+      parameter :photo_url, "Photo url", required: false
+    end
+
+
+    let(:neighborhood) { build :neighborhood }
+    let(:user) { FactoryBot.create(:pro_user) }
+
+    let(:raw_post) { {
+      token: user.token,
+      neighborhood: {
+        name: neighborhood.name,
+        ethics: neighborhood.ethics,
+        latitude: neighborhood.latitude,
+        longitude: neighborhood.longitude,
+        interests: neighborhood.interest_list,
+        photo_url: neighborhood.photo_url
+      }
+    }.to_json }
+
+    context '201' do
+      example_request 'Create neighborhood' do
+        expect(response_status).to eq(201)
+        expect(JSON.parse(response_body)).to have_key('neighborhood')
+      end
+    end
+  end
+end
