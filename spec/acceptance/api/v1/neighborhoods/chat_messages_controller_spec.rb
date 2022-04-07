5a886f676e1ca14a99520f1e5b4c70974f0f966a
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-07 11:26:25 +0200

    EN-4711 add api doc to neighborhoods

---
+++ spec/acceptance/api/v1/neighborhoods/chat_messages_controller_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource Api::V1::Neighborhoods::ChatMessagesController do
+  explanation "Chat messages"
+  header "Content-Type", "application/json"
+
+  # get '/api/v1/neighborhoods/:neighborhood_id/chat_messages' do
+  #   route_summary "Find user chat_messages in a neighborhood"
+  #   # route_description "no description"
+
+  #   parameter :token, type: :string, required: true
+  #   parameter :neighborhood_id, type: :integer, required: true
+
+  #   let(:user) { FactoryBot.create(:public_user) }
+  #   let(:token) { user.token }
+  #   let(:neighborhood) { FactoryBot.create(:neighborhood) }
+  #   let(:neighborhood_id) { neighborhood.id }
+  #   let!(:chat_message) { FactoryBot.create(:chat_message, messageable: neighborhood) }
+
+  #   context '401' do
+  #     example_request 'Cannot see chat_messages if the user does not belong to the neighborhood' do
+  #       expect(response_status).to eq(401)
+  #     end
+  #   end
+
+  #   context '200' do
+  #     let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
+
+  #     example_request 'Get chat_messages' do
+  #       expect(response_status).to eq(200)
+  #     end
+  #   end
+  # end
+
+  post '/api/v1/neighborhoods/:neighborhood_id/chat_messages' do
+    route_summary "Create chat_messages"
+    # route_description "no description"
+
+    parameter :token, type: :string, required: true
+    parameter :neighborhood_id, type: :integer, required: true
+
+    with_options :scope => :chat_message, :required => true do
+      parameter :content, type: :string
+      parameter :message_type, "text, status_update, share", type: :string
+    end
+
+    let(:user) { FactoryBot.create(:pro_user) }
+    let(:neighborhood) { FactoryBot.create(:neighborhood) }
+    let(:neighborhood_id) { neighborhood.id }
+
+    let(:raw_post) { {
+      token: user.token,
+      chat_message: {
+        content: "foo"
+      }
+    }.to_json }
+
+    context '201' do
+      let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
+
+      example_request 'Create chat_message' do
+        expect(response_status).to eq(201)
+        expect(JSON.parse(response_body)).to have_key('chat_message')
+      end
+    end
+
+    context '401' do
+      example_request 'Cannot create chat_messages if the user does not belong to the neighborhood' do
+        expect(response_status).to eq(401)
+      end
+    end
+  end
+end
--- spec/acceptance/api/v1/neighborhoods_controller_spec.rb
+++ spec/acceptance/api/v1/neighborhoods_controller_spec.rb
@@ -5,6 +5,42 @@ resource Api::V1::NeighborhoodsController do
   explanation "Neighborhoods"
   header "Content-Type", "application/json"
 
+  get '/api/v1/neighborhoods' do
+    route_summary "Find neighborhoods"
+
+    parameter :token, "User token", type: :string, required: true
+
+    let(:user) { FactoryBot.create(:pro_user) }
+    let!(:neighborhood) { FactoryBot.create(:neighborhood) }
+    let(:token) { user.token }
+
+    context '200' do
+      example_request 'Get neighborhoods' do
+        expect(response_status).to eq(200)
+        expect(JSON.parse(response_body)).to have_key('neighborhoods')
+      end
+    end
+  end
+
+  get 'api/v1/neighborhoods/:id' do
+    route_summary "Get a neighborhood"
+
+    parameter :id, required: true
+    parameter :token, type: :string, required: true
+
+    let(:neighborhood) { create :neighborhood }
+    let(:id) { neighborhood.id }
+    let(:user) { FactoryBot.create(:pro_user) }
+    let(:token) { user.token }
+
+    context '200' do
+      example_request 'Get neighborhood' do
+        expect(response_status).to eq(200)
+        expect(JSON.parse(response_body)).to have_key('neighborhood')
+      end
+    end
+  end
+
   post 'api/v1/neighborhoods' do
     route_summary "Creates a neighborhood"
 
@@ -16,9 +52,8 @@ resource Api::V1::NeighborhoodsController do
       parameter :latitude, "Latitude"
       parameter :longitude, "Longitude"
       parameter :interests, "Interests", required: false
-      parameter :image_url, "Photo url", required: false
+      parameter :image_url, "Image url", required: false
     end
-
 
     let(:neighborhood) { build :neighborhood }
     let(:user) { FactoryBot.create(:pro_user) }
@@ -38,6 +73,41 @@ resource Api::V1::NeighborhoodsController do
     context '201' do
       example_request 'Create neighborhood' do
         expect(response_status).to eq(201)
+        expect(JSON.parse(response_body)).to have_key('neighborhood')
+      end
+    end
+  end
+
+  patch 'api/v1/neighborhoods/:id' do
+    route_summary "Updates a neighborhood"
+
+    parameter :id, required: true
+    parameter :token, type: :string, required: true
+
+    with_options :scope => :neighborhood, :required => true do
+      parameter :name, "Name", required: false
+      parameter :ethics, "Ethics", required: false
+      parameter :latitude, "Latitude", required: false
+      parameter :longitude, "Longitude", required: false
+      parameter :interests, "Interests", required: false
+      parameter :image_url, "Image url", required: false
+    end
+
+    let(:neighborhood) { FactoryBot.create(:neighborhood, user: user) }
+    let(:user) { FactoryBot.create(:pro_user) }
+
+    let(:id) { neighborhood.id }
+    let(:raw_post) { {
+      token: user.token,
+      neighborhood: {
+        name: "new name",
+        ethics: "new ethics",
+      }
+    }.to_json }
+
+    context '200' do
+      example_request 'Update neighborhood' do
+        expect(response_status).to eq(200)
         expect(JSON.parse(response_body)).to have_key('neighborhood')
       end
     end
