c193a66013b3119eaf91086295dd3b4c9e05b279
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-07 12:26:04 +0200

    EN-4711 add neighborhood chat_messages index

--- app/controllers/api/v1/neighborhoods/chat_messages_controller.rb
+++ app/controllers/api/v1/neighborhoods/chat_messages_controller.rb
@@ -4,14 +4,21 @@ module Api
       class UnauthorizedNeighborhood < StandardError; end
 
       class ChatMessagesController < Api::V1::BaseController
-        before_action :set_neighborhood, only: [:create]
+        before_action :set_neighborhood, only: [:index, :create]
         before_action :authorised_to_see_messages?
 
         rescue_from Api::V1::Neighborhoods::UnauthorizedNeighborhood do |exception|
-          render json: {message: 'unauthorized : you are not accepted in this neighborhood'}, status: :unauthorized
+          render json: { message: 'unauthorized : you are not accepted in this neighborhood' }, status: :unauthorized
         end
 
         def index
+          messages = @neighborhood.chat_messages.ordered.limit(25)
+
+          if messages.present? && (join_request.last_message_read.nil? || join_request.last_message_read < messages.first.created_at)
+            join_request.update(last_message_read: messages.first.created_at)
+          end
+
+          render json: messages, each_serializer: ::V1::ChatMessageSerializer
         end
 
         def create
