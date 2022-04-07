446cb65b01c2f63fd7abd8c297a31720c4b620a7
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-07 12:26:04 +0200

    EN-4711 add neighborhood chat_messages index

--- spec/acceptance/api/v1/neighborhoods/chat_messages_controller_spec.rb
+++ spec/acceptance/api/v1/neighborhoods/chat_messages_controller_spec.rb
@@ -5,33 +5,33 @@ resource Api::V1::Neighborhoods::ChatMessagesController do
   explanation "Chat messages"
   header "Content-Type", "application/json"
 
-  # get '/api/v1/neighborhoods/:neighborhood_id/chat_messages' do
-  #   route_summary "Find user chat_messages in a neighborhood"
-  #   # route_description "no description"
+  get '/api/v1/neighborhoods/:neighborhood_id/chat_messages' do
+    route_summary "Find user chat_messages in a neighborhood"
+    # route_description "no description"
 
-  #   parameter :token, type: :string, required: true
-  #   parameter :neighborhood_id, type: :integer, required: true
+    parameter :token, type: :string, required: true
+    parameter :neighborhood_id, type: :integer, required: true
 
-  #   let(:user) { FactoryBot.create(:public_user) }
-  #   let(:token) { user.token }
-  #   let(:neighborhood) { FactoryBot.create(:neighborhood) }
-  #   let(:neighborhood_id) { neighborhood.id }
-  #   let!(:chat_message) { FactoryBot.create(:chat_message, messageable: neighborhood) }
+    let(:user) { FactoryBot.create(:public_user) }
+    let(:token) { user.token }
+    let(:neighborhood) { FactoryBot.create(:neighborhood) }
+    let(:neighborhood_id) { neighborhood.id }
+    let!(:chat_message) { FactoryBot.create(:chat_message, messageable: neighborhood) }
 
-  #   context '401' do
-  #     example_request 'Cannot see chat_messages if the user does not belong to the neighborhood' do
-  #       expect(response_status).to eq(401)
-  #     end
-  #   end
+    context '200' do
+      let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
 
-  #   context '200' do
-  #     let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
+      example_request 'Get chat_messages' do
+        expect(response_status).to eq(200)
+      end
+    end
 
-  #     example_request 'Get chat_messages' do
-  #       expect(response_status).to eq(200)
-  #     end
-  #   end
-  # end
+    context '401' do
+      example_request 'Cannot see chat_messages if the user does not belong to the neighborhood' do
+        expect(response_status).to eq(401)
+      end
+    end
+  end
 
   post '/api/v1/neighborhoods/:neighborhood_id/chat_messages' do
     route_summary "Create chat_messages"
