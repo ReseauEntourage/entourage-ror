6c33512f1a3b5742e9d2d6536b6dee6e8cf52426
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-15 09:18:15 +0200

    EC-99 Find neighborhoods

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,8 @@ class User < ApplicationRecord
 
   delegate :country, to: :address, allow_nil: true
   delegate :postal_code, to: :address, allow_nil: true
+  delegate :latitude, to: :address, allow_nil: true
+  delegate :longitude, to: :address, allow_nil: true
 
   attr_reader :admin_password
   attr_reader :cnil_explanation
