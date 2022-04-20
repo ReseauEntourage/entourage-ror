c1b5d51c6264c151c779bdd8d71deadb38cd4e80
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-20 10:35:24 +0200

    EN-4711 add 'other' category to user interests

--- spec/controllers/api/v1/tags_controller_spec.rb
+++ spec/controllers/api/v1/tags_controller_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Api::V1::TagsController, :type => :controller do
     it { expect(result).to have_key("interests") }
     it { expect(result["interests"]).to be_a(Hash) }
     it { expect(result["interests"]).to have_key("sport") }
+    it { expect(result["interests"]).to have_key("other") }
     it { expect(result["interests"]["sport"]).to eq("Sport") }
+    it { expect(result["interests"]["other"]).to eq("Autre") }
   end
 end
