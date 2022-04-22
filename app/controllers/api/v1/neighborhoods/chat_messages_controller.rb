f2da420077a35362593e4f681c971333ea247a33
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-22 14:57:48 +0200

    EC-108 Signal a neighborhood chat_message

--- app/controllers/api/v1/neighborhoods/chat_messages_controller.rb
+++ app/controllers/api/v1/neighborhoods/chat_messages_controller.rb
@@ -4,7 +4,8 @@ module Api
       class UnauthorizedNeighborhood < StandardError; end
 
       class ChatMessagesController < Api::V1::BaseController
-        before_action :set_neighborhood, only: [:index, :create]
+        before_action :set_neighborhood, only: [:index, :create, :report]
+        before_action :set_chat_message, only: [:report]
         before_action :authorised_to_see_messages?
 
         rescue_from Api::V1::Neighborhoods::UnauthorizedNeighborhood do |exception|
@@ -41,16 +42,28 @@ module Api
         end
 
         def report
+          return render json: { message: "Wrong chat_message" }, status: :bad_request unless @chat_message
+
+          SlackServices::SignalNeighborhoodChatMessage.new(
+            chat_message: @chat_message,
+            reporting_user: current_user
+          ).notify
+
+          head :created
         end
 
         private
 
-        def chat_messages_params
-          params.require(:chat_message).permit(:content, :message_type)
-        end
-
         def set_neighborhood
           @neighborhood = Neighborhood.find(params[:neighborhood_id])
+        end
+
+        def set_chat_message
+          @chat_message = ChatMessage.where(id: params[:chat_message_id], messageable_type: :Neighborhood).first
+        end
+
+        def chat_messages_params
+          params.require(:chat_message).permit(:content, :message_type)
         end
 
         def join_request
