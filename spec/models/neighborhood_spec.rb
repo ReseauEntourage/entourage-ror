faf0ab621ec3b29f168d6e91a3e925d7a39f6873
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-04-29 17:53:01 +0200

    EN-4711 filter neighborhoods inside user travel_distance

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Neighborhood, :type => :model do
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
 
+  describe 'inside_perimeter' do
+    let!(:neighborhood) { FactoryBot.create :neighborhood, latitude: 48.86, longitude: 2.35 }
+    let(:travel_distance) { 1 }
+
+    # distance is about 26_500 meters
+    subject { Neighborhood.inside_perimeter(48.80, 2, travel_distance) }
+
+    context 'travel_distance is too low' do
+      it { expect(subject.count).to eq(0) }
+    end
+
+    context 'travel_distance is again too low' do
+      let(:travel_distance) { 25 }
+      it { expect(subject.count).to eq(0) }
+    end
+
+    context 'travel_distance is enough' do
+      let(:travel_distance) { 28 }
+      it { expect(subject.count).to eq(1) }
+    end
+  end
 end
