f5cf4f6961df5465504f5c385c5ef81f1c73b2c2
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-04 11:20:41 +0200

    sms_code should be 10 long max

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates_presence_of [:first_name, :last_name, :organization, :email], if: Proc.new { |u| u.pro? }
   validates_associated :organization, if: Proc.new { |u| u.pro? }
   validates_presence_of [:first_name, :last_name, :email], if: Proc.new { |u| u.org_member? }
-  validates :sms_code, length: { minimum: 6 }
+  validates :sms_code, length: { minimum: 6, maximum: 10 }
   validates_length_of :about, maximum: 200, allow_nil: true
   validates_length_of :password, within: 8..256, allow_nil: true
   validates_inclusion_of :community, in: Community.slugs
