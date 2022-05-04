75cb95e82d142f3fdbb343766fa0e2fb1c713e59
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-04 14:27:25 +0200

    EN-4820 use a setter field 'sms_code_password' to avoir any sms_code to be present in form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -287,7 +287,7 @@ module Admin
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :sms_code, :phone, :organization_id, :use_suggestions, :about, :accepts_emails, :targeting_profile, :partner_id, :admin, :moderator)
+      params.require(:user).permit(:first_name, :last_name, :email, :sms_code, :sms_code_password, :phone, :organization_id, :use_suggestions, :about, :accepts_emails, :targeting_profile, :partner_id, :admin, :moderator)
     end
 
     def email_preferences_params
