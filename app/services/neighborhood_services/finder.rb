600072564601b88a5b1d8cf5a9040b09cb21b621
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-10 13:58:19 +0200

    EC-99 using query search, we do not apply all ordering

--- app/services/neighborhood_services/finder.rb
+++ app/services/neighborhood_services/finder.rb
@@ -1,11 +1,26 @@
 module NeighborhoodServices
   class Finder
-    def self.search user:, q: nil
-      Neighborhood.like(q).not_joined_by(user)
-        .inside_perimeter(user.latitude, user.longitude, user.travel_distance)
-        .order_by_interests_matching(user.interest_list)
-        .order_by_activity
-        .order_by_distance_from(user.latitude, user.longitude)
+    class << self
+      def search user:, q: nil
+        neighborhoods = if q.present?
+          search_by_q(q)
+        else
+          default_search
+        end
+
+        Neighborhood.not_joined_by(user)
+          .inside_perimeter(user.latitude, user.longitude, user.travel_distance)
+          .order_by_distance_from(user.latitude, user.longitude)
+      end
+
+      def search_by_q q
+        Neighborhood.like(q)
+      end
+
+      def default_search
+        Neighborhood.order_by_interests_matching(user.interest_list)
+          .order_by_activity
+      end
     end
   end
 end
