7a0789e01062b84a75c9902388ff586b260add48
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-12 15:49:41 +0200

    EN-4711 enhance outing creation

---
+++ spec/acceptance/api/v1/neighborhoods/outings_controller_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource Api::V1::Neighborhoods::OutingsController do
+  explanation "Outings"
+  header "Content-Type", "application/json"
+
+  post '/api/v1/neighborhoods/:neighborhood_id/outings' do
+    route_summary "Create outings"
+    # route_description "no description"
+
+    parameter :token, type: :string, required: true
+    parameter :neighborhood_id, type: :integer, required: true
+
+    with_options :scope => :outing, :required => true do
+      parameter :title, "Title", type: :string
+      parameter :description, "Description", type: :string, :required => false
+      parameter :event_url, "Event url", type: :string, :required => false
+      parameter :latitude, "Latitude", type: :string
+      parameter :longitude, "Longitude", type: :string
+      parameter :metadata, "Metadata", :required => false
+      with_options :scope => "outing[metadata]", :required => true do
+        parameter :starts_at, "Start time"
+        parameter :ends_at, "End time"
+        parameter :place_name, "Place name"
+        parameter :street_address, "Street address"
+        parameter :google_place_id, "Google place ID"
+      end
+    end
+
+    let(:user) { FactoryBot.create(:pro_user) }
+    let(:neighborhood) { FactoryBot.create(:neighborhood) }
+    let(:neighborhood_id) { neighborhood.id }
+
+    let(:outing) { build :outing }
+
+    let(:raw_post) { {
+      token: user.token,
+      outing: {
+        title: 'Groupe de voisins',
+        latitude: outing.latitude,
+        longitude: outing.longitude,
+        metadata: {
+          starts_at: outing.metadata[:starts_at],
+          ends_at: outing.metadata[:ends_at],
+          place_name: outing.metadata[:place_name],
+          street_address: outing.metadata[:street_address],
+          google_place_id: outing.metadata[:google_place_id],
+        }
+      }
+    }.to_json }
+
+    context '201' do
+      let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
+
+      context 'Simple outing' do
+        example_request 'Create outing' do
+          expect(response_status).to eq(201)
+          expect(JSON.parse(response_body)).to have_key('outing')
+        end
+      end
+
+      context 'outing as a comment of another outing' do
+        let!(:parent_id) { outing.id }
+
+        example_request 'Create outing as a comment of another outing' do
+          expect(response_status).to eq(201)
+          expect(JSON.parse(response_body)).to have_key('outing')
+        end
+      end
+    end
+
+    context '401' do
+      example_request 'Cannot create outings if the user does not belong to the neighborhood' do
+        expect(response_status).to eq(401)
+      end
+    end
+  end
+end
--- spec/controllers/api/v1/neighborhoods/outings_controller_spec.rb
+++ spec/controllers/api/v1/neighborhoods/outings_controller_spec.rb
@@ -5,13 +5,36 @@ describe Api::V1::Neighborhoods::OutingsController do
   let(:neighborhood) { create :neighborhood }
 
   describe 'POST create' do
+    let(:params) { {
+      title: "Apéro Entourage",
+      # description: "Apéro Entourage",
+      # event_url: 'bar',
+      latitude: 48.868959,
+      longitude: 2.390185,
+      metadata: {
+        starts_at: "2018-09-04T19:30:00+02:00",
+        ends_at: "2018-09-04T20:30:00+02:00",
+        place_name: "Le Dorothy",
+        street_address: "85 bis rue de Ménilmontant, 75020 Paris, France",
+        google_place_id: "ChIJFzXXy-xt5kcRg5tztdINnp0",
+      }
+    } }
+
     context "not signed in" do
       before { post :create, params: { neighborhood_id: neighborhood.to_param, outing: { title: "foobar", longitude: 1.123, latitude: 4.567 } } }
       it { expect(response.status).to eq(401) }
       it { expect(Entourage.count).to eq(0) }
     end
 
+    context "not joined" do
+      before { post :create, params: { neighborhood_id: neighborhood.to_param, outing: { title: "foobar", longitude: 1.123, latitude: 4.567 }, token: user.token } }
+      it { expect(response.status).to eq(401) }
+      it { expect(Entourage.count).to eq(0) }
+    end
+
     context "signed in" do
+      let!(:join_request) { FactoryBot.create(:join_request, joinable: neighborhood, user: user, status: :accepted) }
+
       context "without all required parameters" do
         before { post :create, params: { neighborhood_id: neighborhood.to_param, outing: {
           title: "foobar",
@@ -25,29 +48,7 @@ describe Api::V1::Neighborhoods::OutingsController do
       end
 
       context "with all required parameters" do
-        before { post :create, params: { neighborhood_id: neighborhood.to_param, outing: {
-          title: 'Groupe de voisins',
-          description: 'Groupe de voisins',
-          entourage_type: 'ask_for_help',
-          display_category: 'social',
-          latitude: 1,
-          longitude: 2,
-          metadata: {
-            starts_at: {
-              date: "2018-09-04",
-              hour: 7,
-              min: 30,
-            },
-            ends_at: {
-              date: "2018-09-05",
-              hour: 7,
-              min: 30,
-            },
-            google_place_id: "ChIJFzXXy-xt5kcRg5tztdINnp0",
-            place_name: "Le Dorothy",
-            street_address: "85 bis rue de Ménilmontant, 75020 Paris, France",
-          },
-        }, token: user.token } }
+        before { post :create, params: { neighborhood_id: neighborhood.to_param, outing: params, token: user.token } }
 
         it { expect(response.status).to eq(201) }
         it { expect(Entourage.count).to eq(1) }
