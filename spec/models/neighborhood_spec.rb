157bc29168b0b3f7f25dfe5bfd00379523bae69a
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-19 14:09:58 +0200

    neighborhood description is required

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Neighborhood, :type => :model do
   it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
   it { should validate_presence_of(:latitude) }
   it { should validate_presence_of(:longitude) }
 
