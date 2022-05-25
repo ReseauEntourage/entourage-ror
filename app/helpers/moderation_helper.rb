c9d22a2ecf53dee24e3b72b54d6cba40161f1955
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-05-25 16:30:34 +0200

    EN-4846 users and entourages index should display all moderation area filters

--- app/helpers/moderation_helper.rb
+++ app/helpers/moderation_helper.rb
@@ -4,4 +4,18 @@ module ModerationHelper
       ["#{moderator.full_name} (#{moderator.email})", moderator.id]
     end
   end
+
+  def moderation_area_filters
+    departement_filters = ModerationArea.only_departements.map do |departement|
+      [departement, {country_eq: 'FR', postal_code_start: departement}]
+    end.to_h
+
+    {
+      "Partout" => {},
+    }.merge(departement_filters).merge({
+      "Hors zone" => {
+        postal_code_not_start_all: ModerationArea.only_departements
+      },
+    })
+  end
 end
