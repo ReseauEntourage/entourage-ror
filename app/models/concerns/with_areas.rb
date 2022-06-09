99b15d2d14a6acd75e7aa39e4f26535e278e3190
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-06-09 11:35:43 +0200

    EC-196 recommandations have user_goals and areas
    refactor: code duplication from announcements

---
+++ app/models/concerns/with_areas.rb
@@ -0,0 +1,17 @@
+module WithAreas
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :areas, :jsonb_set
+
+    validates :areas, presence: true
+
+    scope :for_areas, -> (area_slugs) {
+      where("areas ?| array[%s]" % area_slugs.map { |a| ApplicationRecord.connection.quote(a) }.join(','))
+    }
+
+    before_validation do
+      areas.reject!(&:blank?)
+    end
+  end
+end
