afc2131dd910f222d8930af23bb160d3cb3a5dbf
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-06-09 15:04:51 +0200

    EC-196 add webview to recommandations finder

--- app/services/user_services/recommandations.rb
+++ app/services/user_services/recommandations.rb
@@ -12,7 +12,7 @@ module UserServices
 
         next unless self.class.instance_methods.include?(method)
 
-        key, id = send(method)
+        key, id = send(method, recommandation)
 
         recommandation.instance_key = key
         recommandation.instance_id = id
@@ -20,35 +20,39 @@ module UserServices
       end.compact
     end
 
-    def find_user
+    def find_user recommandation
       [:id, @user.id]
     end
 
-    def find_profile
+    def find_profile recommandation
       [:id, @user.id]
     end
 
-    def find_neighborhood
+    def find_neighborhood recommandation
       [:id, Neighborhood.last.id]
     end
 
-    def find_outing
+    def find_outing recommandation
       [:id, Entourage.where(group_type: :outing).last.id]
     end
 
-    def find_resource
+    def find_resource recommandation
       # [:id, Resource.last.id]
     end
 
-    def find_conversation
+    def find_webview recommandation
+      [:url, recommandation.url]
+    end
+
+    def find_conversation recommandation
       [:id, Entourage.where(group_type: :conversation).last.id]
     end
 
-    def find_contribution
+    def find_contribution recommandation
       [:id, Entourage.where(entourage_type: :contribution).last.id]
     end
 
-    def find_ask_for_help
+    def find_ask_for_help recommandation
       [:id, Entourage.where(entourage_type: :ask_for_help).last.id]
     end
   end
