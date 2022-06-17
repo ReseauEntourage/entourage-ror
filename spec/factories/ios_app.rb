a1c581562f46aa959073d03bc499fd26bab20e3d
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-06-17 15:50:01 +0200

    fix syntax

--- spec/factories/ios_app.rb
+++ spec/factories/ios_app.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :ios_app, class: Rpush::Apnsp8::App do
     name { :entourage }
     environment { :development }
-    apn_key { :apn_key) }
+    apn_key { :apn_key }
     apn_key_id { :apn_key_id }
     team_id { :team_id }
     bundle_id { :bundle_id }
