6490f4a96e4261cc0ab97a1b63d432d25ca87cfd
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-06-21 15:47:45 +0200

    EC-182 recommandation status

--- app/helpers/neighborhoods_helper.rb
+++
@@ -1,9 +0,0 @@
-module NeighborhoodsHelper
-  def neighborhood_status neighborhood
-    type_to_class = {
-      "active" => "label-info",
-      "deleted" => "label-warning"
-    }
-    content_tag :span, neighborhood.status, class: "label #{type_to_class[neighborhood.status]}"
-  end
-end
