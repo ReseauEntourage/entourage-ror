7f333b50b48d7b2a56d4d7d156877de968db9a82
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-07-01 09:43:57 +0200

    EC-273 add allowance to outing duplicate

--- app/controllers/api/v1/outings_controller.rb
+++ app/controllers/api/v1/outings_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class OutingsController < Api::V1::BaseController
       before_action :set_outing, only: [:show, :duplicate]
+      before_action :allowed_duplicate?, only: [:duplicate]
 
       def index
         render json: Outing.order_by_starts_at.page(page).per(per), root: :outings, each_serializer: ::V1::NeighborhoodOutingSerializer, scope: {
@@ -60,6 +61,16 @@ module Api
 
       def per
         params[:per] || 25
+      end
+
+      def allowed_duplicate?
+        unless current_user == Outing.find(params[:id]).user
+          render json: { message: 'unauthorized user' }, status: :unauthorized
+        end
+
+        unless Outing.find(params[:id]).recurrence.present?
+          render json: { message: 'no recurrency settings' }, status: :unauthorized
+        end
       end
     end
   end
