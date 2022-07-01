017cc9ca3bb33c885e7189ba4d765200c9748252
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-07-01 14:07:51 +0200

    EC-273 ensure only active outing recurrence might be duplicated

--- app/models/outing_recurrence.rb
+++ app/models/outing_recurrence.rb
@@ -1,11 +1,14 @@
 class OutingRecurrence < ApplicationRecord
   after_initialize :set_identifier, if: :new_record?
 
+  default_scope { where(continue: true) }
+
   def set_identifier
     self.identifier ||= SecureRandom.hex(8)
   end
 
   def new_outing
+    return unless continue
     return unless last_outing.present?
 
     last_outing.dup
