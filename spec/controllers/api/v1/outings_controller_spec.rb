43d722411bf0897de9fb6082f23ecea144eae6cf
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-07-11 15:57:20 +0200

    EC-306 add posts to outing home serializer

--- spec/controllers/api/v1/outings_controller_spec.rb
+++ spec/controllers/api/v1/outings_controller_spec.rb
@@ -17,6 +17,8 @@ describe Api::V1::OutingsController do
     it { expect(response.status).to eq(200) }
     it { expect(subject).to have_key("outings") }
     it { expect(subject["outings"].count).to eq(1) }
+    it { expect(subject["outings"][0]).to have_key("posts") }
+    it { expect(subject["outings"][0]).to have_key("members") }
     it { expect(subject["outings"][0]["members"]).to eq([{
       "id" => outing.user_id,
       "display_name" => "John D.",
