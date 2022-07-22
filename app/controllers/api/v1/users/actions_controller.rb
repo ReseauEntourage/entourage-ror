894578453c7771c8a1dba9978657dac4e71440d6
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-07-22 17:01:04 +0200

    EC-363 list actions user has created

---
+++ app/controllers/api/v1/users/actions_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    module Users
+      class ActionsController < Api::V1::BaseController
+        before_action :set_user
+
+        def index
+          page = params[:page] || 1
+          per = [(params[:per].try(:to_i) || 25), 25].min
+
+          actions = Action.where(user_id: @user.id)
+            .order(created_at: :desc)
+            .page(page)
+            .per(per)
+
+          render json: actions, status: 200, each_serializer: ::V1::Actions::GenericSerializer, scope: { user: current_user }
+        end
+
+        private
+
+        def set_user
+          @user = if params[:user_id] == "me"
+            current_user
+          else
+            User.find(params[:user_id])
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v1/users/actions_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Api::V1::Users::ActionsController, :type => :controller do
+  render_views
+
+  let(:user) { FactoryBot.create(:pro_user) }
+  let(:result) { JSON.parse(response.body) }
+
+  describe 'GET index' do
+    context "not logged in" do
+      before { get :index, params: { user_id: user.id } }
+      it { expect(response.status).to eq(401) }
+    end
+
+    context "logged in" do
+      let!(:action_created) { FactoryBot.create(:contribution, user: user, created_at: 1.day.ago) }
+      let!(:action_joined) { FactoryBot.create(:contribution, user: user, created_at: 2.day.ago) }
+      let!(:action_other) { FactoryBot.create(:contribution) }
+
+      before { get :index, params: { user_id: user.id, token: user.token } }
+
+      it { expect(response.status).to eq(200) }
+      it { expect(result["actions"].count).to eq(2) }
+      it { expect(result["actions"].map {|actions| actions["id"]}).to eq([action_created.id, action_joined.id]) }
+    end
+  end
+end
