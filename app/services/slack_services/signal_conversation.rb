d15eaf53b61dcf04ac191b55b72c99b9cb08e971
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-08-25 17:22:46 +0200

    EC-453 add conversations/:id/report

---
+++ app/services/slack_services/signal_conversation.rb
@@ -0,0 +1,38 @@
+module SlackServices
+  class SignalConversation < Notifier
+    def initialize conversation:, reporting_user:, signals:, message:
+      @conversation = conversation
+      @reporting_user = find_user(reporting_user)
+      @signals = signals
+      @message = message
+    end
+
+    def env
+      ENV['SLACK_SIGNAL']
+    end
+
+    def payload
+      {
+        text: "<@#{slack_moderator_id(@conversation)}> ou team modération (département : #{departement(@conversation) || 'n/a'}) pouvez-vous vérifier cette conversation ?",
+        attachments: [
+          {
+            text: "Conversation : #{@conversation.title} #{link_to_conversation(@conversation)}"
+          },
+          {
+            text: "Signalée par : #{@reporting_user.full_name} #{link_to_user(@reporting_user.id)}"
+          },
+          {
+            text: "Catégories #{@signals.join(', ')}, message : #{@message}"
+          },
+        ]
+      }
+    end
+
+    def payload_adds
+      {
+        username: "Signalement d'une conversation",
+        channel: webhook('channel'),
+      }
+    end
+  end
+end
