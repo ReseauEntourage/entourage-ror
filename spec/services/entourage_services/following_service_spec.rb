579bfc4016e713e0681ca49946932d8054725951
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-09-05 16:39:11 +0200

    EC-487 remove push notifications related to invitations

--- spec/services/entourage_services/following_service_spec.rb
+++ spec/services/entourage_services/following_service_spec.rb
@@ -15,6 +15,7 @@ describe FollowingService do
 
     it {
       subject
+
       expect(EntourageInvitation.last.attributes).to include(
         "invitable_id" => action.id,
         "inviter_id" => partner_user.id,
@@ -26,29 +27,14 @@ describe FollowingService do
     }
 
     it {
-      expect_any_instance_of(PushNotificationService).to receive(:send_notification).with(
-        "PARTNER_NAME",
-        "Foobar",
-        "PARTNER_NAME vous invite à rejoindre une action.",
-        [following.user],
-        {
-          type: "ENTOURAGE_INVITATION",
-          entourage_id: action.id,
-          group_type: 'action',
-          inviter_id: partner_user.id,
-          invitee_id: following.user.id,
-          invitation_id: 123,
-          instance: "conversations",
-          id: action.id
-        }
-      )
+      expect_any_instance_of(PushNotificationService).not_to receive(:send_notification)
+
       subject
     }
 
     it {
       expect { subject }.to \
-        change { UserServices::UnreadMessages.new(user: following.user).number_of_unread_messages } \
-        .by(0)
+        change { UserServices::UnreadMessages.new(user: following.user).number_of_unread_messages }.by(0)
     }
   end
 end
