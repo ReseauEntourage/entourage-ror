e0d38ce079ba4be176efd726607e1915129f1e57
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-09-15 17:39:07 +0200

    EC-261 fix instance, controller name formats

--- app/services/recommandation_services/completor.rb
+++ app/services/recommandation_services/completor.rb
@@ -1,37 +1,37 @@
 module RecommandationServices
   class Completor
-    attr_accessor :user, :instance_type, :action_name, :params
+    attr_accessor :user, :instance, :action_name, :params
 
     def initialize user:, controller_name:, action_name:, params:
       @user = user
-      @instance_type = controller_name.singularize.to_sym
-      @action_name = action_name
+      @instance = controller_name.singularize.to_sym
+      @action_name = action_name.to_sym
       @params = params
     end
 
     def run
-      return unless Recommandation::INSTANCES.include?(instance_type)
+      return unless Recommandation::INSTANCES.include?(instance)
       return unless Recommandation::ACTIONS.include?(action_name)
 
       method = "after_#{action_name}".to_sym
 
       return unless self.class.instance_methods.include?(method)
-      return unless matched_user_recommandations = send(method, instance_type, params)
+      return unless matched_user_recommandations = send(method, instance, params)
 
       matched_user_recommandations.update_all(completed_at: Time.now)
     end
 
-    def after_index instance_type, params
-      user_recommandations.where(instance_type: instance_type, action: :index)
+    def after_index instance, params
+      user_recommandations.for_instance(instance).where(action: :index)
     end
 
     # @caution does not work with webviews
-    def after_show instance_type, params
-      user_recommandations.where(instance_type: instance_type, action: :show, instance_id: params[:id])
+    def after_show instance, params
+      user_recommandations.for_instance(instance).where(action: :show, instance_id: params[:id])
     end
 
-    def after_create instance_type, params
-      user_recommandations.where(instance_type: instance_type, action: :create)
+    def after_create instance, params
+      user_recommandations.for_instance(instance).where(action: :create)
     end
 
     private
