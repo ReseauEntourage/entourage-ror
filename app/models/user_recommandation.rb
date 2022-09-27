1e9cfd280f4ac29cc55d3f92ec3786160ed7d02f
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-09-27 12:23:08 +0200

    EC-261 do not propose skipped recommandations

--- app/models/user_recommandation.rb
+++ app/models/user_recommandation.rb
@@ -5,7 +5,11 @@ class UserRecommandation < ApplicationRecord
   belongs_to :recommandation
 
   scope :active, -> { where(completed_at: nil, skipped_at: nil) }
+
   scope :completed_by, -> (user) { where(user_id: user.id).where.not(completed_at: nil) }
+  scope :skipped_by, -> (user) { where(user_id: user.id).where.not(skipped_at: nil) }
+  scope :processed_by, -> (user) { UserRecommandation.completed_by(user).or(UserRecommandation.skipped_by(user)) }
+
   scope :to_be_congratulated, -> { where.not(completed_at: nil).where(congrats_at: nil) }
   scope :for_instance, -> (instance) { where(instance_type: instance.to_s.classify) }
 
