092034ef6984504083219ac80082ceae3d21eb80
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-11-21 10:38:40 +0100

    EN-5082 rename inapp_notification_configurations to notification_configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_104201) do
+ActiveRecord::Schema.define(version: 2022_11_21_101400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -372,12 +372,6 @@ ActiveRecord::Schema.define(version: 2022_11_16_104201) do
     t.index ["user_id", "partner_id"], name: "index_followings_on_user_id_and_partner_id", unique: true
   end
 
-  create_table "inapp_notification_configurations", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.jsonb "configuration"
-    t.index ["user_id"], name: "index_inapp_notification_configurations_on_user_id"
-  end
-
   create_table "inapp_notifications", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "instance", null: false
@@ -508,6 +502,12 @@ ActiveRecord::Schema.define(version: 2022_11_16_104201) do
     t.boolean "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "notification_configurations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.jsonb "configuration"
+    t.index ["user_id"], name: "index_notification_configurations_on_user_id"
   end
 
   create_table "old_atd_synchronizations", id: :serial, force: :cascade do |t|
