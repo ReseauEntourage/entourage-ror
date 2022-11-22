8ddf38d81e62bee83edf9141133f9b4cfd8d1175
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-11-22 09:42:49 +0100

    EN-5083 refactor

--- app/models/notification_permission.rb
+++ app/models/notification_permission.rb
@@ -8,6 +8,23 @@ class NotificationPermission < ApplicationRecord
     true
   end
 
+  # accessors
+  def neighborhood
+    return true unless permissions && permissions.has_key?("neighborhood")
+    permissions["neighborhood"]
+  end
+
+  def outing
+    return true unless permissions && permissions.has_key?("outing")
+    permissions["outing"]
+  end
+
+  def private_chat_message
+    return true unless permissions && permissions.has_key?("private_chat_message")
+    permissions["private_chat_message"]
+  end
+
+  # setters
   def neighborhood= accepted
     permissions[:neighborhood] = ActiveModel::Type::Boolean.new.cast(accepted)
   end
--- app/serializers/v1/notification_permission_serializer.rb
+++ app/serializers/v1/notification_permission_serializer.rb
@@ -3,17 +3,5 @@ module V1
     attributes :neighborhood,
       :outing,
       :private_chat_message
-
-    def neighborhood
-      object.permissions["neighborhood"]
-    end
-
-    def outing
-      object.permissions["outing"]
-    end
-
-    def private_chat_message
-      object.permissions["private_chat_message"]
-    end
   end
 end
