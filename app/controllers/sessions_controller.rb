3f9599175225bed9df481b04cdb3d2db716a68bc
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2022-12-20 16:02:49 +0100

    deactivate deprecated /sessions route
    This deactivation aims at reduces the effect of an attack on this route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = UserServices::UserAuthenticator.authenticate_by_phone_and_sms(phone: params[:phone], sms_code: params[:sms_code])
-
-    if user
-      session[:user_id] = user.id
-      session[:admin_user_id] = user.admin ? user.id : nil
-
-      if user.public? && !user.admin
-        return redirect_to edit_public_user_user_path(user)
-      end
-
-      redirect_to(params[:continue].presence || root_path)
-    else
-      flash[:error] = "Identifiants incorrects"
-      redirect_path =
-        if params[:continue].present?
-          new_session_path(continue: params[:continue])
-        else
-          new_session_path
-        end
-      redirect_to redirect_path
-    end
+    head 200
   end
 
   def destroy
