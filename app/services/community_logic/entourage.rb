1328100d0bb0767e550ebd000f55644a14e445c3
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-01-04 11:21:39 +0100

    EN-4769 refactor

--- app/services/community_logic/entourage.rb
+++ app/services/community_logic/entourage.rb
@@ -1,12 +1,7 @@
 class CommunityLogic::Entourage < CommunityLogic::Common
   def self.group_created group
-    case group.group_type
-    when 'action'
-      # do not send email anymore
-    when 'outing' # event
-      GroupMailer.event_created_confirmation(group).deliver_later
-    else # not an action or an event. shouldn't happen.
-      # nothing for now
-    end
+    return unless group.group_type == 'outing'
+
+    GroupMailer.event_created_confirmation(group).deliver_later
   end
 end
