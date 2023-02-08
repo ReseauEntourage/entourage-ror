6cc8c52b4cae64faa5b18c310078fa39d8b5dfae
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-02-08 14:42:42 +0100

    EN-5605 backoffice: deprecates entourage#show_joins, entourage#show_invitations

--- spec/controllers/admin/entourages_controller_spec.rb
+++ spec/controllers/admin/entourages_controller_spec.rb
@@ -108,18 +108,6 @@ describe Admin::EntouragesController do
     it { expect(assigns(:entourage)).to eq(entourage) }
   end
 
-  describe "GET #show_joins" do
-    let(:entourage) { FactoryBot.create(:entourage) }
-    before { get :show_joins, params: { id: entourage.to_param } }
-    it { expect(assigns(:entourage)).to eq(entourage) }
-  end
-
-  describe "GET #show_invitations" do
-    let(:entourage) { FactoryBot.create(:entourage) }
-    before { get :show_invitations, params: { id: entourage.to_param } }
-    it { expect(assigns(:entourage)).to eq(entourage) }
-  end
-
   describe "GET #show_messages" do
     let(:entourage) { FactoryBot.create(:entourage) }
     before { get :show_messages, params: { id: entourage.to_param } }
