a7b6ada65461bca39b855747721ce847086b7c53
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-02-13 12:43:39 +0100

    EN-5605 rspecs to ensure entourage_moderation is created

--- spec/factories/moderation_areas.rb
+++ spec/factories/moderation_areas.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :moderation_area do
+    moderator { association(:public_user) }
     departement { '99' }
     name { "Zone 1" }
+
     welcome_message_1_offer_help     { "WELCOME {{first_name}} (R)" }
     welcome_message_2_offer_help     { "SEE YOU SOON (R)" }
     welcome_message_1_ask_for_help   { "WELCOME {{first_name}} (S)" }
