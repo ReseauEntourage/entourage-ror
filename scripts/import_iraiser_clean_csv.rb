9b0d7e4e9a32ec4d3fa84cfca8eaf54faf6e56be
PaulEntourage <112417197+PaulEntourage@users.noreply.github.com>
2023-03-03 16:29:33 +0100

    EN-5623 donation database structure

--- scripts/import_iraiser_clean_csv.rb
+++ scripts/import_iraiser_clean_csv.rb
@@ -7,7 +7,7 @@ if in_path.nil? || out_path.nil?
 end
 
 def clean_file in_path, out_path
-  columns = [:donator_sex, :donator_country, :donator_postcode, :donator_city, :payment_frequency, :campaign_type, :context_query_string, :context_referer, :donation_amount, :donation_category, :donation_next_transaction_date, :donation_reference, :donation_status, :donation_type, :donation_validation_date, :donator_email, :payment_card_renew_key, :payment_gateway_account, :payment_gateway_ticket_EMAIL, :payment_gateway_ticket_metadata_donator_email, :payment_gateway_ticket_RECEIVERBUSINESS, :payment_gateway_ticket_RECEIVEREMAIL, :payment_succeeded, :sympathizer_id]
+  columns = [:sympathizer_birthdate, :sympathizer_donation_regular_amount, :sympathizer_donation_regular_last_date, :sympathizer_donation_regular_last_year_total, :sympathizer_id, :sympathizer_optin_date, :sympathizer_donation_once_last_date, :sympathizer_donation_regular_first_date, :payment_type, :donator_sex, :donator_country, :donator_postcode, :donator_city, :payment_frequency, :campaign_type, :context_query_string, :context_referer, :donation_amount, :donation_category, :donation_next_transaction_date, :donation_reference, :donation_status, :donation_type, :donation_validation_date, :donator_email, :payment_card_renew_key, :payment_gateway_account, :payment_gateway_ticket_EMAIL, :payment_gateway_ticket_metadata_donator_email, :payment_gateway_ticket_RECEIVERBUSINESS, :payment_gateway_ticket_RECEIVEREMAIL, :payment_succeeded, :sympathizer_id]
 
   csv_table = CSV.read(in_path, col_sep: ';', quote_char: nil, headers: true, header_converters: :symbol, encoding: 'iso-8859-1:utf-8')
   (csv_table.headers - columns).each do |column|
