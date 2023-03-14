d11669996ca03bb061e4daf4593baa203075a858
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-03-14 15:26:42 +0100

    temporary fix: remove index from chat_messages.uuid_v2 to easier migration

--- db/migrate/20230307093302_add_uuid_v2_to_chat_messages.rb
+++ db/migrate/20230307093302_add_uuid_v2_to_chat_messages.rb
@@ -8,11 +8,11 @@ class AddUuidV2ToChatMessages < ActiveRecord::Migration[5.2]
 
     change_column :chat_messages, :uuid_v2, :string, limit: 12, null: false
 
-    add_index :chat_messages, :uuid_v2, unique: true
+    # add_index :chat_messages, :uuid_v2, unique: true
   end
 
   def down
-    remove_index :chat_messages, :uuid_v2
+    # remove_index :chat_messages, :uuid_v2
 
     remove_column :chat_messages, :uuid_v2
   end
