3c80cdf94a54aa8ded107225ff24bd4688e40385
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-03-22 11:07:36 +0100

    cmath is deprecated
    tour functionalities are not maintained anymore

--- app/services/tour_services/tour_simplifier.rb
+++ app/services/tour_services/tour_simplifier.rb
@@ -1,4 +1,4 @@
-require 'cmath'
+# require 'cmath'
 
 module TourServices
   class TourSimplifier
@@ -9,16 +9,6 @@ module TourServices
     end
 
     def simplified_points
-      original_points = tour.tour_points.ordered
-      return original_points if original_points.count <= 10
-
-      points = original_points.map {|tp| {x: R*CMath.cos(tp.latitude)*CMath.cos(tp.longitude),
-                                           y: R*CMath.cos(tp.latitude)*CMath.sin(tp.longitude),
-                                           id: tp.id}}
-      tolerance = 1
-      high_quality = true
-      simplified_points = SimplifyRb.simplify(points, tolerance, high_quality)
-      original_points.where(id: simplified_points.map {|point| point[:id]})
     end
 
     private
