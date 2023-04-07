5f574e0561c6f95bba71f2d65495bd89c20ca659
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-04-07 15:43:21 +0200

    EN-4012 fix login_history rspec

--- spec/models/login_history_spec.rb
+++ spec/models/login_history_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe LoginHistory, :type => :model do
   it { should validate_presence_of(:connected_at) }
 
   describe "unique by hour" do
-    let!(:previous_login) { LoginHistory.create!(user_id: 1, connected_at: DateTime.parse("10/10/2010 21:15:00")) }
-    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be false }
-    it { expect(LoginHistory.new(user_id: 2, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be true }
-    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 21:01:00").utc).save).to be false }
-    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("10/10/2010 20:44:00")).save).to be true }
-    it { expect(LoginHistory.new(user_id: 1, connected_at: DateTime.parse("09/10/2010 21:44:00")).save).to be true }
+    let(:user_1) { create(:public_user) }
+    let(:user_2) { create(:public_user) }
+    let!(:previous_login) { create(:login_history, user: user_1, connected_at: DateTime.parse("10/10/2010 21:15:00")) }
+
+    it { expect(LoginHistory.new(user: user_1, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be false }
+    it { expect(LoginHistory.new(user: user_2, connected_at: DateTime.parse("10/10/2010 21:45:00").utc).save).to be true }
+    it { expect(LoginHistory.new(user: user_1, connected_at: DateTime.parse("10/10/2010 21:01:00").utc).save).to be false }
+    it { expect(LoginHistory.new(user: user_1, connected_at: DateTime.parse("10/10/2010 20:44:00")).save).to be true }
+    it { expect(LoginHistory.new(user: user_1, connected_at: DateTime.parse("09/10/2010 21:44:00")).save).to be true }
   end
 end
