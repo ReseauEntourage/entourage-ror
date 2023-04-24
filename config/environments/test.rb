107590b61492ec2f4d060bbd5b6cf34818a49898
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-04-24 16:16:38 +0200

    run: bin/rails app:update; and update all configurations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,12 +1,14 @@
 require "active_support/core_ext/integer/time"
 
+# The test environment is used exclusively to run your application's
+# test suite. You never need to work with it otherwise. Remember that
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs. Don't rely on the data there!
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
+  # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
   config.action_view.cache_template_loading = true
@@ -19,7 +21,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
