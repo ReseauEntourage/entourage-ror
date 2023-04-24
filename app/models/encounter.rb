531c53795200cc14ac8ce3cc57d5343e00bf79a8
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-04-24 16:40:51 +0200

    attr_encrypted has migrated and fails. We dont need it anymore as long as it is only used for tours. See also: https://pagertree.com/blog/migrate-attr_encrypted-to-rails-7-active-record-encrypts

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -5,7 +5,7 @@ class Encounter < ApplicationRecord
   validates :date, :street_person_name, :tour, presence: true
   validates :latitude, :longitude, presence: true, numericality: true
 
-  attr_encrypted :message, insecure_mode: true, key: :crypting_key, mode: :single_iv_and_salt, algorithm: 'aes-256-cbc', if: :crypting_key_present?
+  # attr_encrypted :message, insecure_mode: true, key: :crypting_key, mode: :single_iv_and_salt, algorithm: 'aes-256-cbc', if: :crypting_key_present?
 
   alias_attribute :voice_message, :voice_message_url
 
