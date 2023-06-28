714ffd5ad6b0d662b541c6e185c05cf0317c597f
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-06-28 10:14:54 +0200

    add sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -49,4 +49,16 @@ Sidekiq.configure_server do |config|
   ActiveSupport.on_load(:after_initialize) do
     Rpush.embed unless Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
   end
+
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
