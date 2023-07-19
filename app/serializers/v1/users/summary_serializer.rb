5f8aed2b12297dae2ddf209fcb6f9459dee02fda
Nicolas Ech Chafaï <75681929+nicolas-entourage@users.noreply.github.com>
2023-07-19 11:32:09 +0200

    EN-6269 fix last_unclosed_action_notification_at new value

--- app/serializers/v1/users/summary_serializer.rb
+++ app/serializers/v1/users/summary_serializer.rb
@@ -46,16 +46,16 @@ module V1
       end
 
       def unclosed_action
-        last_unclosed_action_notification_at = object.last_unclosed_action_notification_at_and_update!
-
         actions = Entourage.where(user: object).action.active.where("created_at < ?", UNCLOSED_ACTION_ALERT.ago)
-        actions = actions.where("created_at > ?", last_unclosed_action_notification_at) if last_unclosed_action_notification_at.present?
+        actions = actions.where("created_at > ?", object.last_unclosed_action_notification_at) if object.last_unclosed_action_notification_at.present?
         actions = actions.order_by_entourage_type(:ask_for_help).order(:created_at)
         action = actions.select(:entourage_type, :id).first
 
         return unless action.present?
 
         action = action.contribution? ? Contribution.find(action.id) : Solicitation.find(action.id)
+
+        object.set_last_unclosed_action_notification_at_and_save(action.created_at)
 
         V1::ActionSerializer.new(action).as_json
       end
--- app/services/user_services/options.rb
+++ app/services/user_services/options.rb
@@ -12,15 +12,12 @@ module UserServices
       define_method("#{option}=") do |value|
         options[option] = value
       end
-    end
 
-    def last_unclosed_action_notification_at_and_update!
-      datetime = last_unclosed_action_notification_at
+      define_method("set_#{option}_and_save") do |value|
+        options[option] = value
 
-      self.last_unclosed_action_notification_at = Time.zone.now
-      self.save
-
-      datetime
+        self.save
+      end
     end
   end
 end
