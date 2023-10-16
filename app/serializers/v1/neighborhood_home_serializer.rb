d03c9b4fe7bb2c45b92695dbac68ca614fe96c0b
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2023-10-16 14:23:02 +0200

    EN-6308 add translation to NeighborhoodHome serializer

--- app/serializers/v1/neighborhood_home_serializer.rb
+++ app/serializers/v1/neighborhood_home_serializer.rb
@@ -33,6 +33,13 @@ module V1
       TranslationServices::Translator.new(object).translate(scope[:user].lang, :name) || object.name
     end
 
+    def description
+      return object.description unless scope && scope[:user].present?
+      return object.description unless scope[:user].lang.present?
+
+      TranslationServices::Translator.new(object).translate(scope[:user].lang, :description) || object.description
+    end
+
     def member
       return false unless scope && scope[:user]
 
