7ee92f525d8026f7dfbb30c134d4ec316167e628
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2023-12-08 14:43:03 +0100

    EN-6561 backoffice: sort moderators

--- app/helpers/moderation_helper.rb
+++ app/helpers/moderation_helper.rb
@@ -2,7 +2,7 @@ module ModerationHelper
   def moderators_for_select
     moderators = User.where("roles->>0 = 'moderator'").map do |moderator|
       ["#{moderator.full_name} (#{moderator.email})", moderator.id]
-    end
+    end.sort_by { |moderator| moderator.first.downcase }
   end
 
   def moderation_area_filters
