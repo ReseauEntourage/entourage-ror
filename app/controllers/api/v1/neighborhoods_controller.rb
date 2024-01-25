06d841775c5f35df47252ef5e97113bb6b00b4cd
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-01-25 16:31:10 +0100

    Revert "Revert "Merge branch 'set-maximum-pagination'""
    This reverts commit b9ff4059af785921ffe54574e2e8f03b9f354505.

--- app/controllers/api/v1/neighborhoods_controller.rb
+++ app/controllers/api/v1/neighborhoods_controller.rb
@@ -118,10 +118,6 @@ module Api
         params[:page] || 1
       end
 
-      def per
-        params[:per] || 25
-      end
-
       def report_params
         params.require(:report).permit(:message, signals: [])
       end
