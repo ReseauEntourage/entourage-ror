df955fa813265ab82fac29864ba314b43259d6bf
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-02-21 14:30:24 +0100

    EN-6802 fix summary computation

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Survey, :type => :model do
 
     it { expect(survey.choices).to eq(["foo", "bar"]) }
   end
+
+  describe "update_summary" do
+    let(:survey) { create :survey, choices: ["foo", "bar"] }
+    let(:chat_message) { create :chat_message, survey: survey }
+    let(:user) { create :user }
+    let(:survey_response) { create :survey_response, chat_message: chat_message, user: user, responses: [1, 0] }
+
+    before do
+      survey_response
+      survey.update_summary
+    end
+
+    it { expect(survey.summary).to eq([1, 0]) }
+  end
 end
