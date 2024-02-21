e9ef51344eca237ea5a4754ea98c5f53f0bb72ac
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-02-21 09:12:52 +0100

    config local api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "api.entourage.localhost"
   config.hosts << "admin.entourage.localhost"
 
   # In the development environment your application's code is reloaded any time
