52171d85b75d585d19b50d31a4afb8a2b02f77e0
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-03-26 15:32:06 +0100

    push notifications: do not compute badge

--- spec/services/push_notification_service_spec.rb
+++ spec/services/push_notification_service_spec.rb
@@ -11,24 +11,24 @@ describe PushNotificationService, type: :service do
   describe '#send_notification' do
     let!(:user_app1) { FactoryBot.create :user_application, device_family: UserApplication::ANDROID, push_token: 'token 1' }
     let!(:user_app2) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 2' }
-    before { UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 } }
+
     subject! { PushNotificationService.new(android_notification_service, ios_notification_service).send_notification(sender, object, content, User.all, :outing, :foo) }
-    it { expect(android_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app1.push_token, user.community.slug, {}, 1) }
-    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app2.push_token, user.community.slug, {}, 1) }
+    it { expect(android_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app1.push_token, user.community.slug, {}, 0) }
+    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app2.push_token, user.community.slug, {}, 0) }
   end
 
   describe '#send_notification_android_only' do
     let!(:user_app) { FactoryBot.create :user_application, device_family: UserApplication::ANDROID, push_token: 'token 1' }
-    before { UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 } }
+
     subject! { PushNotificationService.new(android_notification_service, ios_notification_service).send_notification(sender, object, content, User.all, :outing, :foo) }
-    it { expect(android_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app.push_token, user.community.slug, {}, 1) }
+    it { expect(android_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app.push_token, user.community.slug, {}, 0) }
   end
 
   describe '#send_notification_ios_only' do
     let!(:user_app) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 2' }
-    before { UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 } }
+
     subject! { PushNotificationService.new(android_notification_service, ios_notification_service).send_notification(sender, object, content, User.all, :outing, :foo) }
-    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app.push_token, user.community.slug, {}, 1) }
+    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app.push_token, user.community.slug, {}, 0) }
   end
 
   describe '#send_notification_4ios_tokens' do
@@ -36,11 +36,11 @@ describe PushNotificationService, type: :service do
     let!(:user_app2) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 2' }
     let!(:user_app3) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 3' }
     let!(:user_app4) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 4' }
-    before { UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 } }
+
     subject! { PushNotificationService.new(android_notification_service, ios_notification_service).send_notification(sender, object, content, User.all, :outing, :foo) }
-    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app4.push_token, user.community.slug, {}, 1) }
-    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app2.push_token, user.community.slug, {}, 1) }
-    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app3.push_token, user.community.slug, {}, 1) }
+    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app4.push_token, user.community.slug, {}, 0) }
+    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app2.push_token, user.community.slug, {}, 0) }
+    it { expect(ios_notification_service).to have_received(:send_notification).with(sender, 'object', 'content', user_app3.push_token, user.community.slug, {}, 0) }
   end
 
   describe '#send_notification do not send to blocked users' do
@@ -48,13 +48,10 @@ describe PushNotificationService, type: :service do
     let!(:ios_user) { FactoryBot.create :user_application, device_family: UserApplication::IOS, push_token: 'token 2' }
 
     context 'with valid users' do
-      before {
-        UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 }
-      }
-
       subject! {
         PushNotificationService.new(android_notification_service, ios_notification_service).send_notification(sender, object, content, [android_user.user, ios_user.user], :outing, :foo)
       }
+
       it { expect(android_notification_service).to have_received(:send_notification) }
       it { expect(ios_notification_service).to have_received(:send_notification) }
     end
@@ -63,8 +60,6 @@ describe PushNotificationService, type: :service do
       before {
         android_user.user.update_attribute(:validation_status, :blocked)
         ios_user.user.update_attribute(:validation_status, :blocked)
-
-        UserServices::UnreadMessages.any_instance.stub(:number_of_unread_messages) { 1 }
       }
 
       subject! {
