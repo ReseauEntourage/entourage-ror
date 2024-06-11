3d5893c3e819e25d64b8c8694550d6e40cc7b0a8
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-06-11 15:34:01 +0200

    weekly_planning outing based on starts_at

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -78,7 +78,7 @@ class MemberMailer < MailjetMailer
                         name: outing.title,
                         address: outing.metadata[:display_address],
                         date: I18n.l(outing.metadata[:starts_at].to_date, format: :short, locale: user.lang),
-                        hour: outing.metadata[:ends_at].strftime("%Hh%M"),
+                        hour: outing.metadata[:starts_at].strftime("%Hh%M"),
                         image_url: outing.image_url_with_size(:landscape_url, :medium),
                         url: outing.share_url
                       }
