4027dc58a5c6c6828c34c2199542d747246cf111
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-07-17 13:07:44 +0200

    EN-7343 change moderation_area regions

--- app/services/moderation_services/region.rb
+++ app/services/moderation_services/region.rb
@@ -1,21 +1,20 @@
 module ModerationServices
   class Region
     DEPARTMENTS_IN_REGION = {
-      aura: %w{ 69 42 01 03 07 15 26 38 43 63 73 74 },
-      bretagne: %w{ 35 56 44 22 53 29 85 },
-      hdf: %w{ 59 62 60 80 02 },
-      idf: %w{ 75 92 93 94 77 78 91 95 },
-      paca: %w{ 13 05 83 06 84 04 },
-      gironde: %w{ 33 },
-      digital_community: (
-        %w{ 08 10 51 52 54 55 57 67 68 88 } +
-        %w{ 18 28 36 37 41 45 } +
-        %w{ 21 25 39 58 70 71 89 } +
-        %w{ 16 17 19 23 24 40 47 64 79 86 87 } +
-        %w{ 09 11 12 32 46 65 66 81 82 31 34 30 } +
-        %w{ 14 } +
-        %w{ 49 72 }
-      )
+      # ouest: bretagne, pays de la loire
+      ouest: %w{ 22 29 35 56 44 49 53 72 85 },
+      # sud_ouest: nouvelle aquitaine, occitanie
+      sud_ouest: %w{ 16 17 19 23 24 33 40 47 64 79 86 87 09 11 12 30 31 32 34 46 48 65 66 81 82 },
+      # sud_est: paca, corse
+      sud_est: %w{ 04 05 06 13 83 84 2A 2B },
+      # aura: aura
+      aura: %w{ 01 03 07 15 26 38 42 43 63 69 73 74 },
+      # nord: hauts de france, normandie
+      nord: %w{ 02 59 60 62 80 14 27 50 61 76 },
+      # idf: idf
+      idf: %w{ 75 77 78 91 92 93 94 95 },
+      # centre_est: centre val de loire, bourgogne, grand est
+      centre_est: %w{ 18 28 36 37 41 45 21 25 39 58 70 71 89 90 08 10 51 52 54 55 57 67 68 88 }
     }.freeze
 
     RegionStruct = Struct.new(:id, :name, :departments) do
