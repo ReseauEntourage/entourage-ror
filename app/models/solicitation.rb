edfe933e0f68865c7eb366dde668c6aad1159227
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-07-23 10:24:55 +0200

    refactor Contribution, Solicitation with Action

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -1,8 +1,4 @@
-class Solicitation < Entourage
-  include Actionable
-  include Sectionable
-  include Recommandable
-
+class Solicitation < Action
   default_scope {
     where(group_type: :action, entourage_type: :ask_for_help)
     .where(pin: false)
