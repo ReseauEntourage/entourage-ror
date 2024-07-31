ba05f3b37190071bd534f06083ab9bfa86c82f96
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-07-31 16:12:27 +0200

    EN-7195 fix category_id not defined

--- app/models/concerns/geolocalizable.rb
+++ app/models/concerns/geolocalizable.rb
@@ -21,7 +21,7 @@ module Geolocalizable
         AVG(latitude) AS latitude,
         AVG(longitude) AS longitude
       ").from(sanitize_sql_array ["(
-        SELECT id, name, latitude, longitude, ST_ClusterKMeans(ST_Transform((ST_SetSRID(ST_MakePoint(longitude, latitude), 4326))::geometry, 4326), LEAST(#{max_clusters}, 30)) OVER () AS cluster_id
+        SELECT id, name, latitude, longitude, category_id, ST_ClusterKMeans(ST_Transform((ST_SetSRID(ST_MakePoint(longitude, latitude), 4326))::geometry, 4326), LEAST(#{max_clusters}, 30)) OVER () AS cluster_id
         FROM pois
         WHERE #{bounding_box_sql}
       ) AS clusters"]).group("cluster_id")
