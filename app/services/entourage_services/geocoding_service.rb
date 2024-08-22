14d76c4d68cce0b47b7b7d98432e2add0ee40f63
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-08-22 14:36:30 +0200

    EN-7354 display distance

--- app/services/entourage_services/geocoding_service.rb
+++ app/services/entourage_services/geocoding_service.rb
@@ -1,5 +1,41 @@
 module EntourageServices
   module GeocodingService
+    EARTH_RADIUS = 6371.0
+
+    def self.distance_rounded lat1, lon1, lat2, lon2
+      distance = distance(lat1, lon1, lat2, lon2)
+      return distance if distance == 0
+
+      magnitude = 10 ** Math.log10(distance.abs).floor
+
+      (distance / magnitude).round(1) * magnitude
+    end
+
+    def self.distance lat1, lon1, lat2, lon2
+      # Convertir les degrés en radians
+      lat1_rad = to_radians(lat1)
+      lon1_rad = to_radians(lon1)
+      lat2_rad = to_radians(lat2)
+      lon2_rad = to_radians(lon2)
+
+      # Différences de latitude et de longitude
+      delta_lat = lat2_rad - lat1_rad
+      delta_lon = lon2_rad - lon1_rad
+
+      # Formule de Haversine
+      a = Math.sin(delta_lat / 2) ** 2 +
+          Math.cos(lat1_rad) * Math.cos(lat2_rad) *
+          Math.sin(delta_lon / 2) ** 2
+
+      c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+      # Distance en kilomètres
+      EARTH_RADIUS * c
+    end
+
+    def self.to_radians(degree)
+      degree * Math::PI / 180
+    end
     def self.geocode entourage
       country, postal_code, city = search_postal_code(entourage.latitude, entourage.longitude)
 
