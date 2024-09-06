7a3f3517c63bf9c744dd73e5870c8c1a5def050c
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-09-06 14:27:14 +0200

    EN-7476 use specific commit for rpush version

--- db/migrate/20240906113300_migrate_rpush_to_fcm.rb
+++ db/migrate/20240906113300_migrate_rpush_to_fcm.rb
@@ -2,8 +2,10 @@ class MigrateRpushToFcm < ActiveRecord::Migration[6.1]
   def up
     app = Rpush::Fcm::App.new
     app.name = "entourage"
-    app.firebase_project_id = "entourage-90011"
-    app.json_key = ENV['RPUSH_FCM_JSON_KEY']
+    # app.firebase_project_id = "entourage-90011"
+    # app.json_key = ENV['RPUSH_FCM_JSON_KEY']
+    app.auth_key = ENV['RPUSH_FCM_JSON_KEY']
+    app.environment = 'production'
     app.connections = 30
     app.save!
   end
