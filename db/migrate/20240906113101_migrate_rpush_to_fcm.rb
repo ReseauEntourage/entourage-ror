90856c6fb601f4611f5b023f26843a6647a4d95a
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-09-09 15:25:55 +0200

    EN-7476 avoid duplication whenever Rpush::Fcm::App already created

--- db/migrate/20240906113101_migrate_rpush_to_fcm.rb
+++ db/migrate/20240906113101_migrate_rpush_to_fcm.rb
@@ -1,13 +1,13 @@
 class MigrateRpushToFcm < ActiveRecord::Migration[6.1]
   def up
-    Rpush::Fcm::App.delete_all
-
-    app = Rpush::Fcm::App.new
-    app.name = "entourage"
-    app.firebase_project_id = "entourage-90011"
-    app.json_key = ENV['RPUSH_FCM_JSON_KEY']
-    app.environment = 'production'
-    app.connections = 30
-    app.save!
+    if Rpush::Fcm::App.none?
+      app = Rpush::Fcm::App.new
+      app.name = "entourage"
+      app.firebase_project_id = "entourage-90011"
+      app.json_key = ENV['RPUSH_FCM_JSON_KEY']
+      app.environment = 'production'
+      app.connections = 30
+      app.save!
+    end
   end
 end
