be46695c1972421388c516ddeeb55f83b4be3e93
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-09-10 13:37:24 +0200

    EN-7238 templating: members order and date align

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -6,7 +6,7 @@ module ConversationsHelper
   def conversation_recipients_display_names recipients, max: 3
     count = recipients.count
 
-    recipients = User.where(id: recipients).select(:id, :first_name, :last_name)
+    recipients = User.where(id: recipients).select(:id, :first_name, :last_name).order(:first_name)
 
     recipient_names = recipients.first(max).map { |u| [UserPresenter.full_name(u), u.id] }
 
