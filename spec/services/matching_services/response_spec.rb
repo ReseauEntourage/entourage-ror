e90a1ae8326b128750db436aadd32a5433039c81
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-11-27 11:35:53 +0100

    EN-7354 more robust .parsed_response and spec

---
+++ spec/services/matching_services/response_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe MatchingServices::Response do
+  describe '#parsed_response' do
+    subject { described_class.new(response: response).parsed_response }
+
+    context 'when response is nil' do
+      let(:response) { nil }
+
+      it { expect(subject).to be_nil }
+    end
+
+    context 'when response has no content' do
+      let(:response) { { "content" => [] } }
+
+      it { expect(subject).to be_nil }
+    end
+
+    context 'when content type is not "text"' do
+      let(:response) { { "content" => [{ "type" => "image", "text" => { "value" => '{"key":"value"}' } }] } }
+
+      it { expect(subject).to be_nil }
+    end
+
+    context 'when content type is "text" but text is malformed' do
+      let(:response) { { "content" => [{ "type" => "text", "text" => { "value" => 'invalid json' } }] } }
+
+      it { expect(subject).to be_nil }
+    end
+
+    context 'when content type is "text" and text contains valid JSON' do
+      let(:response) { { "content" => [{ "type" => "text", "text" => { "value" => '{"key":"value"}' } }] } }
+
+      it { expect(subject).to eq({ "key" => "value" }) }
+    end
+
+    context 'when text contains extraneous data but valid JSON inside' do
+      let(:response) { { "content" => [{ "type" => "text", "text" => { "value" => 'Random text before ```json{"key":"value"}``` and after' } }] } }
+
+      it { expect(subject).to eq({ "key" => "value" }) }
+    end
+  end
+end
