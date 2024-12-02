ac4fbdd0f4625c04a9c1d39c4139a4ca31f0b580
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-12-02 16:27:18 +0100

    EN-7354 backoffice manual send_matching

---
+++ spec/models/action_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Action, type: :model do
+  let(:user) { create(:user) }
+  let(:action) { create(:contribution, user: user) }
+  let(:match) { create(:resource) }
+  let(:other_match) { create(:resource) }
+
+  let!(:matching_with_notification) { create(:matching, instance: action, match: match) }
+  let!(:matching_without_notification) { create(:matching, instance: action, match: other_match) }
+
+  let!(:inapp_notification) { create(:inapp_notification, user: user, instance: :resource, instance_id: match.id, context: :matching) }
+
+  describe '#matchings_with_notifications' do
+    subject { action.matchings_with_notifications }
+
+    it 'returns matchings with inapp_notification_exists set to true when notification exists' do
+      result = subject.find { |m| m.id == matching_with_notification.id }
+      expect(result.inapp_notification_exists).to be_truthy
+    end
+
+    it 'returns matchings with inapp_notification_exists set to false when no notification exists' do
+      result = subject.find { |m| m.id == matching_without_notification.id }
+      expect(result.inapp_notification_exists).to be_falsey
+    end
+
+    it 'does not raise errors when no matchings exist' do
+      action.matchings.destroy_all
+      expect { subject }.not_to raise_error
+      expect(subject).to be_empty
+    end
+  end
+end
