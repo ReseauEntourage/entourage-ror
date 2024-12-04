d5c08247238f3de354eaedc243cc036fa5c64875
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2024-12-04 16:55:48 +0100

    EN-7825 add variables to user_message_broadcasts, welcome chat_message

--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -20,6 +20,105 @@ RSpec.describe ChatMessage, type: :model do
     end
   end
 
+  describe '.interpolate' do
+    let(:user) { create(:public_user,
+      first_name: 'John',
+      email: 'john.doe@example.com',
+      phone: '+33612345678',
+      uuid: '123e4567-e89b-12d3-a456-426614174000',
+      interest_list: 'jeux, cuisine',
+      involvement_list: 'outings, resources',
+      availability: {
+        "1" => ["09:00-12:00", "14:00-18:00"],
+        "2" => ["10:00-12:00"]
+      },
+      address: address,
+      addresses: [address]
+    )}
+
+    let(:other_user) { create(:public_user) }
+    let(:author) { create(:public_user, first_name: 'Alice') }
+
+    let(:neighborhood) { create(:neighborhood, name: "Groupe de Paris") }
+    let(:address) { create :address, city: "Paris" }
+
+    before do
+      allow(user).to receive(:default_neighborhood).and_return(neighborhood)
+      allow(UserPresenter).to receive(:format_first_name).with('John').and_return('John')
+    end
+
+    it 'replaces placeholders in the message' do
+      message = <<~TEXT
+        Hello {{ first_name }},
+        Your email is {{ email }}, and your phone number is {{ phone }}.
+        You live in {{ city }} and your ID is {{ uuid }}.
+        Default neighborhood: {{ default_neighborhood }}.
+        Your interests: {{ interests }}.
+        Your involvements: {{ involvements }}.
+        Availability: {{ availability }}.
+        Interlocutor: {{ interlocutor }}.
+      TEXT
+
+      expected_message = <<~TEXT
+        Hello John,
+        Your email is john.doe@example.com, and your phone number is +33612345678.
+        You live in Paris and your ID is 123e4567-e89b-12d3-a456-426614174000.
+        Default neighborhood: Groupe de Paris.
+        Your interests: Jeux, Cuisine.
+        Your involvements: Participer à des événements de convivialité, Apprendre avec des contenus pédagogiques.
+        Availability: lundi : 09:00-12:00, 14:00-18:00
+        mardi : 10:00-12:00.
+        Interlocutor: Alice.
+      TEXT
+
+      result = ChatMessage.interpolate(message: message, user: user, author: author)
+      expect(result.strip).to eq(expected_message.strip)
+    end
+
+    it 'replaces placeholders in the message when user has little information' do
+      message = <<~TEXT
+        Hello {{ first_name }},
+        Your email is {{ email }}, and your phone number is {{ phone }}.
+        You live in {{ city }} and your ID is {{ uuid }}.
+        Default neighborhood: {{ default_neighborhood }}.
+        Your interests: {{ interests }}.
+        Your involvements: {{ involvements }}.
+        Availability: {{ availability }}.
+        Interlocutor: {{ interlocutor }}.
+      TEXT
+
+      expected_message = <<~TEXT
+        Hello John,
+        Your email is #{other_user.email}, and your phone number is #{other_user.phone}.
+        You live in  and your ID is #{other_user.uuid}.
+        Default neighborhood: .
+        Your interests: .
+        Your involvements: .
+        Availability: .
+        Interlocutor: Alice.
+      TEXT
+
+      result = ChatMessage.interpolate(message: message, user: other_user, author: author)
+      expect(result.strip).to eq(expected_message.strip)
+    end
+
+    it 'handles missing placeholders gracefully when author is nil' do
+      message = "Hello {{ first_name }}, your interlocutor is {{ interlocutor }}."
+      expected_message = "Hello John, your interlocutor is ."
+
+      result = ChatMessage.interpolate(message: message, user: user, author: nil)
+      expect(result).to eq(expected_message)
+    end
+
+    it 'handles missing placeholders in the message' do
+      message = "This message has no placeholders."
+      expected_message = "This message has no placeholders."
+
+      result = ChatMessage.interpolate(message: message, user: user)
+      expect(result).to eq(expected_message)
+    end
+  end
+
   describe "custom type" do
     let(:entourage) { create :entourage }
     let!(:group) { create :entourage, uuid_v2: "uuid-123" }
