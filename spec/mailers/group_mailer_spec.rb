72d816c00b1ac4d76de0b09b4a493bcc67275d1a
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-01-10 14:54:46 +0100

    EN-7393 fix mailer args

--- spec/mailers/group_mailer_spec.rb
+++ spec/mailers/group_mailer_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe GroupMailer, type: :mailer do
-  let(:user) { create :public_user }
-  let(:outing) { create :outing, user: user }
+  let(:participant) { create :public_user, first_name: 'Alice' }
+  let(:organizer) { create :public_user, first_name: 'Bob' }
+  let(:outing) { create :outing, user: organizer }
 
   describe ".event_joined_confirmation" do
-    let(:user) { create :public_user }
-    let(:mail) { GroupMailer.event_joined_confirmation(outing) }
+    let(:mail) { GroupMailer.event_joined_confirmation(outing.id, participant.id) }
     let(:json_variables) { JSON.parse(mail['X-MJ-Vars'].value) }
 
-    it { expect(json_variables['first_name']).to eq(user.first_name) }
+    it { expect(json_variables['first_name']).to eq(participant.first_name) }
     it { expect(json_variables).to have_key("outing") }
     it { expect(json_variables['outing']['name']).to eq(outing.title) }
     it { expect(json_variables['outing']['calendar_url']).to match("agenda") }
