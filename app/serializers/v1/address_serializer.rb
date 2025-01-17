f8ed9cf592c76b824c62b76387e6479632baf1a5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-01-17 15:44:47 +0100

    api allow to display other users city

--- app/serializers/v1/address_serializer.rb
+++ app/serializers/v1/address_serializer.rb
@@ -5,12 +5,34 @@ module V1
                :display_address,
                :position
 
+    def latitude
+      return unless me?
+
+      object.latitude
+    end
+
+    def longitude
+      return unless me?
+
+      object.longitude
+    end
+
     def display_address
       address = UserServices::AddressService.update_city_if_nil(object)
 
+      return "#{address.city}, #{address.postal_code}" unless me?
       return address.display_address unless address.city && address.postal_code
 
-      "%s, %s" % [address.city, address.postal_code]
+      "#{address.city}, #{address.postal_code}"
+    end
+
+    def city
+      address = UserServices::AddressService.update_city_if_nil(object)
+      address.city
+    end
+
+    def me?
+      scope[:user] && (object.user_id == scope[:user].id)
     end
   end
 end
