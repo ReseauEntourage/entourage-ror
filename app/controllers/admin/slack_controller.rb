f1403a8c191e62fc45b15e7a9b878c08cde21a05
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-02-19 15:29:02 +0100

    templating

--- app/controllers/admin/slack_controller.rb
+++ app/controllers/admin/slack_controller.rb
@@ -90,12 +90,12 @@ module Admin
         chat_message.is_offensive!
 
         response[:attachments].first[:color] = :danger
-        response[:attachments].last[:text] = "*:#{:white_check_mark}: <@#{@payload['user']['name']}> a marqué le contenu comme offensant*"
+        response[:attachments].last[:text] = "*:#{:no_entry_sign}: <@#{@payload['user']['name']}> a marqué le contenu comme offensant*"
       elsif action == 'is_not_offensive'
         chat_message.is_not_offensive!
 
         response[:attachments].first[:color] = :good
-        response[:attachments].last[:text] = "*:#{:no_entry_sign}: <@#{@payload['user']['name']}> a marqué le contenu comme non offensant*"
+        response[:attachments].last[:text] = "*:#{:white_check_mark}: <@#{@payload['user']['name']}> a marqué le contenu comme non offensant*"
       end
 
       render json: response
