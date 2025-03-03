49a229adc50208454b69d6ba782fec2e6fda9911
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-03-03 10:33:39 +0100

    EN-8012 add methods to describe salesforce tables and fields
    These methods are currently not used in the project but we use them in dev environnement to have an easy access to salesforce schema. For instance, it is mainly useful as a debug tool

--- app/services/salesforce_services/connect.rb
+++ app/services/salesforce_services/connect.rb
@@ -16,5 +16,25 @@ module SalesforceServices
         log_level: :debug
       )
     end
+
+    # describe object fields
+    def object_fields object_name
+      return Hash.new unless metadata = client.describe(object_name)
+
+      metadata[:fields]
+    end
+
+    # describe object field values
+    def object_field_values object_name, field
+      object_fields(object_name).find { |f| f[:name] == field }[:picklistValues]
+    end
+
+    # check whether a field includes a value
+    # example: object_field_has_value?("Campaign", "Type_evenement__c", SalesforceServices::Outing::TYPE_EVENEMENT)
+    def object_field_has_value? object_name, field, value
+      object_field_values(object_name, field).any? do |config|
+        config["value"] == value
+      end
+    end
   end
 end
