23356912ccab534a3d31921a2cd7b87c495806af
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-03-06 11:04:35 +0100

    EN-8090 fix: address is created after the user is created. We should then track address creation to notify on association creation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
 
     stub_request(:any, /.*api.mailjet.com.*/).to_return(status: 200, body: { id: 1 }.to_json, headers: {})
     stub_request(:any, /.*api.openai.*/).to_return(status: 200, body: "{}", headers: {})
+    stub_request(:any, "https://url.to.slack.com").to_return(status: 200)
 
     # deactivate slack_trace notifications
     SlackServices::StackTrace.any_instance.stub(:notify).and_return(nil)
