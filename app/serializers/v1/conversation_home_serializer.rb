2c23f4dcba4618b60dbbb1b90e5558efacd3db50
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-03-13 14:33:19 +0100

    EN-7996 refactor conversation serializers

--- app/serializers/v1/conversation_home_serializer.rb
+++ app/serializers/v1/conversation_home_serializer.rb
@@ -1,51 +1,10 @@
 module V1
-  class ConversationHomeSerializer < ActiveModel::Serializer
-    include AmsLazyRelationships::Core
-    include V1::Entourages::Blockers
-
-    attributes :id,
-      :uuid,
-      :uuid_v2,
-      :status,
-      :type,
-      :name,
-      :image_url,
-      :creator,
-      :member,
-      :members_count,
-      :chat_messages,
-      :has_personal_post
-
-    attribute :user, if: :private_conversation?
-    attribute :section, unless: :private_conversation?
+  class ConversationHomeSerializer < ConversationSerializer
+    attribute :member
+    attribute :chat_messages
     attribute :author, unless: :private_conversation?
-    attribute :blockers, if: :private_conversation?
-
-    has_many :members, serializer: ::V1::Users::BasicSerializer
 
     lazy_relationship :chat_messages
-
-    def type
-      return :private if private_conversation?
-      return :outing if object.outing?
-      return :contribution if object.contribution?
-
-      :solicitation
-    end
-
-    def name
-      return object.title unless private_conversation?
-      return unless other_participant
-
-      UserPresenter.new(user: other_participant).display_name
-    end
-
-    def image_url
-      return object.image_url unless private_conversation?
-      return unless other_participant
-
-      UserServices::Avatar.new(user: other_participant).thumbnail_url
-    end
 
     def creator
       return false unless scope && scope[:user]
@@ -59,43 +18,10 @@ module V1
       object.member_ids.include?(scope[:user].id)
     end
 
-    def members_count
-      object.member_ids.count
-    end
-
     def chat_messages
       object.chat_messages.ordered.limit(25).map do |chat_message|
         V1::ChatMessageHomeSerializer.new(chat_message, scope: { current_join_request: current_join_request }).as_json
       end
-    end
-
-    def has_personal_post
-      return unless scope[:user]
-
-      (lazy_chat_messages.pluck(:user_id) & [scope[:user].id]).any?
-    end
-
-    # @duplicated with V1::ConversationSerializer
-    def user
-      return unless user = other_participant
-
-      partner = user.partner
-
-      {
-        id: user.id,
-        display_name: UserPresenter.new(user: user).display_name,
-        avatar_url: UserServices::Avatar.new(user: user).thumbnail_url,
-        partner: partner.nil? ? nil : V1::PartnerSerializer.new(partner, scope: { minimal: true }, root: false).as_json,
-        partner_role_title: user.partner_role_title.presence,
-        roles: UserPresenter.new(user: user).public_targeting_profiles,
-        moderator: user.admin?,
-      }
-    end
-
-    def section
-      return unless object.action?
-
-      object.becomes(object.action_class).section
     end
 
     def author
@@ -109,8 +35,12 @@ module V1
       }
     end
 
-    def private_conversation?
-      object.group_type.in?(['conversation'])
+    def number_of_unread_messages
+      # no need
+    end
+
+    def last_message
+      # no need
     end
 
     private
@@ -119,10 +49,6 @@ module V1
       return unless scope[:user]
 
       @current_join_request ||= JoinRequest.where(joinable: object, user: scope[:user], status: :accepted).first
-    end
-
-    def other_participant
-      @other_participant ||= object.interlocutor_of(scope[:user])
     end
   end
 end
