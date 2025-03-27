8fd2bcd7385b441e81bf1f58fe0f518bc86bc2b5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-03-27 17:49:31 +0100

    EN-8012 add views to display outing, user from salesforce

--- app/controllers/admin/salesforce/outings_controller.rb
+++ app/controllers/admin/salesforce/outings_controller.rb
@@ -7,6 +7,10 @@ module Admin
         @records = @interface.records(per: 10, page: params[:page].to_i.positive? ? params[:page].to_i : 1)
         @outings = Kaminari.paginate_array(@records[:data], total_count: @records[:total]).page(params[:page]).per(10)
       end
+
+      def show
+        @outing = Outing.find(params[:id])
+      end
     end
   end
 end
