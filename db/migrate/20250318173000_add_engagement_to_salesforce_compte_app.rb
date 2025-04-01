60cbd58c9f9d1b1df181e446b55751a616eceb77
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-01 11:07:32 +0200

    EN-8011 add field_exists? to ensure more secured migrations

--- db/migrate/20250318173000_add_engagement_to_salesforce_compte_app.rb
+++ db/migrate/20250318173000_add_engagement_to_salesforce_compte_app.rb
@@ -1,23 +1,23 @@
 class AddEngagementToSalesforceCompteApp < ActiveRecord::Migration[6.1]
   def up
-    # raise unless SalesforceServices::TableInterface.create_field(
-    #   "Compte_App__c",
-    #   "LastEngagementDate__c",
-    #   "Last Engagement Date",
-    #   "Date"
-    # )
+    SalesforceServices::TableInterface.create_field(
+      "Compte_App__c",
+      "LastEngagementDate__c",
+      "Last Engagement Date",
+      "Date"
+    )
 
-    # raise unless SalesforceServices::TableInterface.create_field(
-    #   "Compte_App__c",
-    #   "IsEngaged__c",
-    #   "Is Engaged",
-    #   "Checkbox",
-    #   default_value: "false"
-    # )
+    SalesforceServices::TableInterface.create_field(
+      "Compte_App__c",
+      "IsEngaged__c",
+      "Is Engaged",
+      "Checkbox",
+      default_value: "false"
+    )
   end
 
   def down
-    # raise unless SalesforceServices::TableInterface.delete_field("Compte_App__c", "LastEngagementDate__c")
-    # raise unless SalesforceServices::TableInterface.delete_field("Compte_App__c", "IsEngaged__c")
+    SalesforceServices::TableInterface.delete_field("Compte_App__c", "LastEngagementDate__c")
+    SalesforceServices::TableInterface.delete_field("Compte_App__c", "IsEngaged__c")
   end
 end
