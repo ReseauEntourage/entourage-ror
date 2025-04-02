fcc2ee5911ab7625c6a55ead7edcf08ea4667ffd
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-02 18:16:15 +0200

    EN-7996 add api/v1/conversations/users#index

--- config/routes.rb
+++ config/routes.rb
@@ -579,7 +579,7 @@ Rails.application.routes.draw do
       resources :conversations do
         resources :chat_messages, :controller => 'conversations/chat_messages', only: [:index, :create, :update, :destroy]
 
-        resources :users, :controller => 'conversations/users', only: [:create] do
+        resources :users, :controller => 'conversations/users', only: [:index, :create] do
           member do
             post :invite
           end
