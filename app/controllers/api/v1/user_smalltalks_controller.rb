a74533cbd48c81782d8546f5b6be981075b5ffd5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-08 12:34:27 +0200

    EN-8136 api/v1/user_smalltalks#show

--- app/controllers/api/v1/user_smalltalks_controller.rb
+++ app/controllers/api/v1/user_smalltalks_controller.rb
@@ -9,7 +9,19 @@ module Api
           .per(per), root: :user_smalltalks, each_serializer: ::V1::UserSmalltalkSerializer, scope: { user: current_user }
       end
 
+      def show
+        return render json: { message: 'unauthorized' }, status: :unauthorized if @user_smalltalk.user != current_user
+
+        render json: @user_smalltalk, serializer: ::V1::UserSmalltalkSerializer, scope: { user: current_user }
+      end
+
       private
+
+      def set_user_smalltalk
+        @user_smalltalk = UserSmalltalk.find(params[:id])
+
+        render json: { message: 'Could not find user_smalltalk' }, status: 400 unless @user_smalltalk.present?
+      end
 
       def page
         params[:page] || 1
