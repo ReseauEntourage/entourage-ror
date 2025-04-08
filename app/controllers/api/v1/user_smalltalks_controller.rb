1a807e9122ea2a51b6b6b104a5934aa7a0c70bd7
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-08 12:48:46 +0200

    EN-8136 api/v1/user_smalltalks#create and update

--- app/controllers/api/v1/user_smalltalks_controller.rb
+++ app/controllers/api/v1/user_smalltalks_controller.rb
@@ -15,12 +15,41 @@ module Api
         render json: @user_smalltalk, serializer: ::V1::UserSmalltalkSerializer, scope: { user: current_user }
       end
 
+      def create
+        @user_smalltalk = UserSmalltalk.new(user_smalltalk_params)
+        @user_smalltalk.user = current_user
+
+        if @user_smalltalk.save
+          render json: @user_smalltalk, status: 201, serializer: ::V1::UserSmalltalkSerializer, scope: { user: current_user }
+        else
+          render json: { message: "Could not create UserSmalltalk", reasons: @user_smalltalk.errors.full_messages }, status: 400
+        end
+      end
+
+      def update
+        return render json: { message: 'unauthorized' }, status: :unauthorized if @user_smalltalk.user != current_user
+
+        @user_smalltalk.assign_attributes(user_smalltalk_params)
+
+        if @user_smalltalk.save
+          render json: @user_smalltalk, status: 200, serializer: ::V1::UserSmalltalkSerializer, scope: { user: current_user }
+        else
+          render json: {
+            message: 'Could not update user_smalltalk', reasons: @user_smalltalk.errors.full_messages
+          }, status: 400
+        end
+      end
+
       private
 
       def set_user_smalltalk
         @user_smalltalk = UserSmalltalk.find(params[:id])
 
         render json: { message: 'Could not find user_smalltalk' }, status: 400 unless @user_smalltalk.present?
+      end
+
+      def user_smalltalk_params
+        params.require(:user_smalltalk).permit(:match_format, :match_locality, :match_gender, :match_interest)
       end
 
       def page
---
+++ app/serializers/v1/user_smalltalk_serializer.rb
@@ -0,0 +1,17 @@
+module V1
+  class UserSmalltalkSerializer < ActiveModel::Serializer
+    attributes :smalltalk_id,
+      :user_gender,
+      :user_profile,
+      :user_latitude,
+      :user_longitude,
+      :match_format,
+      :match_locality,
+      :match_gender,
+      :match_interest,
+      :last_match_computation_at,
+      :matched_at,
+      :deleted_at,
+      :created_at
+  end
+end
