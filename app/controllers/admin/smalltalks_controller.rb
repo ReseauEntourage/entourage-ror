9f8922235200cbb5d48610d6775718c33c342924
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-18 14:06:47 +0200

    EN-8136 backoffice basic templating for user_smalltalks and smalltalks

--- app/controllers/admin/smalltalks_controller.rb
+++ app/controllers/admin/smalltalks_controller.rb
@@ -1,9 +1,34 @@
 module Admin
   class SmalltalksController < Admin::BaseController
-    before_action :authenticate_admin!
+    before_action :set_smalltalk, only: [:show, :show_members, :show_messages]
 
     def index
-      @smalltalks = Smalltalk.includes(:members).order(updated_at: :desc)
+      @smalltalks = Smalltalk.includes(:members).order(updated_at: :desc).page(page).per(per)
+    end
+
+    def show
+    end
+
+    def show_members
+      @members = @smalltalk.members.page(page).per(per)
+    end
+
+    def show_messages
+      @messages = @smalltalk.chat_messages.order(created_at: :desc).page(page).per(per).includes(:user, :survey, :translation)
+    end
+
+    private
+
+    def set_smalltalk
+      @smalltalk = Smalltalk.find(params[:id])
+    end
+
+    def page
+      params[:page] || 1
+    end
+
+    def per
+      params[:per] || 25
     end
   end
 end
