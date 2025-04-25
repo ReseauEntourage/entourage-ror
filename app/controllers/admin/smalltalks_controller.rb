d35d6de6223825e986e8d045efb1ef1f5c9fb0db
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-25 15:23:33 +0200

    EN-8136 backoffice allow admin to send a message in a smalltalk
    Please notice that sending a message from backoffice does not join Smalltalk

--- app/controllers/admin/smalltalks_controller.rb
+++ app/controllers/admin/smalltalks_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class SmalltalksController < Admin::BaseController
-    before_action :set_smalltalk, only: [:show, :show_members, :show_messages]
+    before_action :set_smalltalk, only: [:show, :show_members, :show_messages, :message]
 
     def index
       @smalltalks = Smalltalk.includes(:members).order(updated_at: :desc).page(page).per(per)
@@ -17,10 +17,32 @@ module Admin
       @messages = @smalltalk.chat_messages.order(created_at: :desc).page(page).per(per).includes(:user, :survey, :translation)
     end
 
+    # POST
+    def message
+      ChatServices::ChatMessageBuilder.new(
+        params: chat_messages_params,
+        user: current_user,
+        joinable: @smalltalk,
+        join_request: nil
+      ).create do |on|
+        on.success do |message|
+          redirect_to show_messages_admin_smalltalk_path(@smalltalk)
+        end
+
+        on.failure do |message|
+          redirect_to show_messages_admin_smalltalk_path(@smalltalk), alert: "Erreur lors de l'envoi du message : #{message.errors.full_messages.to_sentence}"
+        end
+      end
+    end
+
     private
 
     def set_smalltalk
       @smalltalk = Smalltalk.find(params[:id])
+    end
+
+    def chat_messages_params
+      params.require(:chat_message).permit(:content, :parent_id)
     end
 
     def page
