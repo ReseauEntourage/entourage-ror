bc4b5a9eecfedbfcfcc67d7976f77ba78e7928af
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-28 15:52:38 +0200

    EN-8136 api smalltalks: add comments, presigned_upload, reactions, survey_responses

---
+++ app/controllers/api/v1/smalltalks/chat_messages/reactions_controller.rb
@@ -0,0 +1,95 @@
+module Api
+  module V1
+    module Smalltalks
+      module ChatMessages
+        class UnauthorizedReaction < StandardError; end
+
+        class ReactionsController < Api::V1::BaseController
+          before_action :set_smalltalk
+          before_action :set_chat_message
+          before_action :ensure_is_member, only: [:create, :destroy]
+
+          rescue_from Api::V1::Smalltalks::ChatMessages::UnauthorizedReaction do |exception|
+            render json: { message: 'unauthorized : you are not accepted in this smalltalk' }, status: :unauthorized
+          end
+
+          def index
+            render json: { reactions: @chat_message.reactions.summary }
+          end
+
+          def details
+            users = @chat_message.user_reactions.where(reaction_id: params[:id])
+              .includes(:user)
+              .order(:created_at)
+              .page(page)
+              .per(per)
+              .map(&:user)
+
+            render json: users, each_serializer: ::V1::Users::BasicSerializer
+          end
+
+          def users
+            user_reactions = @chat_message.user_reactions
+              .includes(:user)
+              .order(:created_at)
+              .page(page)
+              .per(per)
+
+            render json: user_reactions, each_serializer: ::V1::UserReactionSerializer
+          end
+
+          def create
+            reaction = @chat_message.reactions.build(user: current_user, reaction_id: params[:reaction_id])
+
+            if reaction.save
+              render json: reaction, status: 201, serializer: ::V1::ReactionSerializer
+            else
+              render json: {
+                message: "Could not create reaction", reasons: reaction.errors.full_messages
+              }, status: 400
+            end
+          end
+
+          def destroy
+            if reaction_id = @chat_message.reactions.destroy(user: current_user)
+              render json: { reaction_id: reaction_id }, status: 200
+            else
+              render json: { message: "Could not delete reaction" }, status: 400
+            end
+          end
+
+          private
+
+          def set_smalltalk
+            @smalltalk = Smalltalk.find_by_id_through_context(params[:smalltalk_id], params)
+
+            render json: { message: 'Could not find smalltalk' }, status: 400 unless @smalltalk.present?
+          end
+
+          def set_chat_message
+            # we want to force chat_message to belong to Smalltalk
+            @chat_message = ChatMessage.where(messageable: @smalltalk).find_by_id_through_context(params[:chat_message_id], params)
+
+            render json: { message: 'Could not find chat_message' }, status: 400 unless @chat_message.present?
+          end
+
+          def join_request
+            @join_request ||= JoinRequest.where(joinable: @smalltalk, user: current_user, status: :accepted).first
+          end
+
+          def ensure_is_member
+            raise Api::V1::Smalltalks::ChatMessages::UnauthorizedReaction unless join_request
+          end
+
+          def page
+            params[:page] || 1
+          end
+
+          def per
+            params[:per] || 25
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v1/smalltalks/chat_messages/survey_responses_controller_spec.rb
@@ -0,0 +1,134 @@
+require 'rails_helper'
+
+describe Api::V1::Smalltalks::ChatMessages::SurveyResponsesController do
+  let(:user) { create(:pro_user) }
+  let(:smalltalk) { create :smalltalk }
+  let(:survey) { create(:survey) }
+  let(:chat_message) { create(:chat_message, messageable: smalltalk, survey: survey) }
+
+  let(:result) { JSON.parse(response.body) }
+
+  describe 'index' do
+    let!(:survey_response) { create(:survey_response, chat_message: chat_message, responses: [false, true]) }
+
+    context "not signed in" do
+      before { get :index, params: { smalltalk_id: smalltalk.to_param, chat_message_id: chat_message.id } }
+
+      it { expect(response.status).to eq(401) }
+    end
+
+    context "signed in" do
+      before { get :index, params: { smalltalk_id: smalltalk.to_param, token: user.token, chat_message_id: chat_message.id } }
+
+      context "no survey" do
+        let(:chat_message) { create(:chat_message, messageable: smalltalk) }
+
+        it { expect(response.status).to eq(400) }
+      end
+
+      context "survey" do
+        it { expect(response.status).to eq(200) }
+        it { expect(result).to have_key('survey_responses')}
+        it { expect(result).to eq({
+          "survey_responses" => [
+            [],
+            [
+              {
+                "id" => survey_response.user_id,
+                "lang" => survey_response.user.lang,
+                "display_name" => "John D.",
+                "avatar_url" => nil,
+                "community_roles" => [],
+              }
+            ]
+          ]
+        }) }
+      end
+    end
+  end
+
+  describe 'create' do
+    let(:request) { post :create, params: { smalltalk_id: smalltalk.to_param, chat_message_id: chat_message.id, token: user.token, responses: [true, false] } }
+
+    context "not member" do
+      before { request }
+
+      it { expect(response.status).to eq(401) }
+    end
+
+    context "member" do
+      let!(:join_request) { create(:join_request, joinable: smalltalk, user: user, status: :accepted) }
+
+      context "unexisting response for user" do
+        context do
+          before { request }
+
+          it { expect(response.status).to eq(201) }
+          it { expect(survey.reload.summary).to eq([1, 0]) }
+        end
+
+        context do
+          it { expect { request }.to change { SurveyResponse.count }.by(1) }
+        end
+      end
+
+      context "existing response for user" do
+        let!(:survey_response) { create(:survey_response, chat_message: chat_message, user: user, responses: [false, true]) }
+
+        context do
+          before { request }
+
+          it { expect(response.status).to eq(201) }
+          it { expect(survey_response.reload.responses).to eq(["true", "false"]) }
+          it { expect(survey.reload.summary).to eq([1, 0]) }
+        end
+
+        context do
+          it { expect { request }.not_to change { SurveyResponse.count } }
+        end
+      end
+    end
+  end
+
+  describe 'destroy' do
+    let!(:survey_response) { create(:survey_response, user: user, chat_message: chat_message) }
+
+    let(:request) { delete :destroy, params: { smalltalk_id: smalltalk.to_param, chat_message_id: chat_message.id, token: user.token } }
+
+    context "not member" do
+      before { request }
+
+      it { expect(response.status).to eq(401) }
+    end
+
+    context "member" do
+      let!(:join_request) { create(:join_request, joinable: smalltalk, user: user, status: :accepted) }
+
+      context "unexisting survey_response for user" do
+        let!(:survey_response) { create(:survey_response, chat_message: chat_message) }
+
+        context do
+          it { expect { request }.not_to change { SurveyResponse.count } }
+        end
+
+        context do
+          before { request }
+
+          it { expect(response.status).to eq(400) }
+        end
+      end
+
+      context "existing survey_response for user" do
+        context do
+          before { request }
+
+          it { expect(response.status).to eq(200) }
+        end
+
+        context do
+          it { expect { request }.to change { SurveyResponse.count }.by(-1) }
+        end
+      end
+    end
+  end
+end
