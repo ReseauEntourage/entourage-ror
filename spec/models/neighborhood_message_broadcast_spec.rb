15b7324d9f39cf6b9760651dd42299f58c3335cc
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-04-30 17:41:30 +0200

    EN-8152 fix rspec

--- spec/models/neighborhood_message_broadcast_spec.rb
+++ spec/models/neighborhood_message_broadcast_spec.rb
@@ -4,92 +4,33 @@ RSpec.describe NeighborhoodMessageBroadcast, type: :model do
   it { should validate_presence_of(:content) }
   it { should validate_presence_of(:title) }
 
-  let!(:neighborhood_75009) { create(:neighborhood, postal_code: '75009') }
-  let!(:neighborhood_75018) { create(:neighborhood, postal_code: '75018') }
-  let!(:neighborhood_44240) { create(:neighborhood, postal_code: '44240') }
-  let!(:neighborhood_29160) { create(:neighborhood, postal_code: '29160') }
+  let!(:neighborhood_75009) { create(:neighborhood, postal_code: "75009", zone: "ville") }
+  let!(:neighborhood_75018) { create(:neighborhood, postal_code: "75018", zone: "departement") }
+  let!(:neighborhood_44240) { create(:neighborhood, postal_code: "44240", zone: "departement") }
+  let!(:neighborhood_29160) { create(:neighborhood, postal_code: "29160", zone: "no_zone") }
 
-  describe 'departements' do
-    let(:conversation_ids) { [] }
-    let(:neighborhood_message_broadcast) { create(:neighborhood_message_broadcast, conversation_ids: conversation_ids) }
-
-    let(:subject) { neighborhood_message_broadcast.departements }
-
-    context 'finds single departement from multiple postal_codes of that departement' do
-      let(:conversation_ids) { [neighborhood_75009.id, neighborhood_75018.id] }
-
-      it { expect(subject).to match_array(['75']) }
-    end
-
-    context 'finds multiple departements from postal_codes of multiple departements' do
-      let(:conversation_ids) { [neighborhood_75009.id, neighborhood_44240.id] }
-
-      it { expect(subject).to match_array(['75', '44']) }
-    end
-  end
-
-  describe 'neighborhood_ids_in_departements' do
+  describe "neighborhood_ids_in_departements_and_area_type" do
     let(:departements) { [] }
+    let(:area_type) { "zone_all" }
 
-    let(:subject) { NeighborhoodMessageBroadcast::neighborhood_ids_in_departements(departements) }
+    let(:subject) { NeighborhoodMessageBroadcast::neighborhood_ids_in_departements_and_area_type(departements, area_type) }
 
-    context 'finds no neighborhood when unexisting departement' do
-      let(:departements) { ['35'] }
+    context "finds no neighborhood when unexisting departement" do
+      let(:departements) { ["35"] }
 
       it { expect(subject).to match_array([]) }
     end
 
-    context 'finds all departements of single departement' do
-      let(:departements) { ['75'] }
+    context "finds all departements of single departement" do
+      let(:departements) { ["75"] }
 
       it { expect(subject).to match_array([neighborhood_75009.id, neighborhood_75018.id]) }
     end
 
-    context 'finds all departements of multiple departements' do
-      let(:departements) { ['75', '44'] }
+    context "finds all departements of multiple departements" do
+      let(:departements) { ["75", "44"] }
 
       it { expect(subject).to match_array([neighborhood_75009.id, neighborhood_75018.id, neighborhood_44240.id]) }
-    end
-  end
-
-  describe 'has_full_departements_selection?' do
-    let(:conversation_ids) { [] }
-    let(:neighborhood_message_broadcast) { create(:neighborhood_message_broadcast, conversation_ids: conversation_ids) }
-
-    let(:subject) { neighborhood_message_broadcast.has_full_departements_selection? }
-
-    context 'empty broadcast' do
-      it { expect(subject).to eq(true) }
-    end
-
-    context 'one neighborhood in departement and one set on broadcast' do
-      let(:conversation_ids) { [neighborhood_44240.id] }
-
-      it { expect(subject).to eq(true) }
-    end
-
-    context 'multiple neighborhoods in departement but only one set on broadcast' do
-      let(:conversation_ids) { [neighborhood_75009.id] }
-
-      it { expect(subject).to eq(false) }
-    end
-
-    context 'multiple neighborhoods in departement and all set on broadcast' do
-      let(:conversation_ids) { [neighborhood_75009.id, neighborhood_75018.id] }
-
-      it { expect(subject).to eq(true) }
-    end
-
-    context 'multiple neighborhoods in mulitple departements and all set on broadcast' do
-      let(:conversation_ids) { [neighborhood_75009.id, neighborhood_75018.id, neighborhood_44240.id] }
-
-      it { expect(subject).to eq(true) }
-    end
-
-    context 'multiple neighborhoods in mulitple departements but one is missing on broadcast' do
-      let(:conversation_ids) { [neighborhood_75009.id, neighborhood_44240.id] }
-
-      it { expect(subject).to eq(false) }
     end
   end
 end
