c8982129bb38d5e36d6cdb5a42e1e2466cf2142e
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-05-19 12:17:19 +0200

    EN-8012 fix: remove UserId__c from mapping that caused
    INVALID_FIELD: The UserId__c field should not be specified in the sobject data

--- app/services/salesforce_services/user_table_interface.rb
+++ app/services/salesforce_services/user_table_interface.rb
@@ -3,7 +3,6 @@ module SalesforceServices
     TABLE_NAME = "Compte_App__c"
 
     INSTANCE_MAPPING = {
-      id: "UserId__c",
       first_name: "Prenom__c",
       last_name: "Nom__c",
       email: "Email__c",
