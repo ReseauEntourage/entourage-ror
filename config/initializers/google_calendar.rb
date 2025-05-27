11bbc24aa1a6d27c5b4ee7222d9c9d255af676a0
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-05-27 17:59:43 +0200

    fix rspec

--- config/initializers/google_calendar.rb
+++ config/initializers/google_calendar.rb
@@ -3,10 +3,12 @@ require 'googleauth'
 
 GOOGLE_CALENDAR_SERVICE = Google::Apis::CalendarV3::CalendarService.new
 
-credentials = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: StringIO.new(ENV['BONNES_ONDES_SERVICE_ACCOUNT']),
-  scope: ['https://www.googleapis.com/auth/calendar']
-)
+unless EnvironmentHelper.test?
+  credentials = Google::Auth::ServiceAccountCredentials.make_creds(
+    json_key_io: StringIO.new(ENV['BONNES_ONDES_SERVICE_ACCOUNT']),
+    scope: ['https://www.googleapis.com/auth/calendar']
+  )
 
-credentials.sub = ENV['BONNES_ONDES_EMAIL_ACCOUNT']
-GOOGLE_CALENDAR_SERVICE.authorization = credentials
+  credentials.sub = ENV['BONNES_ONDES_EMAIL_ACCOUNT']
+  GOOGLE_CALENDAR_SERVICE.authorization = credentials
+end
