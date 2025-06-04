ba6c94b7b86cbdbf6ee0b3e4e3ce590251b327f4
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-06-04 11:36:28 +0200

    EN-8154 meeting are created using meet_space

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,7 +2,75 @@ class Meeting < ApplicationRecord
   after_create :create_google_meet_event
 
   def create_google_meet_event
-    event = Google::Apis::CalendarV3::Event.new(
+    meeting_space = create_individual_meet_space
+
+    if meeting_space
+      created_event = create_calendar_event_with_space(meeting_space)
+      update_meet_link(meeting_space['meetingUri'], created_event)
+    else
+      created_event = create_standard_calendar_event
+      update_meet_link(nil, created_event)
+    end
+  end
+
+  private
+
+  def create_individual_meet_space
+    auth_token = GOOGLE_CALENDAR_SERVICE.authorization.access_token
+
+    space_config = {
+      config: {
+        access_type: 'OPEN',
+        entry_point_access: 'ALL'
+      }
+    }
+
+    response = HTTParty.post(
+      'https://meet.googleapis.com/v2/spaces',
+      headers: {
+        'Authorization' => "Bearer #{auth_token}",
+        'Content-Type' => 'application/json'
+      },
+      body: space_config.to_json
+    )
+
+    response.success? ? JSON.parse(response.body) : nil
+  rescue
+    nil
+  end
+
+  def create_calendar_event_with_space(meeting_space)
+    meeting_uri = meeting_space['meetingUri']
+    space_name = meeting_space['name']
+
+    event = build_calendar_event({
+      conference_id: space_name,
+      signature: SecureRandom.uuid,
+      entry_points: [
+        {
+          entry_point_type: 'video',
+          uri: meeting_uri,
+          label: 'Rejoindre avec Google Meet'
+        }
+      ]
+    })
+
+    GOOGLE_CALENDAR_SERVICE.insert_event('primary', event, conference_data_version: 1)
+  end
+
+  def create_standard_calendar_event
+    event = build_calendar_event({
+      create_request: {
+        request_id: SecureRandom.uuid,
+        conference_solution_key: { type: 'hangoutsMeet' }
+      }
+    })
+
+    GOOGLE_CALENDAR_SERVICE.insert_event('primary', event, conference_data_version: 1)
+  end
+
+  def build_calendar_event(conference_data)
+    Google::Apis::CalendarV3::Event.new(
       summary: title,
       start: {
         date_time: start_time.iso8601,
@@ -13,41 +81,12 @@ class Meeting < ApplicationRecord
         time_zone: 'Europe/Paris'
       },
       attendees: participant_emails.map { |email| { email: email } },
-      conference_data: {
-        visibility: 'public',
-        create_request: {
-          request_id: SecureRandom.uuid,
-          conference_solution_key: { type: 'hangoutsMeet' }
-        },
-        entry_points: [
-          {
-            entry_point_type: 'video',
-            uri: '', # filled in by Google
-            access_code: '',
-            meeting_code: '',
-            passcode: '',
-            password: ''
-          }
-        ],
-        conference_parameters: {
-          add_on_parameters: {
-            parameters: {
-              'external_only' => 'false',
-              'auto_admit_policy' => 'EVERYONE'
-            }
-          }
-        }
-      }
+      conference_data: conference_data
     )
+  end
 
-    created_event = GOOGLE_CALENDAR_SERVICE.insert_event(
-      'primary',
-      event,
-      conference_data_version: 1
-    )
-
-    return unless created_event.respond_to?(:hangout_link)
-
-    update!(meet_link: created_event.hangout_link)
+  def update_meet_link(meeting_uri, created_event)
+    link = meeting_uri || created_event&.hangout_link
+    update!(meet_link: link) if link
   end
 end
