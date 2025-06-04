26246b78d49c8df43cc9ac01635789ab715399b5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-06-04 13:28:04 +0200

    EN-8154 add authorisations to google api

--- config/initializers/google_calendar.rb
+++ config/initializers/google_calendar.rb
@@ -6,9 +6,15 @@ GOOGLE_CALENDAR_SERVICE = Google::Apis::CalendarV3::CalendarService.new
 unless EnvironmentHelper.test?
   credentials = Google::Auth::ServiceAccountCredentials.make_creds(
     json_key_io: StringIO.new(ENV['BONNES_ONDES_SERVICE_ACCOUNT']),
-    scope: ['https://www.googleapis.com/auth/calendar']
+    scope: [
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.events',
+      'https://www.googleapis.com/auth/meetings.space.created'
+    ]
   )
 
   credentials.sub = ENV['BONNES_ONDES_EMAIL_ACCOUNT']
+  credentials.fetch_access_token!
+
   GOOGLE_CALENDAR_SERVICE.authorization = credentials
 end
