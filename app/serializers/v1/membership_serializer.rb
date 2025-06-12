cd2ab08c5c6760349dea9b07aca07217911e22db
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-06-12 17:15:42 +0200

    EN-8324 add route to index memberships

---
+++ app/serializers/v1/membership_serializer.rb
@@ -0,0 +1,31 @@
+module V1
+  class MembershipSerializer < ActiveModel::Serializer
+    attributes :status,
+      :joinable_status,
+      :name,
+      :joinable_type,
+      :joinable_id,
+      :number_of_people,
+      :number_of_root_chat_messages,
+      :number_of_unread_messages,
+      :last_chat_message
+
+    def name
+      object.joinable.try(:name) || object.joinable.try(:title)
+    end
+
+    def number_of_people
+      object.joinable.try(:number_of_people)
+    end
+
+    def number_of_root_chat_messages
+      object.joinable.try(:number_of_root_chat_messages)
+    end
+
+    def number_of_unread_messages
+    end
+
+    def last_chat_message
+    end
+  end
+end
