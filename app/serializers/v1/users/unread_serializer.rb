5edd89f03ccdee2b5f002dd0b6b0891fd67d2c2d
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-06-16 14:21:29 +0200

    EN-8327 add to unread serializer: - unread_conversations_count - unread_neighborhoods_count

--- app/serializers/v1/users/unread_serializer.rb
+++ app/serializers/v1/users/unread_serializer.rb
@@ -1,10 +1,28 @@
 module V1
   module Users
     class UnreadSerializer < ActiveModel::Serializer
-      attributes :id, :unread_count
+      attributes :id, :unread_count, :unread_conversations_count, :unread_neighborhoods_count
 
+      # sums up unread conversations for private conversations
       def unread_count
         UserServices::UnreadMessages.new(user: object).number_of_unread_messages
+      end
+
+      # sums up unread conversations for:
+      # - private conversations
+      # - outing conversations
+      # - smalltalks conversations
+      def unread_conversations_count
+        UserServices::UnreadMessages.new(
+          user: object
+        ).number_of_unread_for_joinable_types([:Entourage, :Smalltalk])
+      end
+
+      # sums up unread conversations for neighborhood conversations
+      def unread_neighborhoods_count
+        UserServices::UnreadMessages.new(
+          user: object
+        ).number_of_unread_for_joinable_types(:Neighborhood)
       end
     end
   end
