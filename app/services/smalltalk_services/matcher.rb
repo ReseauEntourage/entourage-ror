4fec499ddde371f321e87aa736ac7b51f7ebc0cc
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-06-26 14:58:48 +0200

    EN-8308 close unmatched user_smalltalks after fifteen days

--- app/services/smalltalk_services/matcher.rb
+++ app/services/smalltalk_services/matcher.rb
@@ -25,5 +25,15 @@ module SmalltalkServices
         PushNotificationTrigger.new(user_smalltalk, :almost_match, Hash.new).run
       end
     end
+
+    def self.close_unmatched
+      UserSmalltalk.not_matched.where('created_at < ?', 15.days.ago).find_each do |user_smalltalk|
+        user_smalltalk.update(deleted_at: Time.zone.now)
+
+        # using an observer is not possible to send this notification
+        # this notification is only sent from this job and not on all deletions
+        PushNotificationTrigger.new(user_smalltalk, :expire, Hash.new).run
+      end
+    end
   end
 end
