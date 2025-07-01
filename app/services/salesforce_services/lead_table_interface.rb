fbb33651cc29a1391ddde59fd6672c0e5b08b96b
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-01 10:41:09 +0200

    EN-8012 fix missing method declaration and template

--- app/services/salesforce_services/lead_table_interface.rb
+++ app/services/salesforce_services/lead_table_interface.rb
@@ -2,6 +2,9 @@ module SalesforceServices
   class LeadTableInterface < TableInterface
     TABLE_NAME = "Lead"
 
+    INSTANCE_MAPPING = {
+    }
+
     def initialize instance:
       super(table_name: TABLE_NAME, instance: instance)
     end
@@ -12,6 +15,10 @@ module SalesforceServices
 
     def external_id_value
       "ID_externe__c"
+    end
+
+    def instance_mapping
+      INSTANCE_MAPPING
     end
   end
 end
