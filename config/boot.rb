a30ea27ba9b8d278ac50782ba11cf29197dadabe
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-28 17:03:26 +0200

    EN-8111 temporary fix for Rails 6 with ruby 3.2

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,5 @@
+require 'logger' # this line should be deleted when migrating to Rails 7
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
