626e10928924fbb76063dc57804066d37de5bdc9
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-28 14:45:03 +0200

    EN-8244 use HEROKU_REDIS_GOLD_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -45,20 +45,22 @@ module Rpush
   end
 end if Rails.env.development?
 
+redis_url = ENV["HEROKU_REDIS_GOLD_URL"] || ENV["REDIS_URL"]
+
 Sidekiq.configure_server do |config|
   ActiveSupport.on_load(:after_initialize) do
     Rpush.embed unless Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
   end
 
   config.redis = {
-    url: ENV["REDIS_URL"],
+    url: redis_url,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      url: ENV["REDIS_URL"],
+      url: redis_url,
       ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
