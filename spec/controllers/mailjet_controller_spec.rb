42a829f91af324ca995f22b8ed5f75f48ec1095c
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-29 10:37:50 +0200

    EN-8111 apply RSpec/EmptyLineAfterSubject

--- spec/controllers/mailjet_controller_spec.rb
+++ spec/controllers/mailjet_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe MailjetController do
   describe 'POST event' do
     subject { post :event, params: { _json: [event] } }
+
     let(:user) { create :public_user }
     let(:category) { create :email_category }
 
