e5aea63c3124f4805e0f6a448d5d1bd310ddba1c
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-30 16:28:35 +0200

    EN-8111 run rails app:update: merge all default Rails 7.1 configuration with existant configurations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,80 +1,69 @@
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
+
+  # Eager loading is useful in CI to ensure code is loaded as in production
+  config.eager_load = ENV["CI"].present?
+
+  # Cache classes (faster test runs)
   config.cache_classes = true
 
+  # Cache compiled views
   config.action_view.cache_template_loading = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-
-  # Configure public file server for tests with Cache-Control for performance.
+  # Configure public file server with caching headers for performance
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=3600"
   }
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+  # Show full error reports
+  config.consider_all_requests_local = true
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
-
-  # Disable request forgery protection in test environment.
+  # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection = false
 
-  # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :test
-
+  # Deliveries are not actually sent
   config.action_mailer.perform_caching = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost' }
 
-  # Print deprecation notices to the stderr.
+  # Print deprecation warnings to stderr
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  # config.i18n.raise_on_missing_translations = true
-
-  # Raise exceptions for disallowed deprecations.
+  # Raise exception for disallowed deprecations
   config.active_support.disallowed_deprecation = :raise
+  config.active_support.disallowed_deprecation_warnings = []
 
+  # Log ActiveRecord activity to stdout
   config.active_record.logger = ActiveSupport::TaggedLogging.new(Logger.new($stdout))
   config.active_record.logger.level = Logger::INFO
 
-  # DEV / TEST CREDENTIALS
+  # Test-specific ENV variables
   ENV['ANDROID_GCM_API_KEY'] = 'foobar'
+  ENV['ENTOURAGE_IMAGES_BUCKET'] = 'foobar'
+  ENV['ENTOURAGE_AVATARS_BUCKET'] = 'foobar'
+  ENV['ENTOURAGE_AWS_ACCESS_KEY_ID'] = 'foo'
+  ENV['ENTOURAGE_AWS_SECRET_ACCESS_KEY'] = 'bar'
+  ENV['HOST'] = 'localhost'
+  ENV['ORGANIZATION_ADMIN_URL'] = 'localhost'
+  ENV['ENTOURAGE_SOLIGUIDE_HOST'] = 'https://localhost:8080/api/v1/pois'
 
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  # Speed up password hashing in test suite
+  BCrypt::Engine.cost = BCrypt::Engine::MIN_COST
 
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
-
-  ENV['ENTOURAGE_IMAGES_BUCKET']='foobar'
-  ENV['ENTOURAGE_AVATARS_BUCKET']='foobar'
-  ENV['ENTOURAGE_AWS_ACCESS_KEY_ID']='foo'
-  ENV['ENTOURAGE_AWS_SECRET_ACCESS_KEY']='bar'
-
-  ENV['HOST']='localhost'
-  ENV['ORGANIZATION_ADMIN_URL']='localhost'
-  ENV['ENTOURAGE_SOLIGUIDE_HOST']='https://localhost:8080/api/v1/pois'
+  # Raise error if before_action references non-existent actions
+  config.action_controller.raise_on_missing_callback_actions = true
 
   # Limit slow down due to password hashing
   BCrypt::Engine.cost = BCrypt::Engine::MIN_COST
+  # Raises error for missing translations
+  # config.i18n.raise_on_missing_translations = true
 
-  # Annotate rendered view with file names.
+  # Annotate rendered view with filenames
   # config.action_view.annotate_rendered_view_with_filenames = true
 end
