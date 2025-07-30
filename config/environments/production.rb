40211fbce03e01d1407afa5812980e688c5577a2
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-07-30 16:28:35 +0200

    EN-8111 run rails app:update: merge all default Rails 7.1 configuration with existant configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,109 +1,59 @@
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # Gzip compression
   config.middleware.use Rack::Deflater
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+  # Reloading
+  config.enable_reloading = false
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # Eager load code on boot.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Full error reports are disabled.
+  config.consider_all_requests_local = false
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
-
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Serving static files (let NGINX/Apache handle this)
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
+  # Asset compression
   config.assets.js_compressor = :terser
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # Precompiled assets only
+  config.assets.compile = false
 
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '2.0' # updated to 2.0 with migration to Rails 6
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = 'http://assets.example.com'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  # Asset versioning (manuelle pour purge)
+  config.assets.version = '2.0'
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # Force SSL in production
   # config.force_ssl = true
 
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  # Logging configuration
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
-  # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "entourage_back_production"
-
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+  # i18n fallback
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  # Disable deprecation reporting
+  config.active_support.report_deprecations = false
 
-  # Log disallowed deprecations.
-  config.active_support.disallowed_deprecation = :log
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
+  # Schema dump after migrations
   config.active_record.dump_schema_after_migration = false
 
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  config.action_mailer.delivery_method = :smtp
-
+  # ActionMailer config
   if EnvironmentHelper.staging?
     config.action_mailer.delivery_method = :safety_mailer
-
     config.action_mailer.safety_mailer_settings = {
       allowed_matchers: [ /@entourage\.social\z/, /\Aabn\.audit\.[123]@advens\.fr\z/ ],
       delivery_method: :smtp,
@@ -115,42 +65,27 @@ Rails.application.configure do
         authentication: :plain
       }
     }
-  end
 
-  if EnvironmentHelper.staging?
     ENV['REQUEST_PHONE_CHANGE_CHANNEL'] = '#test-env-sms'
     ENV['ENTOURAGE_SOLIGUIDE_HOST'] = 'https://entourage-soliguide-preprod.herokuapp.com/api/v1/pois'
   else
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      port: '587',
+      address: 'in-v3.mailjet.com',
+      user_name: ENV['MAILJET_API_KEY'],
+      password: ENV['MAILJET_SECRET_KEY'],
+      authentication: :plain
+    }
+
     ENV['REQUEST_PHONE_CHANGE_CHANNEL'] = '#moderation-signalements'
     ENV['ENTOURAGE_SOLIGUIDE_HOST'] = 'https://entourage-soliguide-preprod.herokuapp.com/api/v1/pois'
   end
 
-  config.action_mailer.smtp_settings = {
-    port: '587',
-    address: 'in-v3.mailjet.com',
-    user_name: ENV['MAILJET_API_KEY'],
-    password: ENV['MAILJET_SECRET_KEY'],
-    authentication: :plain
-  }
-
-  # Inserts middleware to perform automatic connection switching.
-  # The `database_selector` hash is used to pass options to the DatabaseSelector
-  # middleware. The `delay` is used to determine how long to wait after a write
-  # to send a subsequent read to the primary.
-  #
-  # The `database_resolver` class is used by the middleware to determine which
-  # database is appropriate to use based on the time delay.
-  #
-  # The `database_resolver_context` class is used by the middleware to set
-  # timestamps for the last write to the primary. The resolver uses the context
-  # class timestamps to determine how long to wait before reading from the
-  # replica.
-  #
-  # By default Rails will store a last write timestamp in the session. The
-  # DatabaseSelector middleware is designed as such you can define your own
-  # strategy for connection switching and pass that into the middleware through
-  # these configuration options.
-  # config.active_record.database_selector = { delay: 2.seconds }
-  # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # DNS rebinding protection (exemple à adapter)
+  # config.hosts = [
+  #   "entourage.social",
+  #   /.*\.entourage\.social/
+  # ]
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
