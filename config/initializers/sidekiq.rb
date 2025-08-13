00d571d7de0aa75e8cccbccc0514d83c5f3a5257
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-08-13 17:57:43 +0200

    EN-8111 configure rpush on startup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,18 @@
 redis_url = ENV["HEROKU_REDIS_GOLD_URL"] || ENV["REDIS_URL"]
 
 Sidekiq.configure_server do |config|
-  ActiveSupport.on_load(:after_initialize) do
-    Rpush.embed unless Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
-  end
-
   config.redis = {
     url: redis_url,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
+
+  config.on(:startup) do
+    require 'rpush'
+    Rpush.configure { |c| c.logger = Logger.new($stdout) }
+    Rpush.embed unless Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
+  end
+
+  config.on(:quiet) { Rpush.try(:shutdown) }
 end
 
 Sidekiq.configure_client do |config|
