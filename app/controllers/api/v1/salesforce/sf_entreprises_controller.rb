d1eed3ecbe04e6cd6ad68c60cb9957410d44c090
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-09-11 15:13:21 +0200

    EN-8440 api salesforce index: disable authentication

--- app/controllers/api/v1/salesforce/sf_entreprises_controller.rb
+++ app/controllers/api/v1/salesforce/sf_entreprises_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     module Salesforce
       class SfEntreprisesController < Api::V1::BaseController
+        skip_before_action :authenticate_user!, only: [:index]
+
         def index
           render json: SalesforceServices::SfEntrepriseTableInterface.new
             .records_attributes(per: per, page: page)
