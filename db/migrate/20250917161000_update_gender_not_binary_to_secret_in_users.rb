b2efddb5564c00058a2280d49a44668c7504ad36
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-09-17 16:11:40 +0200

    EN-8433 remove gender not_binary

---
+++ db/migrate/20250917161000_update_gender_not_binary_to_secret_in_users.rb
@@ -0,0 +1,15 @@
+class UpdateGenderNotBinaryToSecretInUsers < ActiveRecord::Migration[6.1]
+  def up
+    User
+      .where("updated_at > '2025-01-01'")
+      .where("options ->> 'gender' = ?", 'not_binary')
+      .update_all("options = jsonb_set(options, '{gender}', '\"secret\"')")
+  end
+
+  def down
+    User
+      .where("updated_at > '2025-01-01'")
+      .where("options ->> 'gender' = ?", 'secret')
+      .update_all("options = jsonb_set(options, '{gender}', '\"not_binary\"')")
+  end
+end
