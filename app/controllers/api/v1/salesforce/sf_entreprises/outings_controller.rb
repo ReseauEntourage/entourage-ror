63230258547c1e9007628adc527d8d9f47c15ca5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-09-24 12:41:46 +0200

    EN-8440 sf entreprises, outings: fix when lists are empty

--- app/controllers/api/v1/salesforce/sf_entreprises/outings_controller.rb
+++ app/controllers/api/v1/salesforce/sf_entreprises/outings_controller.rb
@@ -6,8 +6,12 @@ module Api
           skip_before_action :authenticate_user!, only: [:index]
 
           def index
-            render json: SalesforceServices::SfEntrepriseOutingTableInterface.new(sf_entreprise_id: params[:sf_entreprise_id])
+            outings = SalesforceServices::SfEntrepriseOutingTableInterface.new(sf_entreprise_id: params[:sf_entreprise_id])
               .records_attributes(per: per, page: page)
+
+            return render json: "[]", content_type: "application/json" if outings.empty?
+
+            render json: outings
           end
 
           private
