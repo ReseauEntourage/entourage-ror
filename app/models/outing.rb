8d96cc8fda6972173d26811b8a1b5701079d6561
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-09-29 17:11:33 +0200

    EN-8259 add automatic chat_message for today outings

--- app/models/outing.rb
+++ app/models/outing.rb
@@ -77,6 +77,7 @@ class Outing < Entourage
   scope :ending_after, -> (from) { where("metadata->>'ends_at' >= ?", from) }
   scope :upcoming, -> (until_at) { where("metadata->>'starts_at' BETWEEN ? AND ?", Time.zone.now, until_at) }
   scope :between, -> (from, to) { where("metadata->>'starts_at' BETWEEN ? AND ?", from, to) }
+  scope :upcoming_today, -> { where("metadata->>'starts_at' BETWEEN ? AND ?", Time.zone.now, Time.zone.now.end_of_day) }
 
   scope :recommandable, -> { self.active.future }
   scope :future_or_ongoing, -> { ending_after(Time.zone.now) }
