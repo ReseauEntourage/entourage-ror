fcc65e5d3299aca6fb548182e0fe89511be18efb
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-10-01 13:59:16 +0200

    EN-8111 change fixture_path configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
