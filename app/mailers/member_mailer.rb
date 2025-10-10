fb39312ce709b844469d6d7e9f9aa736e27be913
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-10-10 11:46:11 +0200

    fix: welcome email should be on future outings

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -9,7 +9,7 @@ class MemberMailer < MailjetMailer
                   from: email_with_name("contact@entourage.social", "Le Réseau Entourage"),
                   variables: {
                     outings_url: Entourage.share_url(:outings),
-                    outings: Outing.welcome_category.limit(3).map { |outing|
+                    outings: Outing.future_or_ongoing.welcome_category.limit(3).map { |outing|
                       {
                         name: outing.title,
                         address: outing.event_url,
