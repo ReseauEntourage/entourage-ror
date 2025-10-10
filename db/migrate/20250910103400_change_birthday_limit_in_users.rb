41e6da1bd07bc679035ddfb67a40fe9327320b94
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-10-10 10:44:15 +0200

    remove sql update failing due to timeout

--- db/migrate/20250910103400_change_birthday_limit_in_users.rb
+++ db/migrate/20250910103400_change_birthday_limit_in_users.rb
@@ -3,8 +3,9 @@ class ChangeBirthdayLimitInUsers < ActiveRecord::Migration[5.2]
     change_column :users, :birthday, :string, limit: 10, nullable: true, default: nil
     rename_column :users, :birthday, :birthdate
 
-    execute <<-SQL
-      update users set birthdate = null;
-    SQL
+    # manual execution due to timeout
+    # execute <<-SQL
+    #   update users set birthdate = null;
+    # SQL
   end
 end
