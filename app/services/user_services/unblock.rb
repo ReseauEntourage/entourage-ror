415812b86f10cdbaaccf6090d04b436319db847c
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-10-17 17:57:50 +0200

    search for blocked users: reduce sql scan

--- app/services/user_services/unblock.rb
+++ app/services/user_services/unblock.rb
@@ -1,7 +1,14 @@
 module UserServices
   class Unblock
+    # temporary_block period is 1.month (User::TEMPORARY_BLOCK_PERIOD)
+    # we use last_sign_in_at to reduce psql scan
     def self.run!
-      User.status_is(:blocked).where('unblock_at is not null and date(unblock_at) = ?', Date.today).pluck(:id).each do |user_id|
+      User
+        .status_is(:blocked)
+        .where("last_sign_in_at >= NOW() - INTERVAL '3 months'")
+        .where('unblock_at is not null and date(unblock_at) = ?', Date.today)
+        .pluck(:id)
+        .each do |user_id|
         SlackServices::UnblockUser.new(user_id: user_id).notify
       end
     end
