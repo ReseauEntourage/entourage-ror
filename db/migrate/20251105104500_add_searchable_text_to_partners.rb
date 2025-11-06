20c0c7e516d5b0dbb44ddfb10e5621cf2cfb969b
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-11-06 16:55:22 +0100

    EN-8642 set searchable_text in migration

--- db/migrate/20251105104500_add_searchable_text_to_partners.rb
+++ db/migrate/20251105104500_add_searchable_text_to_partners.rb
@@ -1,8 +1,19 @@
 class AddSearchableTextToPartners < ActiveRecord::Migration[7.1]
   def change
     enable_extension 'pg_trgm' unless extension_enabled?('pg_trgm')
+    enable_extension 'unaccent' unless extension_enabled?('unaccent')
 
     add_column :partners, :searchable_text, :text
+
+    execute <<~SQL
+      UPDATE partners
+      SET searchable_text = unaccent(
+        lower(
+          coalesce(name, '')
+        )
+      );
+    SQL
+
     add_index :partners,
       :searchable_text,
       name: :index_partners_on_searchable_text_trgm,
