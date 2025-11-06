79463caf7bfc62469db2a71d42e58ca24efa0016
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-11-06 16:30:17 +0100

    salesforce_configs: rename developer_name to name

--- app/services/salesforce_services/record_type.rb
+++ app/services/salesforce_services/record_type.rb
@@ -2,12 +2,12 @@ module SalesforceServices
   class RecordType
     TABLE_NAME = 'RecordType'
 
-    DEVELOPER_NAME_USER_MAPPING = {
+    NAME_USER_MAPPING = {
       ask_for_help: 'Personne_preca',
       offer_help: 'Riverain',
     }
 
-    DEVELOPER_NAME_OUTING = 'Campagne'
+    NAME_OUTING = 'Campagne'
 
     RecordTypeStruct = Struct.new(:record_type) do
       def initialize(record_type: nil)
@@ -17,7 +17,7 @@ module SalesforceServices
       def upsert
         salesforce_config = SalesforceConfig.find_or_initialize_by(salesforce_id: @record_type['Id'])
         salesforce_config.klass = TABLE_NAME
-        salesforce_config.developer_name = @record_type['DeveloperName']
+        salesforce_config.name = @record_type['DeveloperName']
         salesforce_config.save
       end
     end
@@ -33,18 +33,18 @@ module SalesforceServices
         end
       end
 
-      def find_by_developer_name developer_name
-        SalesforceConfig.find_by_klass_and_developer_name(TABLE_NAME, developer_name)
+      def find_by_name name
+        SalesforceConfig.find_by_klass_and_name(TABLE_NAME, name)
       end
 
       def find_for_outing
-        find_by_developer_name(DEVELOPER_NAME_OUTING)
+        find_by_name(NAME_OUTING)
       end
 
       def find_for_user user
-        developer_name = user.is_ask_for_help? ? DEVELOPER_NAME_USER_MAPPING[:ask_for_help] : DEVELOPER_NAME_USER_MAPPING[:offer_help]
+        name = user.is_ask_for_help? ? NAME_USER_MAPPING[:ask_for_help] : NAME_USER_MAPPING[:offer_help]
 
-        find_by_developer_name(developer_name)
+        find_by_name(name)
       end
     end
   end
