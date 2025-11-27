ec3c8d1dea7c94d3ad6afe012d4f1f79c3406b20
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2025-11-27 13:18:02 +0100

    poc-moderator-openai: fix code_chunks db for test env

--- db/migrate/20251127092406_create_code_chunks.rb
+++ db/migrate/20251127092406_create_code_chunks.rb
@@ -1,6 +1,6 @@
 class CreateCodeChunks < ActiveRecord::Migration[7.1]
   def change
-    enable_extension "vector" unless extension_enabled?("vector")
+    enable_extension "vector" unless Rails.env.test?
 
     create_table :code_chunks do |t|
       t.string  :filepath, null: false
@@ -8,16 +8,22 @@ class CreateCodeChunks < ActiveRecord::Migration[7.1]
       t.integer :end_line, null: false
       t.text    :content, null: false
 
-      t.column :embedding, "vector(1536)"
+      if Rails.env.test?
+        t.text :embedding
+      else
+        t.column :embedding, "vector(1536)"
+      end
 
       t.timestamps
     end
 
-    execute <<~SQL
-      CREATE INDEX code_chunks_embedding_ivfflat
-      ON code_chunks
-      USING ivfflat (embedding vector_l2_ops)
-      WITH (lists = 100);
-    SQL
+    unless Rails.env.test?
+      execute <<~SQL
+        CREATE INDEX code_chunks_embedding_ivfflat
+        ON code_chunks
+        USING ivfflat (embedding vector_l2_ops)
+        WITH (lists = 100);
+      SQL
+    end
   end
 end
