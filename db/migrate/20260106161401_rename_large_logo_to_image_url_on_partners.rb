dac786495fa269f88fa49fd2683bfe6e9805d031
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-01-09 15:19:23 +0100

    EN-8684 change migrations: save large_logo_url to large_logo_url_old to ease recovery

--- db/migrate/20260106161401_rename_large_logo_to_image_url_on_partners.rb
+++ db/migrate/20260106161401_rename_large_logo_to_image_url_on_partners.rb
@@ -1,5 +1,29 @@
 class RenameLargeLogoToImageUrlOnPartners < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :partners, :large_logo_url, :image_url
+  def up
+    add_column :partners, :image_url, :string
+
+    execute <<~SQL
+      UPDATE partners
+      SET image_url = large_logo_url
+    SQL
+
+    execute <<~SQL
+      UPDATE partners
+      SET image_url = SUBSTRING(image_url FROM #{prefix.length + 1})
+      WHERE image_url LIKE '#{prefix}%';
+    SQL
+
+    rename_column :partners, :large_logo_url, :large_logo_url_old
+  end
+
+  def down
+    remove_column :partners, :image_url, :string
+    rename_column :partners, :large_logo_url_old, :large_logo_url
+  end
+
+  private
+
+  def prefix
+    @prefix ||= Partner.bucket.public_url(key: Partner.bucket_prefix + "/")
   end
 end
