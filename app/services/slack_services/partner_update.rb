490ea2c054a9697a613023993e36cdea7c285e19
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-01-12 15:54:17 +0100

    EN-8784 change slack alert

--- app/services/slack_services/partner_update.rb
+++ app/services/slack_services/partner_update.rb
@@ -11,17 +11,25 @@ module SlackServices
 
     def payload
       changes_text = changes.map { |attribute, (before, after)|
-        { text: "#{attribute} a changé : \n #{before} (avant) \n #{after} (après)" }
+        if attribute == 'image_url'
+          { text: "Nouveau logo :", image_url: @parter.image_url_with_size(:small) }
+        else
+          { text: "Nouveau #{attribute} : #{after}" }
+        end
       }
 
       {
-        text: "<@#{slack_moderator_id(@user)}> ou team modération (département : #{departement(@user) || 'n/a'}) L'utilisateur #{@user.full_name}, #{@email || '[Email non indiqué]'} a mis à jour l'association suivante. Merci d'en vérifier le contenu.",
+        text: "Une association a modifié ses informations",
+
         attachments: [{
-          text: "Utilisateur : #{link_to_user(@user.id)}"
+          text: "Utilisateur ayant modifié l'association : #{[@user.full_name, @user.email].compact.join(', ')} #{link_to_user(@user.id)}"
         }, {
-          text: "Association : #{link_to_partner(@partner)}",
-          image_url: @partner.image_url_with_size(:small)
-        }] + changes_text
+          text: "Association concernée : #{link_to_partner(@partner)}"
+        }, {
+          text: "Référent modé : <@#{slack_moderator_id(@user)}> (département : #{departement(@user) || 'n/a'})"
+        }] + changes_text + {
+          text: ":index_vers_la_droite::couleur-de-peau-2: Merci de vérifier les informations renseignées !"
+        }
       }
     end
 
