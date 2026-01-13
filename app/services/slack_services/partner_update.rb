4c4fb144cb03571b3d2d6e53832e8c4d1a0bf50d
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-01-13 16:28:40 +0100

    EN-8784 add tracked attributes

--- app/services/slack_services/partner_update.rb
+++ app/services/slack_services/partner_update.rb
@@ -1,8 +1,16 @@
 module SlackServices
   class PartnerUpdate < Notifier
+    TRACKED_ATTRIBUTES = %w[name description image_url].freeze
+
     def initialize user:, partner:
       @user = user
       @partner = partner
+    end
+
+    def notify
+      return unless should_notify?
+
+      super
     end
 
     def env
@@ -42,8 +50,12 @@ module SlackServices
 
     private
 
+    def should_notify?
+      (changes.keys & TRACKED_ATTRIBUTES).any?
+    end
+
     def changes
-      changes = @partner.previous_changes.except("updated_at", "searchable_text")
+      @changes ||= @partner.previous_changes.slice(*TRACKED_ATTRIBUTES)
     end
 
     def url
