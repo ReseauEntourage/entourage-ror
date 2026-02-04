d9105cc8b93df19a3a501cc5ab9182608c275efb
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-02-04 16:31:23 +0100

    skip_before_action verify_authenticity_token on mailjet/event

--- app/controllers/mailjet_controller.rb
+++ app/controllers/mailjet_controller.rb
@@ -1,4 +1,6 @@
 class MailjetController < ActionController::Base
+  skip_before_action :verify_authenticity_token, only: [:event]
+
   # @see https://app.mailjet.com/account/triggers
   # a hook on mailjet sends notification using this controller to notify that a user has unsuscribe
   def event
