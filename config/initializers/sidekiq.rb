7884a4adbcdc699c78b6046ac78157a46990cc7f
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-04-03 15:13:32 +0200

    EN-8831 fix sidekiq.rb: use schedule[:schedule] || schedule['schedule']
    refactor

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,32 +10,65 @@ Sidekiq.configure_server do |config|
   }
 
   config.on(:startup) do
-    require 'rpush'
-    Rpush.configure { |c| c.logger = Logger.new($stdout) }
-    Rpush.embed unless Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
+    setup_rpush
+    load_sidekiq_cron_jobs
   end
 
-  config.on(:quiet) { Rpush.try(:shutdown) }
+  config.on(:quiet) do
+    shutdown_rpush
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      url: redis_url,
-      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
 
-Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    schedule_file = Rails.root.join('config/sidekiq.yml')
+def setup_rpush
+  return if Rails.env.test? || ENV['DISABLE_RPUSH'] == 'true'
 
-    if File.exist?(schedule_file)
-      schedule = YAML.safe_load(
-        File.read(schedule_file),
-        permitted_classes: [Symbol]
-      )
+  require 'rpush'
 
-      Sidekiq::Cron::Job.load_from_hash!(schedule['schedule'])
-    end
+  Rpush.configure do |c|
+    c.logger = Logger.new($stdout)
   end
+
+  Rpush.embed
+rescue => e
+  Rails.logger.error("Rpush setup failed: #{e.message}")
+end
+
+def shutdown_rpush
+  Rpush.try(:shutdown)
+rescue => e
+  Rails.logger.error("Rpush shutdown failed: #{e.message}")
+end
+
+def load_sidekiq_cron_jobs
+  schedule_file = Rails.root.join('config/sidekiq.yml')
+
+  unless File.exist?(schedule_file)
+    Rails.logger.info("No sidekiq.yml schedule file found")
+    return
+  end
+
+  schedule = YAML.safe_load(
+    File.read(schedule_file),
+    permitted_classes: [Symbol],
+    aliases: true
+  )
+
+  cron_config = schedule[:schedule] || schedule['schedule']
+
+  if cron_config.present?
+    Sidekiq::Cron::Job.load_from_hash!(cron_config)
+    Rails.logger.info("Loaded #{cron_config.size} Sidekiq cron jobs")
+  else
+    Rails.logger.warn("No Sidekiq cron schedule found in sidekiq.yml")
+  end
+
+rescue => e
+  Rails.logger.error("Failed to load Sidekiq cron jobs: #{e.message}")
 end
