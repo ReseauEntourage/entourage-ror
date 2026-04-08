58c661186bc04a7f087f8f1146a146e46ab9f35a
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-04-08 18:12:16 +0200

    EN-9093 fix rspec

--- spec/models/entourage_spec.rb
+++ spec/models/entourage_spec.rb
@@ -127,6 +127,8 @@ RSpec.describe Entourage, type: :model do
       :portrait_thumbnail_url => nil,
       :place_limit => nil,
       :reserved_female => nil,
+      :unsubscribed_participants_offer_help => 0,
+      :unsubscribed_participants_ask_for_help => 0,
       :$id=>'urn:entourage:outing:metadata'
     ) }
     it { expect(build(:outing, default_metadata: {}).tap(&:save).errors.messages).to eq(
