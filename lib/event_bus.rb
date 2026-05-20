9e9661fc973a8d083f81fe7ef7d56d078a5f03c6
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-05-20 17:12:12 +0200

    EN-9108 avoid multiple subscriptions

--- lib/event_bus.rb
+++ lib/event_bus.rb
@@ -3,13 +3,17 @@ class EventBus
 
   class << self
     def subscribe(event_name, handler)
-      @subscribers[event_name] << handler
+      @subscribers[event_name] |= [handler]
     end
 
     def publish(event_name, payload = {})
       @subscribers[event_name].each do |handler|
         handler.call(payload)
       end
+    end
+
+    def reset!
+      @subscribers.clear
     end
   end
 end
