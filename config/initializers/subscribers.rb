279901a7d345908cce71e27db45023ab67f855e5
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-05-22 09:12:39 +0200

    Revert "EN-9108 avoid multiple subscriptions"
    This reverts commit 9e9661fc973a8d083f81fe7ef7d56d078a5f03c6.

--- config/initializers/subscribers.rb
+++ config/initializers/subscribers.rb
@@ -1,4 +1,3 @@
 Rails.application.config.to_prepare do
-  EventBus.reset!
   BadgeSubscriber.register!
 end
--- lib/event_bus.rb
+++ lib/event_bus.rb
@@ -3,17 +3,13 @@ class EventBus
 
   class << self
     def subscribe(event_name, handler)
-      @subscribers[event_name] |= [handler]
+      @subscribers[event_name] << handler
     end
 
     def publish(event_name, payload = {})
       @subscribers[event_name].each do |handler|
         handler.call(payload)
       end
-    end
-
-    def reset!
-      @subscribers.clear
     end
   end
 end
