cb3dbf0d9231499e7a8a0f28bca38c59213b330f
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-07-06 17:21:04 +0200

    simplify migration

--- db/migrate/20260706150000_add_instance_type_created_at_index_to_user_reactions.rb
+++ db/migrate/20260706150000_add_instance_type_created_at_index_to_user_reactions.rb
@@ -2,9 +2,6 @@ class AddInstanceTypeCreatedAtIndexToUserReactions < ActiveRecord::Migration[7.1
   disable_ddl_transaction!
 
   def change
-    add_index :user_reactions, [:instance_type, :created_at],
-      name: 'index_user_reactions_on_instance_type_and_created_at',
-      algorithm: :concurrently,
-      if_not_exists: true
+    add_index :user_reactions, [:instance_type, :created_at], if_not_exists: true
   end
 end
