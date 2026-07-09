fa7630b6d2f42cac605dbc3f61225c88339f1f2d
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-07-09 11:19:15 +0200

    EN-9366 filter air_conditioned on api/v1/pois/clusters

--- app/models/concerns/geolocalizable.rb
+++ app/models/concerns/geolocalizable.rb
@@ -3,19 +3,30 @@ module Geolocalizable
 
   included do
     scope :around, -> (latitude, longitude, distance) do
-      return unless latitude && longitude
+      box = geolocalizable_bounding_box(latitude, longitude, distance)
+      return unless box
 
-      distance ||= 10
-      box = Geocoder::Calculations.bounding_box([latitude, longitude], distance, units: :km)
       within_bounding_box(box)
     end
 
-    scope :clustered, -> (latitude, longitude, distance) {
-      return unless latitude && longitude
+    scope :clustered, -> (latitude, longitude, distance, filters = {}) {
+      box = geolocalizable_bounding_box(latitude, longitude, distance)
+      return unless box
 
-      max_clusters = self.around(latitude, longitude, distance).count
-      box = Geocoder::Calculations.bounding_box([latitude, longitude], distance, units: :km)
+      filters = filters.compact
+      filters.each_key do |column|
+        raise ArgumentError, "invalid clustered filter column: #{column}" unless column_names.include?(column.to_s)
+      end
+
+      pois_in_range = self.around(latitude, longitude, distance)
+      pois_in_range = pois_in_range.where(filters) if filters.present?
+      max_clusters = pois_in_range.count
+
       bounding_box_sql = Geocoder::Sql.within_bounding_box(*box, :latitude, :longitude)
+
+      conditions_sql = [bounding_box_sql] + filters.map { |column, value|
+        sanitize_sql_array(["#{column} = ?", value])
+      }
 
       select("
         CASE WHEN COUNT(*) = 1 THEN MIN(pois.id) ELSE NULL END AS id,
@@ -32,8 +43,14 @@ module Geolocalizable
       ").from(sanitize_sql_array ["(
         SELECT id, validated, source, source_id, name, adress, latitude, phone, email, longitude, category_id, ST_ClusterKMeans(ST_Transform((ST_SetSRID(ST_MakePoint(longitude, latitude), 4326))::geometry, 4326), LEAST(#{max_clusters}, 30)) OVER () AS cluster_id
         FROM pois as to_be_clustered
-        WHERE #{bounding_box_sql}
+        WHERE #{conditions_sql.join(' AND ')}
       ) AS pois"]).group('cluster_id')
     }
+
+    def self.geolocalizable_bounding_box(latitude, longitude, distance)
+      return unless latitude && longitude
+
+      Geocoder::Calculations.bounding_box([latitude, longitude], distance || 10, units: :km)
+    end
   end
 end
