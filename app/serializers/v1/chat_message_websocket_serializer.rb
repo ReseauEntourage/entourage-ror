cd5a1c3b843e516f24e9d4f56224b04f461b5411
nicolas-entourage <75681929+nicolas-entourage@users.noreply.github.com>
2026-09-10 11:22:53 +0200

    EN-9370 add post_id to ChatMessageWebsocketSerializer

--- app/serializers/v1/chat_message_websocket_serializer.rb
+++ app/serializers/v1/chat_message_websocket_serializer.rb
@@ -1,6 +1,11 @@
 module V1
   class ChatMessageWebsocketSerializer < V1::ChatMessageSerializer
     attribute :reactions
+    attribute :post_id
+
+    def post_id
+      object.parent_id
+    end
 
     def reactions
       object.reactions.summary
